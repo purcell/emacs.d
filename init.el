17c9568f07269d57e9cede43c95f7ea065c947a7
Steve Purcell <steve@sanityinc.com>
2006-10-05 15:48:00 +0000

    Move customised settings to a separate file
    darcs-hash:20061005154807-6ba2e-6e386619f1d944fdd1c65364f9aed2ba4c780d0a.gz

--- init.el
+++ init.el
@@ -117,6 +117,9 @@
 (require 'darcsum)
 (setq darcsum-whatsnew-switches "-l")
 
+(require 'grep)
+(add-to-list 'grep-find-ignored-directories "_darcs")
+
 
 ;;----------------------------------------------------------------------------
 ;; Multiple major modes
@@ -156,8 +159,9 @@
 ;;----------------------------------------------------------------------------
 ;; Desktop saving
 ;;----------------------------------------------------------------------------
-;; save a list of open files in ~/.emacs.desktop
+;; save a list of open files in ~/.emacs.d/.emacs.desktop
 ;; save the desktop file automatically if it already exists
+(setq desktop-path '("~/.emacs.d"))
 (setq desktop-save 'if-exists)
 (desktop-save-mode 1)
 
@@ -204,6 +208,7 @@
 
 (and standard-display-table (aset standard-display-table ?\C-i  ; Show tabs highlighted
                                   (vector (+ ?\C-i (* (face-id 'we-hate-tabs-face) 524288) ))))
+(setq show-trailing-whitespace t)
 
 
 ;;----------------------------------------------------------------------------
@@ -379,71 +384,8 @@
 ;;----------------------------------------------------------------------------
 ;; Variables configured via the interactive 'customize' interface
 ;;----------------------------------------------------------------------------
-(custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(auto-compression-mode t nil (jka-compr))
- '(blink-cursor-delay 0)
- '(blink-cursor-interval 0.4)
- '(buffers-menu-max-size 30)
- '(case-fold-search t)
- '(column-number-mode t)
- '(compilation-scroll-output t)
- '(compile-command "make")
- '(current-language-environment "UTF-8")
- '(default-input-method "latin-1-prefix")
- '(desktop-path (quote ("~/.emacs.d" "." "~")))
- '(ecb-compile-window-height 0.2)
- '(ecb-grep-find-function (quote rgrep))
- '(ecb-layout-name "left-dir-plus-speedbar")
- '(ecb-layout-window-sizes (quote (("left-analyse" (0.20297029702970298 . 0.33962264150943394) (0.20297029702970298 . 0.20754716981132076) (0.20297029702970298 . 0.20754716981132076) (0.20297029702970298 . 0.22641509433962265)))))
- '(ecb-options-version "2.33beta1")
- '(ecb-primary-secondary-mouse-buttons (quote mouse-1--C-mouse-1))
- '(ecb-run-ediff-in-ecb-frame nil)
- '(ecb-tip-of-the-day nil)
- '(ecb-wget-setup (quote ("wget" . other)))
- '(ecb-windows-width 0.2)
- '(ediff-window-setup-function (quote ediff-setup-windows-plain))
- '(face-default-stipple "gray3" t)
- '(follow-auto nil)
- '(frame-background-mode nil)
- '(global-font-lock-mode t nil (font-lock))
- '(global-hl-line-mode nil)
- '(grep-find-ignored-directories (quote ("CVS" ".hg" "{arch}" ".svn" "_darcs")))
- '(highlight-changes-global-changes-existing-buffers t)
- '(highlight-changes-global-initial-state (quote active))
- '(ibuffer-enable t)
- '(indent-tabs-mode nil)
- '(inhibit-splash-screen t)
- '(make-backup-files nil)
- '(mouse-yank-at-point t)
- '(recentf-exclude (quote ("/tmp/")))
- '(recentf-mode t)
- '(ruby-electric-expand-delimiters-list nil)
- '(show-paren-mode t)
- '(show-trailing-whitespace t)
- '(slime-autodoc-mode t t)
- '(speedbar-directory-unshown-regexp "^\\(CVS\\|RCS\\|SCCS\\|\\.svn\\|_darcs\\)\\'")
- '(speedbar-show-unknown-files t)
- '(speedbar-use-images nil)
- '(time-stamp-active t)
- '(time-stamp-format "%3a %3b %2d %02H:%02M:%02S %Z %:y")
- '(tooltip-delay 1.5)
- '(tooltip-mode t nil (tooltip))
- '(truncate-lines t)
- '(uniquify-buffer-name-style nil nil (uniquify))
- '(visible-bell t)
- '(whitespace-check-indent-whitespace t))
-(custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(default ((t (:background "black" :foreground "white"))))
- '(cursor ((t (:background "white" :inverse-video t))))
- '(highlight-changes ((((min-colors 88) (class color)) (:stipple nil :background "#1E1E1E")))))
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
 
 
 ;;----------------------------------------------------------------------------
