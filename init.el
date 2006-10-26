92576363ad5f8cefc8c9e42846960d2c65db6784
Steve Purcell <steve@sanityinc.com>
2006-10-26 07:00:00 +0000

    Correct typo in call to set-terminal-coding-system
    darcs-hash:20061026070057-6ba2e-75d22215ef4cd854b4d538f70af33e978297ab1f.gz

--- init.el
+++ init.el
@@ -434,7 +434,6 @@
   (set-keyboard-coding-system 'utf-8-mac)
   (setq locale-coding-system 'utf-8)
   (set-default-coding-systems 'utf-8)
-  (set-terminal-coding-systems 'utf-8)
-  (set-clipboard-coding-systems 'utf-8)
+  (set-terminal-coding-system 'utf-8)
   (set-selection-coding-system 'utf-8)
   (prefer-coding-system 'utf-8))
