a1725cb106177c73a4e23ae58dfbe8394f4a64a4
Steve Purcell <steve@sanityinc.com>
2007-08-12 10:40:00 +0000

    Add lispdoc function
    darcs-hash:20070812104023-6ba2e-c5fd3159fc6ace978cac4f786fdc334d32db3e90.gz

--- init.el
+++ init.el
@@ -522,7 +522,33 @@
   (add-auto-mode 'lisp-mode "\\.cl$")
   (add-hook 'slime-mode-hook 'pretty-lambdas)
   (add-hook 'slime-mode-hook (lambda () (enable-paredit slime-mode-map)))
-  (global-set-key [f4] 'slime-selector))
+  (global-set-key [f4] 'slime-selector)
+
+  ; From http://bc.tech.coop/blog/070515.html
+  (defun lispdoc ()
+    "Searches lispdoc.com for SYMBOL, which is by default the symbol currently under the curser"
+    (interactive)
+    (let* ((word-at-point (word-at-point))
+           (symbol-at-point (symbol-at-point))
+           (default (symbol-name symbol-at-point))
+           (inp (read-from-minibuffer
+                 (if (or word-at-point symbol-at-point)
+                     (concat "Symbol (default " default "): ")
+                   "Symbol (no default): "))))
+      (if (and (string= inp "") (not word-at-point) (not
+                                                     symbol-at-point))
+          (message "you didn't enter a symbol!")
+        (let ((search-type (read-from-minibuffer
+                            "full-text (f) or basic (b) search (default b)? ")))
+          (browse-url (concat "http://lispdoc.com?q="
+                              (if (string= inp "")
+                                  default
+                                inp)
+                              "&search="
+                              (if (string-equal search-type "f")
+                                  "full+text+search"
+                                "basic+search")))))))
+  (define-key lisp-mode-map (kbd "C-c l") 'lispdoc))
 
 (when *scheme-support-enabled*
   ; See http://bc.tech.coop/scheme/scheme-emacs.htm
