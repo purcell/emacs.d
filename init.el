9383232490c28a562db3c37518b8f39c8141a7ca
Steve Purcell <steve@sanityinc.com>
2008-04-24 15:42:25 +0200

    Add redshank

--- init.el
+++ init.el
@@ -687,7 +687,12 @@
                               (if (string-equal search-type "f")
                                   "full+text+search"
                                 "basic+search")))))))
-  (define-key lisp-mode-map (kbd "C-c l") 'lispdoc))
+
+  (define-key lisp-mode-map (kbd "C-c l") 'lispdoc)
+
+  (autoload 'redshank-mode "redshank" "Minor mode for editing and refactoring (Common) Lisp code." t)
+  (autoload 'turn-on-redshank-mode "redshank" "Turn on Redshank mode. Please see function `redshank-mode'." t)
+  (add-hook 'lisp-mode-hook 'turn-on-redshank-mode))
 
 (when *scheme-support-enabled*
   ; See http://bc.tech.coop/scheme/scheme-emacs.htm
