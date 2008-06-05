2c2daf688d8398f97ec6562d642fd8297970ff91
Steve Purcell <steve@sanityinc.com>
2008-06-05 17:20:23 +0200

    Update haml-mode and sass-mode from upstream

--- site-lisp/haml/haml-mode.el
+++ site-lisp/haml/haml-mode.el
@@ -1,5 +1,4 @@
 ;;; haml-mode.el -- Major mode for editing Haml files
-;;; Version 0.0.1
 ;;; Written by Nathan Weizenbaum
 
 ;;; Because Haml's indentation schema is similar
@@ -7,12 +6,15 @@
 ;;; functions are similar to those in yaml-mode and python-mode.
 
 ;;; To install, save this somewhere and add the following to your .emacs file:
-;;; 
+;;;
 ;;; (add-to-list 'load-path "/path/to/haml-mode.el")
 ;;; (require 'haml-mode nil 't)
-;;; 
+;;; (add-to-list 'auto-mode-alist '("\\.sass$" . sass-mode))
+;;;
 
 ;;; Code:
+
+(eval-when-compile (require 'cl))
 
 ;; User definable variables
 
@@ -22,120 +24,73 @@
   :prefix "haml-")
 
 (defcustom haml-mode-hook nil
-  "*Hook run by `haml-mode'."
+  "Hook run when entering Haml mode."
   :type 'hook
   :group 'haml)
 
 (defcustom haml-indent-offset 2
-  "*Amount of offset per level of indentation."
+  "Amount of offset per level of indentation."
   :type 'integer
   :group 'haml)
 
-(defcustom haml-backspace-function 'backward-delete-char-untabify
-  "*Function called by `haml-electric-backspace' when deleting backwards."
-  :type 'function
+(defcustom haml-backspace-backdents-nesting t
+  "Non-nil to have `haml-electric-backspace' re-indent all code
+nested beneath the backspaced line be re-indented along with the
+line itself."
+  :type 'boolean
   :group 'haml)
 
 (defface haml-tab-face
-   '((((class color)) (:background "red" :foreground "red" :bold t))
-     (t (:reverse-video t)))
+  '((((class color)) (:background "hotpink"))
+    (t (:reverse-video t)))
   "Face to use for highlighting tabs in Haml files."
   :group 'faces
   :group 'haml)
 
-;; Helper Functions
+(defvar haml-indent-function 'haml-indent-p
+  "This function should look at the current line and return true
+if the next line could be nested within this line.")
 
-(defun string-* (str n)
-  "Concatenates a string with itself n times."
-  (if (= n 0) ""
-    (concat str (string-* str (- n 1)))))
-
-(defun find-if (f lst)
-  "Returns the first element of a list for which a function returns a non-nil value, or nil if no such element is found."
-  (while (not (or (null lst)
-                  (apply f (list (car lst)))))
-    (setq lst (cdr lst)))
-  (if (null lst) nil (car lst)))
-
-(defun hre (str)
-  "Prepends a Haml-tab-matching regexp to str."
-  (concat "^\\(" (string-* " " haml-indent-offset) "\\)*" str))
+(defvar haml-block-openers
+  `("^ *\\([%\\.#][^ \t]*\\)\\(\\[.*\\]\\)?\\({.*}\\)?\\(\\[.*\\]\\)?[ \t]*$"
+    "^ *[-=].*do[ \t]*\\(|.*|[ \t]*\\)?$"
+    ,(concat "^ *-[ \t]*\\("
+             (regexp-opt '("else" "elsif" "rescue" "ensure" "when"))
+             "\\)")
+    "^ */\\(\\[.*\\]\\)?[ \t]*$"
+    "^ *-#"
+    "^ *:")
+  "A list of regexps that match lines of Haml that could have
+text nested beneath them.")
 
 ;; Font lock
 
-(defconst haml-font-lock-keywords-1
-  (list
-   ;; instruct
-   '("^!!!.*"                    0 font-lock-constant-face)
-   ;; strings
-   '("\\('[^']*'\\)"             1 font-lock-string-face append)
-   '("\\(\"[^\"]*\"\\)"          1 font-lock-string-face append)
-   ;; symbol
-   '("&?:\\w+"                   0 font-lock-constant-face append)
-   ;; ruby varible
-   '("@[a-z0-9_]+"               0 font-lock-variable-name-face append)
-   ;; pipe
-   '("| *$"                      0 font-lock-string-face)
-   ;; comment
-   '("^[ \t]*\\(/.*\\)$"         1 font-lock-comment-face append)
-   ;; id
-   '("^ *\\(#[a-z0-9_]+\/?\\)"   1 font-lock-keyword-face)
-   ;; class
-   '("^ *\\(\\.[a-z0-9_]+\/?\\)" 1 font-lock-type-face)
-   ;; tag
-   '("^ *\\(%[a-z0-9_]+\/?\\)"   1 font-lock-function-name-face )
-   ;; class after id
-   '("^ *\\(#[a-z0-9_]+\/?\\)"   (1 font-lock-keyword-face) ("\\.[a-z0-9_]+" nil nil (0 font-lock-type-face)))
-   ;; class after class
-   '("^ *\\(\\.[a-z0-9_]+\/?\\)" (1 font-lock-type-face) ("\\.[a-z0-9_]+" nil nil (0 font-lock-type-face)))
-   ;; id after class
-   '("^ *\\(\\.[a-z0-9_]+\/?\\)" (1 font-lock-type-face) ("\\#[a-z0-9_]+" nil nil (0 font-lock-keyword-face)))
-   ;; class after tag
-   '("^ *\\(%[a-z0-9_]+\/?\\)"   (1 font-lock-function-name-face) ("\\.[a-z0-9_]+" nil nil (0 font-lock-type-face)))
-   ;; id after tag
-   '("^ *\\(%[a-z0-9_]+\/?\\)"   (1 font-lock-function-name-face) ("\\#[a-z0-9_]+" nil nil (0 font-lock-keyword-face)))
-   ;; embeded ruby: beggin of line
-   '("^ *\\([~=-] .*\\)"          1 font-lock-preprocessor-face prepend)
-   ;; embeded ruby: after tag,class,id
-   '("^ *[\\.#%a-z0-9_]+\\([~=-] .*\\)"     1 font-lock-preprocessor-face prepend)
-   ;; embeded ruby: attributes
-   '("^ *[\\.#%a-z0-9_]+\\({[^}]+}\\)"      1 font-lock-preprocessor-face prepend)
-   ;; embeded ruby: square
-   '("^ *[\\.#%a-z0-9_]+\\(\\[[^]]+\\]\\)"  1 font-lock-preprocessor-face prepend)))
-
-;; Constants
-
-(defconst haml-mode-version "0.0.1" "Version of `haml-mode.'")
-
-(defconst haml-blank-line-re "^[ \t]*$"
-  "Regexp matching a line containing only whitespace.")
-
-; Base for Regexen matching a Haml tag.
-(setq haml-tag-re-base (hre "\\([%\\.#][^ \t]*\\)\\({.*}\\)?\\(\\[.*\\]\\)?"))
-
-(defconst haml-tag-nest-re (concat haml-tag-re-base "[ \t]*$")
-  "Regexp matching a Haml tag that can have nested elements.")
-
-(defconst haml-tag-re (concat haml-tag-re-base "\\(.?\\)")
-  "Regexp matching a Haml tag.")
-
-(defconst haml-block-re (hre "[-=].*do[ \t]*\\(|.*|[ \t]*\\)?$")
-  "Regexp matching a Ruby block in Haml.")
-
-(defconst haml-block-cont-re (hre (concat "-[ \t]*"
-                                          (regexp-opt '("else" "elsif"
-                                                        "rescue" "ensure"
-                                                        "when"))))
-  "Regexp matching a continued Ruby block in Haml.")
-
-(defconst haml-html-comment-re (hre "/\\(\\[.*\\]\\)?[ \t]*$")
-  "Regexp matching a Haml HTML comment command.")
-
-(defconst haml-comment-re (hre "-#[ \t]$")
-  "Regexp matching a Haml comment command.")
-
-(defconst haml-filter-re (hre ":")
-  "Regexp matching a Haml filter command.")
+(defconst haml-font-lock-keywords
+  '(("^ *\\(\t\\)"               1 'haml-tab-face)
+    ("^!!!.*"                    0 font-lock-constant-face)
+    ("\\('[^']*'\\)"             1 font-lock-string-face append)
+    ("\\(\"[^\"]*\"\\)"          1 font-lock-string-face append)
+    ("&?:\\w+"                   0 font-lock-constant-face append)
+    ("@[a-z0-9_]+"               0 font-lock-variable-name-face append)
+    ("| *$"                      0 font-lock-string-face)
+    ("^[ \t]*\\(/.*\\)$"         1 font-lock-comment-face append)
+    ("^ *\\(#[a-z0-9_]+\/?\\)"   1 font-lock-keyword-face)
+    ("^ *\\(\\.[a-z0-9_]+\/?\\)" 1 font-lock-type-face)
+    ("^ *\\(%[a-z0-9_]+\/?\\)"   1 font-lock-function-name-face)
+    ("^ *\\(#[a-z0-9_]+\/?\\)"   (1 font-lock-keyword-face)
+     ("\\.[a-z0-9_]+" nil nil    (0 font-lock-type-face)))
+    ("^ *\\(\\.[a-z0-9_]+\/?\\)" (1 font-lock-type-face)
+     ("\\.[a-z0-9_]+" nil nil    (0 font-lock-type-face)))
+    ("^ *\\(\\.[a-z0-9_]+\/?\\)" (1 font-lock-type-face)
+     ("\\#[a-z0-9_]+" nil nil    (0 font-lock-keyword-face)))
+    ("^ *\\(%[a-z0-9_]+\/?\\)"   (1 font-lock-function-name-face)
+     ("\\.[a-z0-9_]+" nil nil    (0 font-lock-type-face)))
+    ("^ *\\(%[a-z0-9_]+\/?\\)"   (1 font-lock-function-name-face)
+     ("\\#[a-z0-9_]+" nil nil    (0 font-lock-keyword-face)))
+    ("^ *\\([~=-] .*\\)"         1 font-lock-preprocessor-face prepend)
+    ("^ *[\\.#%a-z0-9_]+\\([~=-] .*\\)"     1 font-lock-preprocessor-face prepend)
+    ("^ *[\\.#%a-z0-9_]+\\({[^}]+}\\)"      1 font-lock-preprocessor-face prepend)
+    ("^ *[\\.#%a-z0-9_]+\\(\\[[^]]+\\]\\)"  1 font-lock-preprocessor-face prepend)))
 
 ;; Mode setup
 
@@ -146,45 +101,169 @@
     table)
   "Syntax table in use in haml-mode buffers.")
 
-(defvar haml-mode-map ()
-  "Keymap used in `haml-mode' buffers.")
-(if haml-mode-map
-    nil
-  (setq haml-mode-map (make-sparse-keymap))
-  (define-key haml-mode-map [backspace] 'haml-electric-backspace)
-  (define-key haml-mode-map "\C-?" 'haml-electric-backspace)
-  (define-key haml-mode-map "\C-j" 'newline-and-indent))
+(defvar haml-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map [backspace] 'haml-electric-backspace)
+    (define-key map "\C-?" 'haml-electric-backspace)
+    (define-key map "\C-\M-f" 'haml-forward-sexp)
+    (define-key map "\C-\M-b" 'haml-backward-sexp)
+    (define-key map "\C-\M-u" 'haml-up-list)
+    (define-key map "\C-\M-d" 'haml-down-list)
+    (define-key map "\C-C\C-k" 'haml-kill-line-and-indent)
+    map))
 
 (define-derived-mode haml-mode fundamental-mode "Haml"
-  "Simple mode to edit Haml.
+  "Major mode for editing Haml files.
 
 \\{haml-mode-map}"
   (set-syntax-table haml-mode-syntax-table)
   (set (make-local-variable 'indent-line-function) 'haml-indent-line)
-  (set (make-local-variable 'font-lock-defaults)
-       '((haml-font-lock-keywords-1)
-         nil
-         t)))
+  (set (make-local-variable 'indent-region-function) 'haml-indent-region)
+  (set (make-local-variable 'forward-sexp-function) 'haml-forward-sexp)
+  (setq font-lock-defaults '((haml-font-lock-keywords) nil t)))
+
+;; Navigation
+
+(defun haml-forward-through-whitespace (&optional backward)
+  "Move the point forward at least one line, until it reaches
+either the end of the buffer or a line with no whitespace.
+
+If `backward' is non-nil, move the point backward instead."
+  (let ((arg (if backward -1 1))
+        (endp (if backward 'bobp 'eobp)))
+    (loop do (forward-line arg)
+          while (and (not (funcall endp))
+                     (looking-at "^[ \t]*$")))))
+
+(defun haml-at-indent-p ()
+  "Returns whether or not the point is at the first
+non-whitespace character in a line or whitespace preceding that
+character."
+  (let ((opoint (point)))
+    (save-excursion
+      (back-to-indentation)
+      (>= (point) opoint))))
+
+(defun haml-forward-sexp (&optional arg)
+  "Move forward across one nested expression.
+With `arg', do it that many times.  Negative arg -N means move
+backward across N balanced expressions.
+
+A sexp in Haml is defined as a line of Haml code as well as any
+lines nested beneath it."
+  (interactive "p")
+  (or arg (setq arg 1))
+  (if (and (< arg 0) (not (haml-at-indent-p)))
+      (back-to-indentation)
+    (while (/= arg 0)
+      (let ((indent (current-indentation)))
+        (loop do (haml-forward-through-whitespace (< arg 0))
+              while (and (not (eobp))
+                         (not (bobp))
+                         (> (current-indentation) indent)))
+        (back-to-indentation)
+      (setq arg (+ arg (if (> arg 0) -1 1)))))))
+
+(defun haml-backward-sexp (&optional arg)
+  "Move backward across one nested expression.
+With ARG, do it that many times.  Negative arg -N means move
+forward across N balanced expressions.
+
+A sexp in Haml is defined as a line of Haml code as well as any
+lines nested beneath it."
+  (interactive "p")
+  (haml-forward-sexp (if arg (- arg) -1)))
+
+(defun haml-up-list (&optional arg)
+  "Move out of one level of nesting.
+With ARG, do this that many times."
+  (interactive "p")
+  (or arg (setq arg 1))
+  (while (> arg 0)
+    (let ((indent (current-indentation)))
+      (loop do (haml-forward-through-whitespace t)
+            while (and (not (bobp))
+                       (>= (current-indentation) indent)))
+      (setq arg (- arg 1))))
+  (back-to-indentation))
+
+(defun haml-down-list (&optional arg)
+  "Move down one level of nesting.
+With ARG, do this that many times."
+  (interactive "p")
+  (or arg (setq arg 1))
+  (while (> arg 0)
+    (let ((indent (current-indentation)))
+      (haml-forward-through-whitespace)
+      (when (<= (current-indentation) indent)
+        (haml-forward-through-whitespace t)
+        (back-to-indentation)
+        (error "Nothing is nested beneath this line"))
+      (setq arg (- arg 1))))
+  (back-to-indentation))
+
+(defun haml-mark-sexp-but-not-next-line ()
+  "Marks the next Haml sexp, but puts the mark at the end of the
+last line of the sexp rather than the first non-whitespace
+character of the next line."
+  (mark-sexp)
+  (set-mark
+   (save-excursion
+     (goto-char (mark))
+     (forward-line -1)
+     (end-of-line)
+     (point))))
 
 ;; Indentation and electric keys
+
+(defun haml-indent-p ()
+  "Returns true if the current line can have lines nested beneath it."
+  (loop for opener in haml-block-openers
+        if (looking-at opener) return t
+        finally return nil))
 
 (defun haml-compute-indentation ()
   "Calculate the maximum sensible indentation for the current line."
   (save-excursion
     (beginning-of-line)
-    (if (bobp) 10
-      (forward-line -1)
-      (while (and (looking-at haml-blank-line-re)
-                  (> (point) (point-min)))
-        (forward-line -1))
+    (if (bobp) 0
+      (haml-forward-through-whitespace t)
       (+ (current-indentation)
-         (if (or (looking-at haml-filter-re)
-                 (looking-at haml-comment-re)
-                 (looking-at haml-html-comment-re)
-                 (looking-at haml-block-cont-re)
-                 (looking-at haml-tag-nest-re)
-                 (looking-at haml-block-re))
-             haml-indent-offset 0)))))
+         (if (funcall haml-indent-function) haml-indent-offset
+           0)))))
+
+(defun haml-indent-region (start end)
+  "Indent each nonblank line in the region.
+This is done by indenting the first line based on
+`haml-compute-indentation' and preserving the relative
+indentation of the rest of the region.
+
+If this command is used multiple times in a row, it will cycle
+between possible indentations."
+  (save-excursion
+    (goto-char end)
+    (setq end (point-marker))
+    (goto-char start)
+    (let (this-line-column current-column
+          (next-line-column
+           (if (and (equal last-command this-command) (/= (current-indentation) 0))
+               (* (/ (- (current-indentation) 1) haml-indent-offset) haml-indent-offset)
+             (haml-compute-indentation))))
+      (while (< (point) end)
+        (setq this-line-column next-line-column
+              current-column (current-indentation))
+        ;; Delete whitespace chars at beginning of line
+        (delete-horizontal-space)
+        (unless (eolp)
+          (setq next-line-column (save-excursion
+                                   (loop do (forward-line 1)
+                                         while (and (not (eobp)) (looking-at "^[ \t]*$")))
+                                   (+ this-line-column
+                                      (- (current-indentation) current-column))))
+          ;; Don't indent an empty line
+          (unless (eolp) (indent-to this-line-column)))
+        (forward-line 1)))
+    (move-marker end nil)))
 
 (defun haml-indent-line ()
   "Indent the current line.
@@ -205,33 +284,46 @@ back-dent the line by `haml-indent-offset' spaces.  On reaching column
       (if (< (current-column) (current-indentation))
           (forward-to-indentation 0))))
 
+(defun haml-reindent-region-by (n)
+  "Add N spaces to the beginning of each line in the region.
+If N is negative, will remove the spaces instead.  Assumes all
+lines in the region have indentation >= that of the first line."
+  (let ((ci (current-indentation)))
+    (save-excursion
+      (replace-regexp (concat "^" (make-string ci ? ))
+                      (make-string (max 0 (+ ci n)) ? )
+                      nil (point) (mark)))))
+
 (defun haml-electric-backspace (arg)
   "Delete characters or back-dent the current line.
-If invoked following only whitespace on a line, will back-dent to the
-immediately previous multiple of `haml-indent-offset' spaces."
+If invoked following only whitespace on a line, will back-dent
+the line and all nested lines to the immediately previous
+multiple of `haml-indent-offset' spaces.
+
+Set `haml-backspace-backdents-nesting' to nil to just back-dent
+the current line."
   (interactive "*p")
-  (if (or (/= (current-indentation) (current-column)) (bolp))
-      (funcall haml-backspace-function arg)
+  (if (or (/= (current-indentation) (current-column))
+          (bolp)
+          (looking-at "^[ \t]+$"))
+      (backward-delete-char arg)
     (let ((ci (current-column)))
       (beginning-of-line)
-      (delete-horizontal-space)
-      (indent-to (* (/ (- ci (* arg haml-indent-offset))
-                       haml-indent-offset)
-                    haml-indent-offset)))))
+      (if haml-backspace-backdents-nesting
+          (haml-mark-sexp-but-not-next-line)
+        (set-mark (save-excursion (end-of-line) (point))))
+      (haml-reindent-region-by (* (- arg) haml-indent-offset))
+      (back-to-indentation)
+      (pop-mark))))
+
+(defun haml-kill-line-and-indent ()
+  "Kill the current line, and re-indent all lines nested beneath it."
+  (interactive)
+  (beginning-of-line)
+  (haml-mark-sexp-but-not-next-line)
+  (kill-line 1)
+  (haml-reindent-region-by (* -1 haml-indent-offset)))
 
 ;; Setup/Activation
 
-(defun haml-mode-version ()
-  "Diplay version of `haml-mode'."
-  (interactive)
-  (message "haml-mode %s" haml-mode-version)
-  haml-mode-version)
-
 (provide 'haml-mode)
-
-(unless (find-if
-         #'(lambda(it) (string= it "\\.haml\\'"))
-         (mapcar 'car auto-mode-alist))
-  (add-to-list 'auto-mode-alist '("\\.haml\\'" . haml-mode)))
-
-;;; haml-mode.el ends here
--- site-lisp/haml/sass-mode.el
+++ site-lisp/haml/sass-mode.el
@@ -1,5 +1,4 @@
 ;;; sass-mode.el -- Major mode for editing Sass files
-;;; Version 0.0.1
 ;;; Written by Nathan Weizenbaum
 
 ;;; Because Sass's indentation schema is similar
@@ -7,12 +6,15 @@
 ;;; functions are similar to those in yaml-mode and python-mode.
 
 ;;; To install, save this somewhere and add the following to your .emacs file:
-;;; 
+;;;
 ;;; (add-to-list 'load-path "/path/to/sass-mode.el")
 ;;; (require 'sass-mode nil 't)
-;;; 
+;;; (add-to-list 'auto-mode-alist '("\\.sass$" . sass-mode))
+;;;
 
 ;;; Code:
+
+(require 'haml-mode)
 
 ;; User definable variables
 
@@ -22,134 +24,65 @@
   :prefix "sass-")
 
 (defcustom sass-mode-hook nil
-  "*Hook run by `sass-mode'."
+  "Hook run when entering Sass mode."
   :type 'hook
   :group 'sass)
 
 (defcustom sass-indent-offset 2
-  "*Amount of offset per level of indentation."
+  "Amount of offset per level of indentation."
   :type 'integer
   :group 'sass)
 
-(defcustom sass-backspace-function 'backward-delete-char-untabify
-  "*Function called by `sass-electric-backspace' when deleting backwards."
-  :type 'function
-  :group 'sass)
+(defvar sass-non-block-openers
+  '("^ *:[^ \t]+[ \t]+[^ \t]"
+    "^ *[^ \t:]+[ \t]*[=:][ \t]*[^ \t]")
+  "A list of regexps that match lines of Sass that couldn't have
+text nested beneath them.")
 
-(defface sass-tab-face
-   '((((class color)) (:background "red" :foreground "red" :bold t))
-     (t (:reverse-video t)))
-  "Face to use for highlighting tabs in Sass files."
-  :group 'faces
-  :group 'sass)
+;; Font lock
 
-;; Helper Functions
-
-(defun string-* (str n)
-  "Concatenates a string with itself n times."
-  (if (= n 0) ""
-    (concat str (string-* str (- n 1)))))
-
-(defun find-if (f lst)
-  "Returns the first element of a list for which a function returns a non-nil value, or nil if no such element is found."
-  (while (not (or (null lst)
-                  (apply f (list (car lst)))))
-    (setq lst (cdr lst)))
-  (if (null lst) nil (car lst)))
-
-(defun sre (str)
-  "Prepends a Sass-tab-matching regexp to str."
-  (concat "^\\(" (string-* " " sass-indent-offset) "\\)*" str))
+(defconst sass-font-lock-keywords
+  '(("^ *\\(\t\\)"                            1 'haml-tab-face)
+    ("^@.*"                                   0 font-lock-constant-face)
+    ("\\(\'[^']*'\\)"                         1 font-lock-string-face append)
+    ("\\(\"[^\"]*\"\\)"                       1 font-lock-string-face append)
+    ("\\(#[0-9a-fA-F]\\{3\\}\\{1,2\\}\\>\\)"  1 font-lock-string-face append)
+    ("\\(:[A-Za-z-]+\\|[A-Za-z-]+:\\)"        0 font-lock-constant-face append)
+    ("![a-z0-9_-]+"                           0 font-lock-variable-name-face append)
+    ("^ *\\(/[/*].*\\)$"                      1 font-lock-comment-face append)
+    ("\\(?:^\\|,\\) *\\(#[a-z0-9_-]+\/?\\)"   1 font-lock-keyword-face)
+    ("\\(?:^\\|,\\) *\\(\\.[a-z0-9_-]+\/?\\)" 1 font-lock-type-face)
+    ("\\(?:^\\|,\\) *\\(&\\|[a-z0-9_]+\/?\\)" 1 font-lock-function-name-face)
+    ("\\([=]\\)"                              0 font-lock-preprocessor-face prepend)
+    ("\\(?:^\\|,\\) *\\(#[a-z0-9_]+\/?\\)"    (1 font-lock-keyword-face)
+     ("\\.[a-z0-9_-]+" nil nil                (0 font-lock-type-face)))
+    ("\\(?:^\\|,\\) *\\(\\.[a-z0-9_]+\/?\\)"  (1 font-lock-type-face)
+     ("\\.[a-z0-9_-]+" nil nil                (0 font-lock-type-face)))
+    ("\\(?:^\\|,\\) *\\(\\.[a-z0-9_]+\/?\\)"  (1 font-lock-type-face)
+     ("\\#[a-z0-9_-]+" nil nil                (0 font-lock-keyword-face)))
+    ("\\(?:^\\|,\\) *\\(&\\|[a-z0-9_]+\/?\\)" (1 font-lock-function-name-face)
+     ("\\.[a-z0-9_-]+" nil nil                (0 font-lock-type-face)))
+    ("\\(?:^\\|,\\) *\\(&\\|[a-z0-9_]+\/?\\)" (1 font-lock-function-name-face)
+     ("\\#[a-z0-9_-]+" nil nil                (0 font-lock-keyword-face)))))
 
 ;; Constants
 
-(defconst sass-mode-version "0.0.1" "Version of `sass-mode.'")
-
-(defconst sass-blank-line-re "^[ \t]*$"
-  "Regexp matching a line containing only whitespace.")
-
-(defconst sass-full-attr-re (sre ":[^ \t]+[ \t]+[^ \t]")
-  "Regexp matching a Sass attribute with content.")
-
 ;; Mode setup
 
-(defvar sass-mode-map ()
-  "Keymap used in `sass-mode' buffers.")
-(if sass-mode-map
-    nil
-  (setq sass-mode-map (make-sparse-keymap))
-  (define-key sass-mode-map [backspace] 'sass-electric-backspace)
-  (define-key sass-mode-map "\C-?" 'sass-electric-backspace)
-  (define-key sass-mode-map "\C-j" 'newline-and-indent))
+(define-derived-mode sass-mode haml-mode "Sass"
+  "Major mode for editing Sass files."
+  (set (make-local-variable 'haml-indent-function) 'sass-indent-p)
+  (set (make-local-variable 'haml-indent-offset) sass-indent-offset)
+  (setq font-lock-defaults '(sass-font-lock-keywords nil t)))
 
-(define-derived-mode sass-mode fundamental-mode "Sass"
-  "Simple mode to edit Sass.
+;; Indentation
 
-\\{sass-mode-map}"
-  (set (make-local-variable 'indent-line-function) 'sass-indent-line))
-
-;; Indentation and electric keys
-
-(defun sass-compute-indentation ()
-  "Calculate the maximum sensible indentation for the current line."
-  (save-excursion
-    (beginning-of-line)
-    (if (bobp) 10
-      (forward-line -1)
-      (while (and (looking-at sass-blank-line-re)
-                  (> (point) (point-min)))
-        (forward-line -1))
-      (+ (current-indentation)
-         (if (not (looking-at sass-full-attr-re))
-             sass-indent-offset 0)))))
-
-(defun sass-indent-line ()
-  "Indent the current line.
-The first time this command is used, the line will be indented to the
-maximum sensible indentation.  Each immediately subsequent usage will
-back-dent the line by `sass-indent-offset' spaces.  On reaching column
-0, it will cycle back to the maximum sensible indentation."
-  (interactive "*")
-  (let ((ci (current-indentation))
-        (cc (current-column))
-        (need (sass-compute-indentation)))
-    (save-excursion
-      (beginning-of-line)
-      (delete-horizontal-space)
-      (if (and (equal last-command this-command) (/= ci 0))
-          (indent-to (* (/ (- ci 1) sass-indent-offset) sass-indent-offset))
-        (indent-to need)))
-      (if (< (current-column) (current-indentation))
-          (forward-to-indentation 0))))
-
-(defun sass-electric-backspace (arg)
-  "Delete characters or back-dent the current line.
-If invoked following only whitespace on a line, will back-dent to the
-immediately previous multiple of `sass-indent-offset' spaces."
-  (interactive "*p")
-  (if (or (/= (current-indentation) (current-column)) (bolp))
-      (funcall sass-backspace-function arg)
-    (let ((ci (current-column)))
-      (beginning-of-line)
-      (delete-horizontal-space)
-      (indent-to (* (/ (- ci (* arg sass-indent-offset))
-                       sass-indent-offset)
-                    sass-indent-offset)))))
+(defun sass-indent-p ()
+  "Returns true if the current line can have lines nested beneath it."
+  (loop for opener in sass-non-block-openers
+        unless (looking-at opener) return t
+        return nil))
 
 ;; Setup/Activation
 
-(defun sass-mode-version ()
-  "Diplay version of `sass-mode'."
-  (interactive)
-  (message "sass-mode %s" sass-mode-version)
-  sass-mode-version)
-
 (provide 'sass-mode)
-
-
-(unless (find-if
-         #'(lambda(it) (string= it "\\.sass\\'"))
-         (mapcar 'car auto-mode-alist))
-  (add-to-list 'auto-mode-alist '("\\.sass\\'" . sass-mode)))
-
-;;; sass-mode.el ends here
