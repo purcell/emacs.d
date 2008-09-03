24de3ac7452107d000996bb7a9e0de7959d201c7
Steve Purcell <steve@sanityinc.com>
2008-09-03 11:43:54 +0200

    Make "C-x |" convert a vertically split window to a horizontally split window

--- init.el
+++ init.el
@@ -405,6 +405,14 @@
 (global-set-key "\C-x2" (split-window-func-with-other-buffer 'split-window-vertically))
 (global-set-key "\C-x3" (split-window-func-with-other-buffer 'split-window-horizontally))
 
+(defun split-window-horizontally-instead ()
+  (interactive)
+  (save-excursion
+    (delete-other-windows)
+    (funcall (split-window-func-with-other-buffer 'split-window-horizontally))))
+
+(global-set-key "\C-x|" 'split-window-horizontally-instead)
+
 
 ;;----------------------------------------------------------------------------
 ;; Desktop saving
