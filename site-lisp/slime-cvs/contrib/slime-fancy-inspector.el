48f0ef16368dd81b87a0cc58aceee7da8f8a119f
Steve Purcell <steve@sanityinc.com>
2008-09-11 10:02:06 +0200

    Import slime as a git submodule instead

--- site-lisp/slime-cvs/contrib/slime-fancy-inspector.el
+++
@@ -1,9 +0,0 @@
-;;; slime-fancy-inspector.el --- Fancy inspector for CLOS objects
-;;
-;; Author: Marco Baringer <mb@bese.it> and others
-;; License: GNU GPL (same license as Emacs)
-;;
-
-(slime-require :swank-fancy-inspector)
-
-(provide 'slime-fancy-inspector)
