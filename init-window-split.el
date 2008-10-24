412d58226086f91b2f0e5a82e1fd0c03da14c157
Steve Purcell <steve@sanityinc.com>
2008-10-24 13:46:48 +0200

    Extract split-window-* functions to a separate init file

---
+++ init-window-split.el
@@ -0,0 +1,17 @@
+(defun split-window-func-with-other-buffer (split-function)
+  (lexical-let ((s-f split-function))
+    (lambda ()
+      (interactive)
+      (funcall s-f)
+      (set-window-buffer (next-window) (other-buffer)))))
+
+(global-set-key "\C-x2" (split-window-func-with-other-buffer 'split-window-vertically))
+(global-set-key "\C-x3" (split-window-func-with-other-buffer 'split-window-horizontally))
+
+(defun split-window-horizontally-instead ()
+  (interactive)
+  (save-excursion
+    (delete-other-windows)
+    (funcall (split-window-func-with-other-buffer 'split-window-horizontally))))
+
+(global-set-key "\C-x|" 'split-window-horizontally-instead)
--- init.el
+++ init.el
@@ -312,24 +312,7 @@
 ;;----------------------------------------------------------------------------
 ;; When splitting window, show (other-buffer) in the new window
 ;;----------------------------------------------------------------------------
-(defun split-window-func-with-other-buffer (split-function)
-  (lexical-let ((s-f split-function))
-    (lambda ()
-      (interactive)
-      (funcall s-f)
-      (set-window-buffer (next-window) (other-buffer)))))
-
-(global-set-key "\C-x2" (split-window-func-with-other-buffer 'split-window-vertically))
-(global-set-key "\C-x3" (split-window-func-with-other-buffer 'split-window-horizontally))
-
-(defun split-window-horizontally-instead ()
-  (interactive)
-  (save-excursion
-    (delete-other-windows)
-    (funcall (split-window-func-with-other-buffer 'split-window-horizontally))))
-
-(global-set-key "\C-x|" 'split-window-horizontally-instead)
-
+(load "init-window-split")
 
 ;;----------------------------------------------------------------------------
 ;; Desktop saving
