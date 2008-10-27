8dbac80489f6edfe2f8330255845add3df81df73
Steve Purcell <steve@sanityinc.com>
2008-10-27 10:52:22 +0100

    Add todochiku for growl notifications when compilation finishes

--- init.el
+++ init.el
@@ -388,6 +388,7 @@
 ;;----------------------------------------------------------------------------
 ;; Compilation
 ;;----------------------------------------------------------------------------
+(require 'todochiku) ;; growl notifications when compilation finishes
 (add-hook 'compilation-mode-hook (lambda () (local-set-key [f6] 'recompile)))
 
 
