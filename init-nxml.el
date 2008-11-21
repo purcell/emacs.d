bb1739109f98fb546b75771d358537ceeed7a699
Steve Purcell <steve@sanityinc.com>
2008-11-21 14:18:59 +0100

    Fix some odd unicode backslashes that sneaked in

--- init-nxml.el
+++ init-nxml.el
@@ -3,7 +3,7 @@
               (cons (concat "\\." (regexp-opt '("xml" "xsd" "sch" "rng" "xslt" "svg" "rss") t) "\\'")
                     'nxml-mode))
 (unify-8859-on-decoding-mode)
-(setq magic-mode-alist (cons '("<＼＼?xml " . nxml-mode) magic-mode-alist))
+(setq magic-mode-alist (cons '("<\\?xml " . nxml-mode) magic-mode-alist))
 (fset 'html-mode 'nxml-mode)
 (fset 'xml-mode 'nxml-mode)
 (add-hook 'nxml-mode-hook (lambda ()
