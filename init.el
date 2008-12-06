f075fb5e0e5a091c0c4f93b829dc2b751a002ff6
Steve Purcell <steve@sanityinc.com>
2008-12-06 09:39:42 +0100

    Bind ns-do-hide-others on Mac, and use kbd to define key combos more elegantly

--- init.el
+++ init.el
@@ -138,11 +138,11 @@
   (setq mouse-wheel-scroll-amount '(0.001))
   (when *is-cocoa-emacs*
     ;; Woohoo!!
-    (global-set-key "\M-`" 'ns-next-frame)
-
-    (global-set-key "\M-h" 'ns-do-hide-emacs)
-    (global-set-key "\M-c" 'ns-copy-including-secondary)
-    (global-set-key "\M-v" 'ns-paste-secondary))
+    (global-set-key (kbd "M-`") 'ns-next-frame)
+    (global-set-key (kbd "M-h") 'ns-do-hide-emacs)
+    (global-set-key (kbd "M-ˍ") 'ns-do-hide-others)  ;; what describe-key reports
+    (global-set-key (kbd "M-c") 'ns-copy-including-secondary)
+    (global-set-key (kbd "M-v") 'ns-paste-secondary))
   ;; Use Apple-w to close current buffer on OS-X (is normally bound to kill-ring-save)
   (global-set-key [(meta w)] 'kill-this-buffer))
 
