590412f3222c327a1a0c9bd40fb042f3ef112580
Steve Purcell <steve@sanityinc.com>
2008-12-21 21:11:55 +0100

    anything: add idle delay before searching exhaustively, and also search emacs functions

--- init-anything.el
+++ init-anything.el
@@ -34,8 +34,10 @@
         anything-c-source-recentf
         anything-c-source-occur
         anything-c-source-files-in-current-dir
-        anything-c-source-emacs-commands))
+        anything-c-source-emacs-commands
+        anything-c-source-emacs-functions))
 
 (setq anything-samewindow nil)
+(setq anything-input-idle-delay 0.05)
 
 (global-set-key [\M-f10] 'anything)
