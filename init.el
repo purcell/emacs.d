a393e19d1ac0b944fe99dd4cc458f55ff228a87a
Steve Purcell <steve@sanityinc.com>
2009-01-17 20:53:25 +0100

    Add count-words function

--- init.el
+++ init.el
@@ -215,6 +215,17 @@
 
 
 ;;----------------------------------------------------------------------------
+;; Easily count words (http://emacs-fu.blogspot.com/2009/01/counting-words.html)
+;;----------------------------------------------------------------------------
+(defun count-words (&optional begin end)
+  "count words between BEGIN and END (region); if no region defined, count words in buffer"
+  (interactive "r")
+  (let ((b (if mark-active begin (point-min)))
+      (e (if mark-active end (point-max))))
+    (message "Word count: %s" (how-many "\\w+" b e))))
+
+
+;;----------------------------------------------------------------------------
 ;; Modeline tweaks
 ;;----------------------------------------------------------------------------
 (size-indication-mode)
