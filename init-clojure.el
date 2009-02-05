65aecc2e11d1cdaa81b32fde70f1e6f3b2f01df5
Steve Purcell <steve@sanityinc.com>
2009-02-05 17:25:26 +0100

    Actually get swank-clojure working with a recent clojure

--- init-clojure.el
+++ init-clojure.el
@@ -1,2 +1,10 @@
-(setq swank-clojure-jar-path (expand-file-name "~/Projects/External/clojure/clojure.jar"))
 (require 'swank-clojure-autoload)
+(swank-clojure-config
+ (setq swank-clojure-jar-path (expand-file-name "~/Projects/External/clojure/clojure.jar")))
+
+(add-auto-mode 'lisp-mode "\\.clj$")
+
+(defun slime-clojure ()
+  "Fire up slime running the swank-clojure backend"
+  (interactive)
+  (slime 'clojure))
