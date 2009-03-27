4ee502e36ffe281bca7bd6f3b43b79a421882e5d
Steve Purcell <steve@sanityinc.com>
2009-03-27 21:40:38 +0100

    Remove local anything-c-source-occur defn, which is now part of anything-config

--- init-anything.el
+++ init-anything.el
@@ -1,34 +1,5 @@
 (require 'anything-config)
 
-;; From http://www.emacswiki.org/cgi-bin/wiki/AnythingSources
-(defvar anything-c-source-occur
-  '((name . "Occur")
-    (init . (lambda ()
-              (setq anything-occur-current-buffer
-                    (current-buffer))))
-    (candidates . (lambda ()
-                    (let ((anything-occur-buffer (get-buffer-create "*Anything Occur*")))
-                      (with-current-buffer anything-occur-buffer
-                        (occur-mode)
-                        (erase-buffer)
-                        (let ((count (occur-engine anything-pattern
-                                                   (list anything-occur-current-buffer) anything-occur-buffer
-                                                   list-matching-lines-default-context-lines case-fold-search
-                                                   list-matching-lines-buffer-name-face
-                                                   nil list-matching-lines-face
-                                                   (not (eq occur-excluded-properties t)))))
-                          (when (> count 0)
-                            (setq next-error-last-buffer anything-occur-buffer)
-                            (cdr (split-string (buffer-string) "\n" t))))))))
-    (action . (("Goto line" . (lambda (candidate)
-                                (with-current-buffer "*Anything Occur*"
-                                  (search-forward candidate))
-                                (goto-line (string-to-number candidate) anything-occur-current-buffer)))))
-    (requires-pattern . 3)
-    (volatile)
-    (delayed)))
-
-
 (setq anything-sources
       '(anything-c-source-buffers
         anything-c-source-recentf
