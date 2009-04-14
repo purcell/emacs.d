68cad0a42660f243976db7171ecbf9540ecf80c6
Steve Purcell <steve@sanityinc.com>
2009-04-14 16:14:15 +0200

    Tidy up rails init file

--- init-rails.el
+++ init-rails.el
@@ -1,13 +1,14 @@
+(require 'rinari)
 (dolist (hook '(nxml-mode-hook haml-mode-hook sass-mode-hook magit-mode-hook))
   (add-hook hook (lambda () (rinari-launch))))
+
 (defun update-rails-ctags ()
   (interactive)
   (let ((default-directory (or (rinari-root) default-directory)))
     (shell-command (concat "ctags -a -e -f " rinari-tags-file-name " --tag-relative -R app lib vendor test"))))
-(require 'rinari)
+
 
 (add-hook 'rails-minor-mode-hook (lambda () (local-set-key [f6] 'recompile)))
-
 
 
 (provide 'init-rails)
