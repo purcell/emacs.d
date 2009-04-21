c71ef4421f39b0b7e48d8066794a30a3c1d73432
Steve Purcell <steve@sanityinc.com>
2009-04-21 11:46:07 +0200

    Add to 'anything-sources, and bind 'anything-at-point instead of 'anything

--- init-anything.el
+++ init-anything.el
@@ -1,17 +1,18 @@
 (require 'anything-config)
 
 (setq anything-sources
-      '(anything-c-source-buffers
+      '(anything-c-source-org-headline
+        anything-c-source-buffers
         anything-c-source-recentf
-        anything-c-source-occur
         anything-c-source-files-in-current-dir
         anything-c-source-emacs-commands
-        anything-c-source-emacs-functions))
+        anything-c-source-emacs-functions
+        anything-c-source-minibuffer-history))
 
 (setq anything-samewindow nil)
-(setq anything-input-idle-delay 0.05)
+(setq anything-input-idle-delay 0.2)
 
-(global-set-key [\M-f10] 'anything)
+(global-set-key [\M-f10] 'anything-at-point) ;; With C-u prefix, starts with symbol at point
 
 
 (provide 'init-anything)
