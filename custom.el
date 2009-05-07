7ed11f41106fa0ff68118fb1cf64a2d7e38fd97f
Steve Purcell <steve@sanityinc.com>
2009-05-07 16:34:22 +0200

    Don't set the colors of the "default" face; rely entirely on color-theme to do this later

--- custom.el
+++ custom.el
@@ -84,7 +84,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(default ((t (:stipple nil :background "black" :foreground "wheat" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 110 :width normal :foundry "apple" :family "Monaco"))))
+ '(default ((t (:slant normal :weight normal :height 110 :width normal :foundry "apple" :family "Monaco"))))
  '(darcsum-need-action-marked-face ((t (:foreground "forestgreen"))))
  '(flymake-errline ((((class color)) (:background "red3"))))
  '(flymake-warnline ((((class color)) (:background "red4"))))
