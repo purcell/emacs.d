603ad75d5b447bbf9e5f76ef80a629be3b6af5d1
Steve Purcell <steve@sanityinc.com>
2009-06-03 11:58:02 +0200

    Clojure setup tweaks

--- init-clojure.el
+++ init-clojure.el
@@ -1,13 +1,11 @@
 (require 'swank-clojure-autoload)
-(swank-clojure-config
- (setq swank-clojure-jar-path (expand-file-name "~/Projects/External/clojure/clojure.jar")))
 
-(add-auto-mode 'lisp-mode "\\.clj$")
+(setq clojure-src-root (expand-file-name "~/Projects/External"))
+(eval-after-load 'clojure-mode '(clojure-slime-config))
 
 (defun slime-clojure ()
   "Fire up slime running the swank-clojure backend"
   (interactive)
   (slime 'clojure))
-
 
 (provide 'init-clojure)
