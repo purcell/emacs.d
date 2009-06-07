904c3754fe5636fe60c0b28437b51e6a76be99a8
Steve Purcell <steve@sanityinc.com>
2009-06-07 23:44:50 +0200

    Add remap bindings for ctrl + arrow keys in Apple's Terminal.app

--- init.el
+++ init.el
@@ -129,7 +129,11 @@ in `exec-path', or nil if no such command exists"
   (define-key function-key-map "\e[1;5A" [C-up])
   (define-key function-key-map "\e[1;5B" [C-down])
   (define-key function-key-map "\e[1;5C" [C-right])
-  (define-key function-key-map "\e[1;5D" [C-left]))
+  (define-key function-key-map "\e[1;5D" [C-left]
+  (define-key function-key-map "\e[5A"   [C-up])
+  (define-key function-key-map "\e[5B"   [C-down])
+  (define-key function-key-map "\e[5C"   [C-right])
+  (define-key function-key-map "\e[5D"   [C-left]))
 
 (add-hook 'after-make-console-frame-hooks
           (lambda ()
