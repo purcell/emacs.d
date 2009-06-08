dc541f11e798111cda80e131e56d8561f038cdd5
Steve Purcell <steve@sanityinc.com>
2009-06-08 11:35:56 +0200

    Fix typo in init.el

--- init.el
+++ init.el
@@ -129,7 +129,7 @@ in `exec-path', or nil if no such command exists"
   (define-key function-key-map "\e[1;5A" [C-up])
   (define-key function-key-map "\e[1;5B" [C-down])
   (define-key function-key-map "\e[1;5C" [C-right])
-  (define-key function-key-map "\e[1;5D" [C-left]
+  (define-key function-key-map "\e[1;5D" [C-left])
   (define-key function-key-map "\e[5A"   [C-up])
   (define-key function-key-map "\e[5B"   [C-down])
   (define-key function-key-map "\e[5C"   [C-right])
