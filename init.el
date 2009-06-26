40b80f6e2fcd34e919ab0f12baed207ce457c0db
Steve Purcell <steve@sanityinc.com>
2009-06-26 10:09:28 +0200

    Stop C-u from shadowing prefix-arg in viper-mode -- I always use C-b/C-f to scroll anyway

--- init.el
+++ init.el
@@ -244,6 +244,9 @@ in `exec-path', or nil if no such command exists"
   (define-key viper-insert-global-user-map (kbd "C-n") 'dabbrev-expand)
   (define-key viper-insert-global-user-map (kbd "C-p") 'dabbrev-expand)
 
+  ;; Stop C-u from clobbering prefix-arg -- I always use C-b/C-f to scroll
+  (define-key viper-vi-basic-map "\C-u" nil)
+
   ;; Vim-style searching of the symbol at point, made easy by highlight-symbol
   (autoload 'highlight-symbol-next "highlight-symbol" "Highlight symbol at point")
   (autoload 'highlight-symbol-prev "highlight-symbol" "Highlight symbol at point")
