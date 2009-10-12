9cac1af731c890ac034b2d871052fd6e5ba27842
Steve Purcell <steve@sanityinc.com>
2009-10-12 22:05:45 +0100

    Colourise CSS colour literals so that, e.g., #0f0 appears in green

--- init-css.el
+++ init-css.el
@@ -28,4 +28,48 @@
      (dolist (mode (list 'html-mode 'nxml-mode))
        (mmm-add-mode-ext-class mode "\\.r?html\\(\\.erb\\)?$" 'html-css))))
 
+
+
+;; Colourise CSS colour literals
+;; Inspired by http://xahlee.org/emacs/emacs_html.html, but much extended
+
+(require 'hexl)
+(defun choose-contrasting-colour (hex-colour)
+  (if (> (hexl-hex-string-to-integer (substring hex-colour 1))
+         (hexl-hex-string-to-integer "777777"))
+      "#000000"
+    "#ffffff"))
+
+(defun three-hex-colour-to-six (three)
+  (let ((six "#"))
+    (progn
+      (dolist (c (string-to-list (substring three 1)))
+        (setq six (concat six (string c c))))
+      six)))
+
+(defun hexcolour-font-lock-face (hexcolour)
+  (list :background hexcolour :foreground (choose-contrasting-colour hexcolour)))
+
+(setq hexcolour-keywords
+  '(("#[abcdef[:digit:]]\\{3\\}"
+     (0 (put-text-property
+         (match-beginning 0)
+         (match-end 0)
+         'face (hexcolour-font-lock-face
+                (three-hex-colour-to-six (match-string-no-properties 0))))))
+    ("#[abcdef[:digit:]]\\{6\\}"
+     (0 (put-text-property
+         (match-beginning 0)
+         (match-end 0)
+         'face (hexcolour-font-lock-face
+                (match-string-no-properties 0)))))))
+
+(defun hexcolour-add-to-font-lock ()
+  (font-lock-add-keywords nil hexcolour-keywords))
+
+(add-hook 'css-mode-hook 'hexcolour-add-to-font-lock)
+(add-hook 'html-mode-hook 'hexcolour-add-to-font-lock)
+(add-hook 'sass-mode-hook 'hexcolour-add-to-font-lock)
+
+
 (provide 'init-css)
