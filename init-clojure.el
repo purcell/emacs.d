4ad34c6ce5d0776c78439f8dea05f20883898332
Steve Purcell <steve@sanityinc.com>
2009-12-03 14:41:11 +0000

    Simplify init-clojure.el now that I'm using technomancy's enhanced swank-clojure

--- init-clojure.el
+++ init-clojure.el
@@ -1,24 +1,6 @@
-(add-hook 'clojure-mode-hook 'pretty-lambdas)
 (add-hook 'clojure-mode-hook (lambda () (enable-paredit clojure-mode-map)))
 
-
 (setq clojure-src-root (expand-file-name "~/Projects/External"))
-
-(setq swank-clojure-jar-path (concat clojure-src-root "/clojure/clojure.jar")
-      swank-clojure-extra-vm-args (list "-Xmx1024m")
-      swank-clojure-extra-classpaths (list (concat clojure-src-root "/clojure-contrib/clojure-contrib.jar")))
-
-(require 'swank-clojure-autoload)
-
-(eval-after-load "slime"
-  '(progn
-     ;; Ensure we get a REPL
-     (slime-setup '(slime-repl))))
-
-
-(defun slime-clojure ()
-  "Fire up slime running the swank-clojure backend"
-  (interactive)
-  (slime 'clojure))
+(setq swank-clojure-extra-vm-args (list "-Xmx1024m"))
 
 (provide 'init-clojure)
