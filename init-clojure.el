4951b5261d253bc09ef50a0137b8b2d1fcc19345
Steve Purcell <steve@sanityinc.com>
2009-12-20 17:08:20 +0000

    If using viper, force enabling in clojure-mode

--- init-clojure.el
+++ init-clojure.el
@@ -3,4 +3,7 @@
 (setq clojure-src-root (expand-file-name "~/Projects/External"))
 (setq swank-clojure-extra-vm-args (list "-Xmx1024m"))
 
+(when *vi-emulation-support-enabled*
+  (add-to-list 'viper-vi-state-mode-list 'clojure-mode))
+
 (provide 'init-clojure)
