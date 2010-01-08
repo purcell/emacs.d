c1b1b7f98565c55c0a7b0ee30c02a9327373d88e
Steve Purcell <steve@sanityinc.com>
2010-01-08 11:03:46 +0000

    Tweak swank-clojure-extra-vm-args for performance

--- init-clojure.el
+++ init-clojure.el
@@ -1,7 +1,11 @@
 (add-hook 'clojure-mode-hook (lambda () (enable-paredit clojure-mode-map)))
 
 (setq clojure-src-root (expand-file-name "~/Projects/External"))
-(setq swank-clojure-extra-vm-args (list "-Xmx1024m"))
+(setq swank-clojure-extra-vm-args (list "-server"
+                                        "-Xmx1024m"
+                                        "-XX:+UseConcMarkSweepGC"
+                                        "-XX:+UseCompressedOops"
+                                        "-XX:+DoEscapeAnalysis"))
 
 (when *vi-emulation-support-enabled*
   (add-to-list 'viper-vi-state-mode-list 'clojure-mode))
