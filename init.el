cbde902d7282d5a534e6074ba00b9b21bc4f550f
Steve Purcell <steve@sanityinc.com>
2010-01-25 11:25:41 +0000

    Make C-w and M-w act on the entire current line if the mark is not set

--- init.el
+++ init.el
@@ -262,6 +262,26 @@ in `exec-path', or nil if no such command exists"
 (global-set-key [M-S-down] 'move-text-down)
 
 
+
+;;----------------------------------------------------------------------------
+;; Cut/copy the current line if no region is active
+;;----------------------------------------------------------------------------
+(defadvice kill-ring-save (before slick-copy activate compile) "When called
+  interactively with no active region, copy a single line instead."
+  (interactive (if mark-active (list (region-beginning) (region-end))
+                 (message "Copied line")
+                 (list (line-beginning-position)
+                       (line-beginning-position 2)))))
+
+(defadvice kill-region (before slick-cut activate compile)
+  "When called interactively with no active region, kill a single line instead."
+  (interactive
+    (if mark-active (list (region-beginning) (region-end))
+      (message "Killed line")
+      (list (line-beginning-position)
+        (line-beginning-position 2)))))
+
+
 ;;----------------------------------------------------------------------------
 ;; OS X usability tweaks
 ;;----------------------------------------------------------------------------
