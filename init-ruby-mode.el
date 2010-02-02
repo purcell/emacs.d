c2e3657e40f954aaa89a4950509ea50228333c26
Steve Purcell <steve@sanityinc.com>
2010-02-02 10:35:12 +0000

    Move haml/sass config to a separate init file

--- init-ruby-mode.el
+++ init-ruby-mode.el
@@ -78,20 +78,6 @@
 
 
 ;;----------------------------------------------------------------------------
-;; Ruby - haml & sass
-;;----------------------------------------------------------------------------
-(add-auto-mode 'haml-mode "\.haml$")
-(add-auto-mode 'sass-mode "\.sass$")
-(autoload 'haml-mode "haml-mode" "Mode for editing haml files" t)
-(autoload 'sass-mode "sass-mode" "Mode for editing sass files" t)
-
-(require 'flymake-haml)
-(add-hook 'haml-mode-hook 'flymake-haml-load)
-(add-hook 'sass-mode-hook 'flymake-sass-load)
-
-(add-hook 'sass-mode-hook 'ac-css-keywords-setup)
-
-;;----------------------------------------------------------------------------
 ;; Ruby - compilation
 ;;----------------------------------------------------------------------------
 
