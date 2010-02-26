9ca2c2886b4a62cf119c22340e5c5e3a7be68d90
Steve Purcell <steve@sanityinc.com>
2010-02-26 13:23:31 +0000

    Make frame maximisation fns safer for terminal frames

--- init-maxframe.el
+++ init-maxframe.el
@@ -7,20 +7,26 @@
   (setq mf-max-height 900)
   (setq mf-offset-x 0))
 
+(defmacro with-selected-frame (frame &rest forms)
+  (let ((prev-frame (gensym)))
+    `(progn
+       (let ((,prev-frame (selected-frame)))
+         (select-frame (or ,frame (selected-frame)))
+         (unwind-protect
+             (progn ,@forms)
+           (select-frame ,prev-frame))))))
+
 (defun maybe-maximize-frame (&optional frame)
-  (let* ((prev-frame (selected-frame))
-         (f (or frame prev-frame)))
-    (select-frame f)
-    (prog1
-        (if window-system (maximize-frame))
-      (select-frame prev-frame))))
+  (with-selected-frame frame
+    (if window-system (maximize-frame))))
 
 (add-hook 'after-make-frame-functions 'maybe-maximize-frame)
 (add-hook 'after-init-hook 'maybe-maximize-frame)
 
 (defun maximized-p (&optional frame)
-  (and (<= (abs (- (mf-max-display-pixel-width) (frame-pixel-width frame))) (frame-char-width frame))
-       (<= (abs (- (mf-max-display-pixel-height) (+ mf-display-padding-height (frame-pixel-height frame)))) (+ 5 (frame-char-height frame)))))
+  (or (not (with-selected-frame frame window-system))
+      (and (<= (abs (- (mf-max-display-pixel-width) (frame-pixel-width frame))) (frame-char-width frame))
+           (<= (abs (- (mf-max-display-pixel-height) (+ mf-display-padding-height (frame-pixel-height frame)))) (+ 5 (frame-char-height frame))))))
 
 
 (provide 'init-maxframe)
