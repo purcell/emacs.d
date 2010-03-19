cedca8b4eac60ab5e80f73446e0a58e265f8da5e
Steve Purcell <steve@sanityinc.com>
2010-03-19 21:42:00 +0000

    Add michalmarczyk's code for making Clojure REPLs look/feel nicer

--- init-clojure.el
+++ init-clojure.el
@@ -7,6 +7,47 @@
                                         "-XX:+UseCompressedOops"
                                         "-XX:+DoEscapeAnalysis"))
 
+;;------
+;; Patch from michalmarczyk, http://gist.github.com/337280
+
+;;; all code in this function lifted from the clojure-mode function
+;;; from clojure-mode.el
+(defun clojure-font-lock-setup ()
+  (interactive)
+  (set (make-local-variable 'lisp-indent-function)
+       'clojure-indent-function)
+  (set (make-local-variable 'lisp-doc-string-elt-property)
+       'clojure-doc-string-elt)
+  (set (make-local-variable 'font-lock-multiline) t)
+
+  (add-to-list 'font-lock-extend-region-functions
+               'clojure-font-lock-extend-region-def t)
+
+  (when clojure-mode-font-lock-comment-sexp
+    (add-to-list 'font-lock-extend-region-functions
+                 'clojure-font-lock-extend-region-comment t)
+    (make-local-variable 'clojure-font-lock-keywords)
+    (add-to-list 'clojure-font-lock-keywords
+                 'clojure-font-lock-mark-comment t)
+    (set (make-local-variable 'open-paren-in-column-0-is-defun-start) nil))
+
+  (setq font-lock-defaults
+        '(clojure-font-lock-keywords    ; keywords
+          nil nil
+          (("+-*/.<>=!?$%_&~^:@" . "w")) ; syntax alist
+          nil
+          (font-lock-mark-block-function . mark-defun)
+          (font-lock-syntactic-face-function
+           . lisp-font-lock-syntactic-face-function))))
+
+(add-hook 'slime-repl-mode-hook
+          (lambda ()
+            (font-lock-mode nil)
+            (clojure-font-lock-setup)
+            (font-lock-mode t)))
+;;------
+
+
 (defun slime-clojure-repl-setup ()
   (when (string-equal "clojure" (slime-connection-name))
     (message "Setting up repl for clojure")
