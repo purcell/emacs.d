c521b625603594291f1baa3576d4bc338600951b
Steve Purcell <steve@sanityinc.com>
2010-05-07 14:22:32 +0100

    Remove vc-git.el in favour of version bundled with recent Emacsen, and git.el in favour of magit

--- init-git.el
+++ init-git.el
@@ -2,7 +2,6 @@
 ;; Downloaded from http://git.kernel.org/?p=git/git.git ;a=tree;hb=HEAD;f=contrib/emacs
 (require 'vc-git)
 (when (featurep 'vc-git) (add-to-list 'vc-handled-backends 'git))
-(require 'git)
 (autoload 'git-blame-mode "git-blame" "Minor mode for incremental blame for Git." t)
 
 
