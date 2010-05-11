f3094d659e2b4409a1fab1119b50895c3a6cb9ba
Steve Purcell <steve@sanityinc.com>
2010-05-11 17:35:59 +0100

    Simplify hooks for clojure font-locking in repl now that clojure-mode-font-lock-setup is in clojure-mode

--- init-clojure.el
+++ init-clojure.el
@@ -28,40 +28,6 @@
 ;;------
 ;; Patch from michalmarczyk, http://gist.github.com/337280
 
-;;; all code in this function lifted from the clojure-mode function
-;;; from clojure-mode.el
-(defun clojure-font-lock-setup ()
-  (interactive)
-  (set (make-local-variable 'lisp-indent-function)
-       'clojure-indent-function)
-  (set (make-local-variable 'lisp-doc-string-elt-property)
-       'clojure-doc-string-elt)
-  (set (make-local-variable 'font-lock-multiline) t)
-
-  (add-to-list 'font-lock-extend-region-functions
-               'clojure-font-lock-extend-region-def t)
-
-  (when clojure-mode-font-lock-comment-sexp
-    (add-to-list 'font-lock-extend-region-functions
-                 'clojure-font-lock-extend-region-comment t)
-    (make-local-variable 'clojure-font-lock-keywords)
-    (add-to-list 'clojure-font-lock-keywords
-                 'clojure-font-lock-mark-comment t)
-    (set (make-local-variable 'open-paren-in-column-0-is-defun-start) nil))
-
-  (setq font-lock-defaults
-        '(clojure-font-lock-keywords    ; keywords
-          nil nil
-          (("+-*/.<>=!?$%_&~^:@" . "w")) ; syntax alist
-          nil
-          (font-lock-mark-block-function . mark-defun)
-          (font-lock-syntactic-face-function
-           . lisp-font-lock-syntactic-face-function))))
-
-(add-hook 'slime-repl-mode-hook
-          (lambda ()
-            (clojure-font-lock-setup)))
-
 (defadvice slime-repl-emit (after sr-emit-ad activate)
   (with-current-buffer (slime-output-buffer)
     (add-text-properties slime-output-start slime-output-end
@@ -84,6 +50,7 @@
 (defun slime-clojure-repl-setup ()
   (when (string-equal "clojure" (slime-connection-name))
     (message "Setting up repl for clojure")
+    (clojure-mode-font-lock-setup)
     (when (slime-inferior-process)
       (slime-redirect-inferior-output))
     (set-syntax-table clojure-mode-syntax-table) ;; Tell paredit about [ and {
