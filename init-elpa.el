d7405cc774133a7ada1758f280de1dc11195df08
Steve Purcell <steve@sanityinc.com>
2010-08-11 11:17:12 +0100

    Add Tom Tromey's ELPA package archive when using Emacs 24, since the new gnu.org archive is almost empty

--- init-elpa.el
+++ init-elpa.el
@@ -1,4 +1,7 @@
 (require 'package)
 (package-initialize)
 
+(when (> emacs-major-version 23)
+  (setq package-archives (cons '("tromey" . "http://tromey.com/elpa/") package-archives)))
+
 (provide 'init-elpa)
