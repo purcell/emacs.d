ae4792d38dfcbe877257d859708b8b13ffe1e79c
Steve Purcell <steve@sanityinc.com>
2010-10-10 09:18:13 +0100

    Update README to reflect move to el-get

--- init-el-get.el
+++ init-el-get.el
@@ -1,5 +1,5 @@
 (when (not (load "~/.emacs.d/el-get/el-get/el-get.el" t))
-  (error "Please bootstrap el-get using the instructions here: http://github.com/dimitri/el-get/"))
+  (error "Please bootstrap el-get using the instructions here: http://github.com/dimitri/el-get/, then restart Emacs"))
 
 (setq el-get-byte-compile nil
       el-get-sources
