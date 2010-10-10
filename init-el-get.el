f6546d01da0a2b0e0d82d4eb9244ed8300f5a291
Steve Purcell <steve@sanityinc.com>
2010-10-10 09:18:57 +0100

    Run el-get synchronously on start-up, and provide el-get-update-all

--- init-el-get.el
+++ init-el-get.el
@@ -129,6 +129,14 @@
         (:name lua-mode :type elpa)
 ))
 
-(el-get)
+(defun el-get-update-all ()
+  "Update all el-get packages"
+  (interactive)
+  (dolist (package (mapcar 'el-get-source-name el-get-sources))
+    (unless (memq (plist-get (el-get-package-def package) :type) '(http-tar elpa))
+      (el-get-update package))))
+
+
+(el-get 'sync)
 
 (provide 'init-el-get)
