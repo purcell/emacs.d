02952baf579ffafbad010fd8bae466044b6d276b
Steve Purcell <steve@sanityinc.com>
2011-02-05 10:30:14 +0000

    Add marmalade to package archives list

--- init-elpa.el
+++ init-elpa.el
@@ -1,5 +1,10 @@
 (eval-after-load "package"
-  '(when (> emacs-major-version 23)
-     (setq package-archives (cons '("tromey" . "http://tromey.com/elpa/") package-archives))))
+  '(progn
+     (when (> emacs-major-version 23)
+       (add-to-list 'package-archives '("tromey" . "http://tromey.com/elpa/")))
+
+     (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+     (add-to-list 'package-archives '("technomancy" . "http://repo.technomancy.us/emacs"))
+))
 
 (provide 'init-elpa)
