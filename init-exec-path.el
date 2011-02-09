473354aaf08b861730b128e1c144ad7a5b370c8f
Steve Purcell <steve@sanityinc.com>
2011-02-09 11:23:20 +0000

    Trim trailing whitespace when querying shell for $PATH

--- init-exec-path.el
+++ init-exec-path.el
@@ -1,5 +1,8 @@
 (defun set-exec-path-from-shell-PATH ()
-  (let ((path-from-shell (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'")))
+  (let ((path-from-shell (replace-regexp-in-string
+                          "[ \t\n]*$"
+                          ""
+                          (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
     (setenv "PATH" path-from-shell)
     (setq exec-path (split-string path-from-shell path-separator))))
 
