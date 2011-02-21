d394af7e9a8e9015e3f5cda9de8080ff3bd09154
Steve Purcell <steve@sanityinc.com>
2011-02-21 13:38:28 +0000

    Drop reference to technomancy's defunct elpa archive

--- init-elpa.el
+++ init-elpa.el
@@ -2,9 +2,7 @@
   '(progn
      (when (> emacs-major-version 23)
        (add-to-list 'package-archives '("tromey" . "http://tromey.com/elpa/")))
-
-     (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-     (add-to-list 'package-archives '("technomancy" . "http://repo.technomancy.us/emacs"))
+    (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 ))
 
 (provide 'init-elpa)
