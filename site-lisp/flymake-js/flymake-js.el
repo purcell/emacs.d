65abb183bb714ce2c06a224ae28d041a4e0fc4a4
Steve Purcell <steve@sanityinc.com>
2011-02-26 22:17:39 +0000

    Require flymake explicitly before initializing flymake-js

--- site-lisp/flymake-js/flymake-js.el
+++ site-lisp/flymake-js/flymake-js.el
@@ -25,6 +25,7 @@
 
 (defun flymake-js-load ()
   (interactive)
+  (require 'flymake)
   (defadvice flymake-post-syntax-check (before flymake-force-check-was-interrupted)
     (setq flymake-check-was-interrupted t))
   (ad-activate 'flymake-post-syntax-check)
