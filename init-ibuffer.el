28bf3923c87def89274d5951b133e2cdad48a9a0
Steve Purcell <steve@sanityinc.com>
2011-03-22 11:27:15 +0000

    By default, group entries in ibuffer list by vc directory

--- init-ibuffer.el
+++ init-ibuffer.el
@@ -1,5 +1,44 @@
 (require 'ibuffer)
 
+(require 'vc-hooks)
+(defun vc-find-any-root (file-name)
+  (let ((root nil))
+    (loop for dir in '(".git" ".svn" "CVS" ".bzr" "_darcs")
+          do (setq root (vc-find-root file-name dir))
+          until root
+          finally return root)))
+
+(defun ibuffer-vc-root (buf)
+  (let* ((file (buffer-local-value 'buffer-file-name buf))
+         (dir (buffer-local-value 'default-directory buf))
+         (ref (or file dir)))
+    (vc-find-any-root ref)))
+
+(define-ibuffer-filter vc-root
+    "Toggle current view to buffers with vc root dir QUALIFIER."
+  (:description "vc root dir"
+                :reader (read-from-minibuffer "Filter by vc root dir (regexp): "))
+  (ibuffer-awhen (ibuffer-vc-root buf)
+    (message "Comparing root %s against qualifier %s" it qualifier)
+    (string-match (expand-file-name qualifier) (expand-file-name it))))
+
+(defun ibuffer-generate-filter-groups-by-vc-root ()
+  "Create a set of ibuffer filter groups based on the vc root dirs of buffers"
+  (mapcar (lambda (vc-dir)
+                  (cons (format "%s" vc-dir) `((vc-root . ,vc-dir))))
+                (ibuffer-remove-duplicates
+                 (delq nil (mapcar 'ibuffer-vc-root (buffer-list))))))
+
+(defun ibuffer-set-filter-groups-by-vc-root ()
+  "Set the current filter groups to filter by vc root dir."
+  (interactive)
+  (setq ibuffer-filter-groups (ibuffer-generate-filter-groups-by-vc-root))
+  (ibuffer-update nil t))
+
+(defun ibuffer-set-up-preferred-filters ()
+  (ibuffer-set-filter-groups-by-vc-root))
+(add-hook 'ibuffer-mode-hook 'ibuffer-set-up-preferred-filters)
+
 
 
 ;; (setq ibuffer-saved-filter-groups
@@ -12,10 +51,6 @@
 ;;                  (mode . mail-mode)
 ;;                  ;; etc.; all your mail related modes
 ;;                  ))
-;;                ("MyProject1"
-;;                 (filename . "src/myproject1/"))
-;;                ("MyProject2"
-;;                 (filename . "src/myproject2/"))
 ;;                ("Programming" ;; prog stuff not already in MyProjectX
 ;;                 (or
 ;;                  (mode . c-mode)
