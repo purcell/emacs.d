56f3d354bbb84e6eadfe31e089645c27e88f9b1e
Steve Purcell <steve@sanityinc.com>
2011-04-18 14:13:17 +0100

    Fix size-h ibuffer column definition borrowed from elsewhere, which was incorrect

--- init-ibuffer.el
+++ init-ibuffer.el
@@ -14,8 +14,8 @@
 (define-ibuffer-column size-h
   (:name "Size" :inline t)
   (cond
-   ((> (buffer-size) 1000) (format "%7.1fk" (/ (buffer-size) 1000.0)))
    ((> (buffer-size) 1000000) (format "%7.1fM" (/ (buffer-size) 1000000.0)))
+   ((> (buffer-size) 1000) (format "%7.1fk" (/ (buffer-size) 1000.0)))
    (t (format "%8d" (buffer-size)))))
 
 
