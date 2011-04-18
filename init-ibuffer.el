bfd24dd7b77ffee7f46b98145c9975e24850042d
Steve Purcell <steve@sanityinc.com>
2011-04-18 14:12:36 +0100

    Remove some unused commented-out code

--- init-ibuffer.el
+++ init-ibuffer.el
@@ -33,31 +33,6 @@
               filename-and-process)))
 
 
-;; (setq ibuffer-saved-filter-groups
-;;       (quote (("default"
-;;                ("Org" ;; all org-related buffers
-;;                 (mode . org-mode))
-;;                ("Mail"
-;;                 (or ;; mail-related buffers
-;;                  (mode . message-mode)
-;;                  (mode . mail-mode)
-;;                  ;; etc.; all your mail related modes
-;;                  ))
-;;                ("Programming" ;; prog stuff not already in MyProjectX
-;;                 (or
-;;                  (mode . c-mode)
-;;                  (mode . perl-mode)
-;;                  (mode . python-mode)
-;;                  (mode . emacs-lisp-mode)
-;;                  ;; etc
-;;                  ))
-;;                ("ERC"   (mode . erc-mode))))))
-
-;; (add-hook 'ibuffer-mode-hook
-;;   (lambda ()
-;;     (ibuffer-switch-to-saved-filter-groups "default")))
-
-
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 
 (provide 'init-ibuffer)
