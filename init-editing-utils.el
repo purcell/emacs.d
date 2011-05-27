c9f1a6bdff3a3d121be09323609c8e2e46aed340
Steve Purcell <steve@sanityinc.com>
2011-05-27 14:33:19 +0100

    Add Xah Lee's unfill-paragraph and unfill-region

--- init-editing-utils.el
+++ init-editing-utils.el
@@ -204,4 +204,23 @@
 (diminish 'fuzzy-format-mode)
 
 
+;;----------------------------------------------------------------------------
+;; Unfill regions or paragraphs (see http://xahlee.org/emacs/emacs_unfill-paragraph.html)
+;;----------------------------------------------------------------------------
+(defun unfill-paragraph ()
+  "Replace newline chars in current paragraph by single spaces.
+This command does the reverse of `fill-paragraph'."
+  (interactive)
+  (let ((fill-column 90002000))
+    (fill-paragraph nil)))
+
+(defun unfill-region (start end)
+  "Replace newline chars in region by single spaces.
+This command does the reverse of `fill-region'."
+  (interactive "r")
+  (let ((fill-column 90002000))
+    (fill-region start end)))
+
+
+
 (provide 'init-editing-utils)
