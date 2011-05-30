b9bdafe48c185061974cd8286d82f93adc3f81c3
Steve Purcell <steve@sanityinc.com>
2011-05-30 23:15:26 +0100

    Add sort-lines-random
    http://stackoverflow.com/questions/6172054/how-can-i-random-sort-lines-in-a-buffer/6174107#6174107

--- init-editing-utils.el
+++ init-editing-utils.el
@@ -222,4 +222,20 @@ This command does the reverse of `fill-region'."
 
 
 
+;;----------------------------------------------------------------------------
+;; Random line sorting
+;;----------------------------------------------------------------------------
+(defun sort-lines-random (beg end)
+  "Sort lines in region randomly."
+  (interactive "r")
+  (save-excursion
+    (save-restriction
+      (narrow-to-region beg end)
+      (goto-char (point-min))
+      (let ;; To make `end-of-line' and etc. to ignore fields.
+          ((inhibit-field-text-motion t))
+        (sort-subr nil 'forward-line 'end-of-line nil nil
+                   (lambda (s1 s2) (eq (random 2) 0)))))))
+
+
 (provide 'init-editing-utils)
