f5775c30f038d429d4869c38b4b448d186e2eaa4
Steve Purcell <steve@sanityinc.com>
2011-05-31 17:03:25 +0100

    Add missing quote

--- init-osx-keys.el
+++ init-osx-keys.el
@@ -9,7 +9,7 @@
     (global-set-key (kbd "M-`") 'ns-next-frame)
     (global-set-key (kbd "M-h") 'ns-do-hide-emacs)
     (eval-after-load "nxml-mode"
-      (define-key nxml-mode-map (kbd "M-h") nil))
+      '(define-key nxml-mode-map (kbd "M-h") nil))
     (global-set-key (kbd "M-ˍ") 'ns-do-hide-others) ;; what describe-key reports
     (global-set-key (kbd "M-c") 'ns-copy-including-secondary)
     (global-set-key (kbd "M-v") 'ns-paste-secondary))
