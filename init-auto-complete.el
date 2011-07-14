e265760857254f95205697083d117406e5aa6877
Steve Purcell <steve@sanityinc.com>
2011-07-14 14:26:07 +0100

    Tweak auto-complete config

--- init-auto-complete.el
+++ init-auto-complete.el
@@ -2,7 +2,7 @@
 (require 'auto-complete-config)
 (global-auto-complete-mode t)
 (setq ac-auto-start nil)
-(setq ac-dwim t)
+(setq ac-dwim nil) ; To get pop-ups with docs even if a word is uniquely completed
 (define-key ac-completing-map (kbd "C-n") 'ac-next)
 (define-key ac-completing-map (kbd "C-p") 'ac-previous)
 
