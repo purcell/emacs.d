81854a9520c8dbb70071e6b7aa3aba121225b554
Steve Purcell <steve@sanityinc.com>
2011-07-27 13:57:24 +0100

    Fontify email addresses in code just like links

--- init-misc.el
+++ init-misc.el
@@ -5,7 +5,7 @@
 (fset 'yes-or-no-p 'y-or-n-p)
 (add-hook 'find-file-hooks 'goto-address-prog-mode)
 (add-hook 'after-save-hook 'executable-make-buffer-file-executable-if-script-p)
-
+(setq goto-address-mail-face 'link)
 
 (autoload 'mwe:log-keyboard-commands "mwe-log-commands"
   "Log commands executed in the current buffer" t)
