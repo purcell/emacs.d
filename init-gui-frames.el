9081c30370d17c283eaa7a603387f36d5c422979
Steve Purcell <steve@sanityinc.com>
2011-08-14 09:13:08 +0100

    Use inhibit-startup-echo-area-message to quieten startup

--- init-gui-frames.el
+++ init-gui-frames.el
@@ -14,6 +14,7 @@
 (setq use-file-dialog nil)
 (setq use-dialog-box nil)
 (setq inhibit-startup-screen t)
+(setq inhibit-startup-echo-area-message t)
 
 
 ;;----------------------------------------------------------------------------
