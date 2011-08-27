b55ee0083128cc25e7be2d674742c10dc760db01
Steve Purcell <steve@sanityinc.com>
2011-08-27 09:12:53 +0100

    When switching between Emacs 23 and 24, we always use the bundled package.el in Emacs 24

--- init-elpa.el
+++ init-elpa.el
@@ -3,6 +3,12 @@
   (or (package-installed-p package min-version)
       (package-install package)))
 
+;; When switching between Emacs 23 and 24, we always use the bundled package.el in Emacs 24
+(let ((package-el-site-lisp-dir (expand-file-name "~/.emacs.d/site-lisp/package")))
+  (when (and (file-directory-p package-el-site-lisp-dir)
+             (> emacs-major-version 23))
+    (message "Removing local package.el from load-path to avoid shadowing bundled version")
+    (setq load-path (remove package-el-site-lisp-dir load-path))))
 
 (require 'package)
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
