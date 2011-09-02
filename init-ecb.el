9941c013dd25291bf3f9bac9ede98e24e5ebdc9b
chen bin <chenbin.sh@gmail.com>
2011-09-02 23:52:05 +0800

    remove cedet and ecb

--- init-ecb.el
+++
@@ -1,5 +0,0 @@
-(load-library "ecb")
-(global-set-key (kbd "C-x C-e") 'ecb-activate)
-(global-set-key (kbd "C-x C-q") 'ecb-deactivate)
-
-(provide 'init-ecb)
--- site-lisp/cedet/tests/cit-test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-EMACS=${EMACS:-emacs};
-
-rm -r /tmp/CEDET_INTEG*
-$EMACS -q -l ../common/cedet.el -l cit-load.el -f toggle-debug-on-error  -f cedet-integ-test
