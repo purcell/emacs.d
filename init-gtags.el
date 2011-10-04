732381c632f2dfcabffb3f51cf1f15bf958ff157
chen bin <chenbin.sh@gmail.com>
2011-10-04 21:07:04 +0800

    gtags emacs

--- init-gtags.el
+++ init-gtags.el
@@ -12,19 +12,20 @@
               (local-set-key "\M-m" 'gtags-find-file)
               (local-set-key "\M-/" 'gtags-find-pattern)
               (local-set-key "\M-;" 'gtags-find-with-grep)))
-  ;; @see http://emacs-fu.blogspot.com/2009/01/navigating-through-source-code-using.html
-  (defun djcb-gtags-create-or-update ()
-    "create or update the gnu global tag file"
-    (interactive)
-    (if (not (= 0 (call-process "global" nil nil nil " -p"))) ; tagfile doesn't exist?
-      (let ((olddir default-directory)
-            (topdir (read-directory-name
-                      "gtags: top of source tree:" default-directory)))
-        (cd topdir)
-        (shell-command "gtags && echo 'created tagfile'")
-        (cd olddir)) ; restore
-      ;;  tagfile already exists; update it
-      (shell-command "global -u && echo 'updated tagfile'")))
   )
+
+;; @see http://emacs-fu.blogspot.com/2009/01/navigating-through-source-code-using.html
+(defun djcb-gtags-create-or-update ()
+  "create or update the gnu global tag file"
+  (interactive)
+  (if (not (= 0 (call-process "global" nil nil nil " -p"))) ; tagfile doesn't exist?
+    (let ((olddir default-directory)
+          (topdir (read-directory-name
+                    "gtags: top of source tree:" default-directory)))
+      (cd topdir)
+      (shell-command "gtags && echo 'created tagfile'")
+      (cd olddir)) ; restore
+    ;;  tagfile already exists; update it
+    (shell-command "global -u && echo 'updated tagfile'")))
 
 (provide 'init-gtags)
