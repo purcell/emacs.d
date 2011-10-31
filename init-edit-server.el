d7cfe609274dd2297ebed41acaec0e713123b691
Steve Purcell <steve@sanityinc.com>
2011-10-31 08:40:24 +0000

    Use the 'edit-server-url-major-mode-alist custom var I got the edit-server guys to merge

--- init-edit-server.el
+++ init-edit-server.el
@@ -2,5 +2,10 @@
 (setq edit-server-new-frame nil)
 (edit-server-start t)
 
+(setq edit-server-url-major-mode-alist
+      '(("github\\.com" . markdown-mode)
+        ("stackoverflow\\.com" . markdown-mode)
+        ("lighthouseapp\\.com" . markdown-mode)
+        ("rubyonrails\\.org" . textile-mode)))
 
 (provide 'init-edit-server)
