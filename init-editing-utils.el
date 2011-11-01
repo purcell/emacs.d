8c1668887425e3c19bcc47963469b76ec6ad59bb
Steve Purcell <steve@sanityinc.com>
2011-11-01 12:02:38 +0000

    Use C-. for set-mark-command; C-SPC is for Quicksilver, and C-@ is too much work

--- init-editing-utils.el
+++ init-editing-utils.el
@@ -83,6 +83,8 @@
 (global-set-key (kbd "C-c j") 'join-line)
 (global-set-key (kbd "C-c J") (lambda () (interactive) (join-line 1)))
 (global-set-key (kbd "M-T") 'transpose-lines)
+(global-set-key (kbd "C-.") 'set-mark-command)
+(global-set-key (kbd "C-x C-.") 'pop-global-mark)
 
 (defun duplicate-line ()
   (interactive)
