d23864c1079c70d270c7c0b9fbd5f9799ed2b9b9
Steve Purcell <steve@sanityinc.com>
2011-11-02 08:31:35 +0000

    Remove defunct setting of man path for 'woman

--- init-exec-path.el
+++ init-exec-path.el
@@ -4,9 +4,6 @@
     (setq exec-path (split-string path-from-shell path-separator))))
 
 (when *is-a-mac*
-  (eval-after-load "woman"
-    '(setq woman-manpath (append (list "/opt/local/man") woman-manpath)))
-
   ;; When started from Emacs.app or similar, ensure $PATH
   ;; is the same the user would see in Terminal.app
   (if window-system (set-exec-path-from-shell-PATH)))
