f9439226bffd02a0636e704b6fb5d4003781d18b
Steve Purcell <steve@sanityinc.com>
2011-11-04 13:09:39 +0000

    Explicitly require ibuffer-vc to get its column definitions, which can't be autoloaded

--- init-ibuffer.el
+++ init-ibuffer.el
@@ -16,6 +16,12 @@
         ((> (buffer-size) 1000) (format "%7.1fk" (/ (buffer-size) 1000.0)))
         (t (format "%8d" (buffer-size)))))))
 
+
+;; Explicitly require ibuffer-vc to get its column definitions, which
+;; can't be autoloaded
+(eval-after-load 'ibuffer
+  '(require 'ibuffer-vc))
+
 ;; Modify the default ibuffer-formats
 (setq ibuffer-formats
       '((mark modified read-only vc-status-mini " "
