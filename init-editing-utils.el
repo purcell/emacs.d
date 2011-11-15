7d3932ad686b596059193f4275e73adad20ab45d
Steve Purcell <steve@sanityinc.com>
2011-11-15 11:35:18 +0000

    Recent Emacs versions have 'count-words, so remove my version

--- init-editing-utils.el
+++ init-editing-utils.el
@@ -184,16 +184,6 @@
 
 (suspend-mode-during-cua-rect-selection 'whole-line-or-region-mode)
 
-;;----------------------------------------------------------------------------
-;; Easily count words (http://emacs-fu.blogspot.com/2009/01/counting-words.html)
-;;----------------------------------------------------------------------------
-(defun count-words (&optional begin end)
-  "count words between BEGIN and END (region); if no region defined, count words in buffer"
-  (interactive "r")
-  (let ((b (if mark-active begin (point-min)))
-      (e (if mark-active end (point-max))))
-    (message "Word count: %s" (how-many "\\w+" b e))))
-
 
 ;;----------------------------------------------------------------------------
 ;; Unfill regions or paragraphs (see http://xahlee.org/emacs/emacs_unfill-paragraph.html)
