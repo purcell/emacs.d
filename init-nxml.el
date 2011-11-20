daf14b2da26be29b2718cb82c37f1ac9022106ea
Steve Purcell <steve@sanityinc.com>
2011-11-20 14:28:26 +0000

    Remove reference to obsolete 'unify-8859-on-decoding-mode

--- init-nxml.el
+++ init-nxml.el
@@ -2,7 +2,6 @@
 (add-to-list 'auto-mode-alist
               (cons (concat "\\." (regexp-opt '("xml" "xsd" "sch" "rng" "xslt" "svg" "rss") t) "\\'")
                     'nxml-mode))
-(unify-8859-on-decoding-mode)
 (setq magic-mode-alist (cons '("<\\?xml " . nxml-mode) magic-mode-alist))
 (fset 'html-mode 'nxml-mode)
 (fset 'xml-mode 'nxml-mode)
