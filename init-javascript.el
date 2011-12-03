13fd149460db57016c7d6ecee17640c5ddf066a5
Steve Purcell <steve@sanityinc.com>
2011-12-03 09:59:07 +0000

    Use 'preferred-javascript-mode for json too

--- init-javascript.el
+++ init-javascript.el
@@ -8,7 +8,7 @@
 
 ;; Need to first remove from list if present, since elpa adds entries too, which
 ;; may be in an arbitrary order
-(setq auto-mode-alist (cons `("\\.js\\(\\.erb\\)?$" . ,preferred-javascript-mode)
+(setq auto-mode-alist (cons `("\\.js\\(\\.erb\\|on\\)?$" . ,preferred-javascript-mode)
                             (loop for entry in auto-mode-alist
                                   unless (eq preferred-javascript-mode (cdr entry))
                                   collect entry)))
