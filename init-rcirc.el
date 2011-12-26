6e49fe25ded7071824a102355845f789be227e5e
chen bin <chenbin.sh@gmail.com>
2011-12-26 22:16:05 +0800

    rcirc tweak

--- init-rcirc.el
+++ init-rcirc.el
@@ -2,18 +2,42 @@
 ;; connect to two networks anyway.
 (require 'rcirc)
 
-;; Keep input line at bottom.
-(add-hook 'rcirc-mode-hook
-          (lambda ()
-            (flyspell-mode 1)
-            (set (make-local-variable 'scroll-conservatively) 8192)))
+;; Stop rcirc from printing /away message
+(defun rcirc-handler-301 (process cmd sender args)
+  "/away message handler.")
 
-
+;; Use fly spell check
+(add-hook 'rcirc-mode-hook (lambda ()
+                             (flyspell-mode 1)))
 ;; Keep input line at bottom.
 (add-hook 'rcirc-mode-hook
           (lambda ()
             (set (make-local-variable 'scroll-conservatively)
                  8192)))
+;; Reconnect
+(eval-after-load 'rcirc
+  '(defun-rcirc-command reconnect (arg)
+     "Reconnect the server process."
+     (interactive "i")
+     (unless process
+       (error "There's no process for this target"))
+     (let* ((server (car (process-contact process)))
+            (port (process-contact process :service))
+            (nick (rcirc-nick process))
+            channels query-buffers)
+       (dolist (buf (buffer-list))
+         (with-current-buffer buf
+           (when (eq process (rcirc-buffer-process))
+             (remove-hook 'change-major-mode-hook
+                          'rcirc-change-major-mode-hook)
+             (if (rcirc-channel-p rcirc-target)
+                 (setq channels (cons rcirc-target channels))
+               (setq query-buffers (cons buf query-buffers))))))
+       (delete-process process)
+       (rcirc-connect server port nick
+                      rcirc-default-user-name
+                      rcirc-default-user-full-name
+                      channels))))
 
 ;; Turn the debugging mode on
 ;(setq rcirc-debug-flag t)
