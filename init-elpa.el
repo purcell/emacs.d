53b1d5f9fce657d79de1d6a8ba0ec1e9b2238f90
Steve Purcell <steve@sanityinc.com>
2012-01-08 09:17:43 +0000

    Explicitly return t from 'require-package when package is already installed

--- init-elpa.el
+++ init-elpa.el
@@ -1,6 +1,7 @@
 (defun require-package (package &optional min-version no-refresh)
   "Ask elpa to install given PACKAGE."
-  (unless (package-installed-p package min-version)
+  (if (package-installed-p package min-version)
+      t
     (if (or (assoc package package-archive-contents) no-refresh)
         (package-install package)
       (progn
