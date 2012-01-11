30cc02d3301db4c90928d1a27235b35ac857e6e6
Steve Purcell <steve@sanityinc.com>
2012-01-11 10:24:47 +0000

    Remove redundant haml config that is provided by elpa packages

--- init-haml.el
+++ init-haml.el
@@ -1,5 +1,4 @@
-(add-auto-mode 'haml-mode "\.haml$")
-(add-auto-mode 'sass-mode "\.sass$" "\.scss$")
+(add-auto-mode 'sass-mode "\.scss$")
 
 (add-hook 'haml-mode-hook 'flymake-haml-load)
 (add-hook 'sass-mode-hook 'flymake-sass-load)
