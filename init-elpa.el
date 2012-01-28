8c74cdf2708dfa0679e8da5bd6add7a7fa90a3d8
Steve Purcell <steve@sanityinc.com>
2012-01-28 15:44:09 +0000

    Use packages from Melpa, but just for slime, rvm and magit

--- init-elpa.el
+++ init-elpa.el
@@ -1,3 +1,43 @@
+;;------------------------------------------------------------------------------
+;; Find and load the correct package.el
+;;------------------------------------------------------------------------------
+
+;; When switching between Emacs 23 and 24, we always use the bundled package.el in Emacs 24
+(let ((package-el-site-lisp-dir (expand-file-name "~/.emacs.d/site-lisp/package")))
+  (when (and (file-directory-p package-el-site-lisp-dir)
+             (> emacs-major-version 23))
+    (message "Removing local package.el from load-path to avoid shadowing bundled version")
+    (setq load-path (remove package-el-site-lisp-dir load-path))))
+
+(require 'package)
+
+
+;;------------------------------------------------------------------------------
+;; Add support to package.el for pre-filtering available packages
+;;------------------------------------------------------------------------------
+
+(defvar package-filter-function nil
+  "Optional predicate function used to internally filter packages used by package.el.
+
+The function is called with the arguments PACKAGE VERSION ARCHIVE, where
+PACKAGE is a symbol, VERSION is a vector as produced by `version-to-list', and
+ARCHIVE is the string name of the package archive.")
+
+(defadvice package--add-to-archive-contents
+  (around filter-packages (package archive) activate)
+  "Add filtering of available packages using `package-filter-function', if non-nil."
+  (when (or (null package-filter-function)
+            (funcall package-filter-function
+                     (car package)
+                     (package-desc-vers (cdr package))
+                     archive))
+    ad-do-it))
+
+
+;;------------------------------------------------------------------------------
+;; On-demand installation of packages
+;;------------------------------------------------------------------------------
+
 (defun require-package (package &optional min-version no-refresh)
   "Ask elpa to install given PACKAGE."
   (if (package-installed-p package min-version)
@@ -8,16 +48,39 @@
         (package-refresh-contents)
         (require-package package min-version t)))))
 
-;; When switching between Emacs 23 and 24, we always use the bundled package.el in Emacs 24
-(let ((package-el-site-lisp-dir (expand-file-name "~/.emacs.d/site-lisp/package")))
-  (when (and (file-directory-p package-el-site-lisp-dir)
-             (> emacs-major-version 23))
-    (message "Removing local package.el from load-path to avoid shadowing bundled version")
-    (setq load-path (remove package-el-site-lisp-dir load-path))))
 
-(require 'package)
+;;------------------------------------------------------------------------------
+;; Standard package repositories
+;;------------------------------------------------------------------------------
+
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("tromey" . "http://tromey.com/elpa/"))
+
+
+;;------------------------------------------------------------------------------
+;; Also use Melpa for some packages built straight from VC
+;;------------------------------------------------------------------------------
+
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+
+;; Only take certain packages from Melpa
+
+(setq package-filter-function
+      (lambda (package version archive)
+        (or (not (string-equal archive "melpa"))
+            (memq package '(magit rvm slime)))))
+
+
+(defadvice package-download-transaction
+  (around disable-keepalives (&optional args) activate)
+  "Disable HTTP keep-alives to work around network issues with Melpa host."
+  (let ((url-http-attempt-keepalives nil))
+    ad-do-it))
+
+
+;;------------------------------------------------------------------------------
+;; Fire up package.el and ensure the following packages are installed.
+;;------------------------------------------------------------------------------
 
 (package-initialize)
 
@@ -28,6 +91,7 @@
 (require-package 'gnuplot)
 (require-package 'haskell-mode)
 (require-package 'tuareg)
+(require-package 'magit)
 (require-package 'flymake-cursor)
 (require-package 'json)
 (require-package 'js2-mode)
