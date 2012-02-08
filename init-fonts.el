c8b09f07fa57bee74283348915c8bf0c482ac638
Steve Purcell <steve@sanityinc.com>
2012-02-08 13:24:40 +0000

    Clarify 'increment-default-font-height

--- init-fonts.el
+++ init-fonts.el
@@ -7,16 +7,21 @@
     (mapconcat 'identity parts "-")))
 
 (defun increment-default-font-height (delta)
-  (let ((new-height (+ (face-attribute 'default :height) delta)))
+  "Adjust the default font height by DELTA on every frame.
+The pixel size of the frame is kept (approximately) the same.
+DELTA should be a multiple of 10, in the units used by the
+:height face attribute."
+  (let* ((new-height (+ (face-attribute 'default :height) delta))
+         (new-point-height (/ new-height 10)))
     (dolist (f (frame-list))
       (with-selected-frame f
         ;; Latest 'set-frame-font supports a "frames" arg, but
         ;; we cater to Emacs 23 by looping instead.
         (set-frame-font (font-name-replace-size (face-font 'default)
-                                                (/ new-height 10))
+                                                new-point-height)
                         t)))
     (set-face-attribute 'default nil :height new-height)
-    (message "default font size is now %d" (/ new-height 10))))
+    (message "default font size is now %d" new-point-height)))
 
 (defun increase-default-font-height ()
   (interactive)
