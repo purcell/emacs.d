98a4eac32337c89136c4a6ee32683e34fd06c87d
chen bin <chenbin.sh@gmail.com>
2012-03-07 21:58:08 +0800

    change color theme

--- init-themes.el
+++ init-themes.el
@@ -1,5 +1,5 @@
 (if window-system
-     (color-theme-sanityinc-solarized-dark)
-    )
-
+    ;; (color-theme-sanityinc-solarized-dark)
+    (color-theme-sanityinc-tomorrow-bright)
+  )
 (provide 'init-themes)
