7f570b47257a417a12c63cffe6bb694039d9886e
Steve Purcell <steve@sanityinc.com>
2012-03-21 13:23:50 +0000

    Don't use melpa version of slime

--- init-elpa.el
+++ init-elpa.el
@@ -67,7 +67,7 @@ ARCHIVE is the string name of the package archive.")
 (setq package-filter-function
       (lambda (package version archive)
         (or (not (string-equal archive "melpa"))
-            (memq package '(magit rvm slime mmm-mode dired+ csv-mode
+            (memq package '(magit rvm mmm-mode dired+ csv-mode
                                   pretty-mode darcsum org-fstree textile-mode
                                   ruby-mode js3 git-blame todochiku)))))
 
