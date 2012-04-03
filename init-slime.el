ab6ca0bfb79e208e3c5fb5d281d108e295d65aea
Steve Purcell <steve@sanityinc.com>
2012-04-03 09:49:36 +0100

    Bind TAB to indent-for-tab-command in Slime REPL, so auto-complete works there too

--- init-slime.el
+++ init-slime.el
@@ -2,6 +2,10 @@
 (eval-after-load 'slime-fuzzy
   '(require 'slime-repl))
 
+(defun smp/set-up-slime-repl-auto-complete ()
+  "Bind TAB to `indent-for-tab-command', as in regular Slime buffers."
+  (local-set-key (kbd "TAB") 'indent-for-tab-command))
+
 (eval-after-load 'slime
   '(progn
      (add-to-list 'load-path (concat (directory-of-library "slime") "/contrib"))
@@ -26,6 +30,8 @@
 
      (add-hook 'slime-mode-hook 'set-up-slime-ac)
      (add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
+
+     (add-hook 'slime-repl-mode-hook 'smp/set-up-slime-repl-auto-complete)
 
      (eval-after-load 'auto-complete
        '(add-to-list 'ac-modes 'slime-repl-mode))))
