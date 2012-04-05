ec3025e254bfe0ba501fbeaceda046bb216b551b
Steve Purcell <steve@sanityinc.com>
2012-04-05 19:22:12 +0100

    Update ruby-mode hooks for compatibility with latest inf-ruby

--- init-ruby-mode.el
+++ init-ruby-mode.el
@@ -6,10 +6,6 @@
 
 
 (autoload 'run-ruby "inf-ruby" "Run an inferior Ruby process")
-(autoload 'inf-ruby-keys "inf-ruby"
-  "Set local key defs for inf-ruby in ruby-mode")
-
-(add-hook 'ruby-mode-hook 'inf-ruby-keys)
 
 (setq ruby-use-encoding-map nil)
 
