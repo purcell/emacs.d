029ed5fca24e9fbf76a29ac733c96813a6312ba6
Steve Purcell <steve@sanityinc.com>
2012-04-20 14:35:25 +0100

    Enable auto-complete in js3

--- init-auto-complete.el
+++ init-auto-complete.el
@@ -28,7 +28,7 @@
                 sass-mode yaml-mode csv-mode espresso-mode haskell-mode
                 html-mode nxml-mode sh-mode smarty-mode clojure-mode
                 lisp-mode textile-mode markdown-mode tuareg-mode
-                css-mode less-css-mode))
+                js3-mode css-mode less-css-mode))
   (add-to-list 'ac-modes mode))
 
 
