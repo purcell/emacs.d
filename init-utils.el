31c818dbc6c48eb2516f436310fb8a1a31e83045
Steve Purcell <steve@sanityinc.com>
2012-05-02 08:26:23 +0100

    Fix minor error in rename-this-file-and-buffer, triggered by uniquify's buffer renaming

--- init-utils.el
+++ init-utils.el
@@ -60,7 +60,7 @@
     (if (get-buffer new-name)
         (message "A buffer named '%s' already exists!" new-name)
       (progn
-        (rename-file name new-name 1)
+        (rename-file filename new-name 1)
         (rename-buffer new-name)
         (set-visited-file-name new-name)
         (set-buffer-modified-p nil)))))
