9b5f163ee6ed3ed9ed69c4ddd26f76f80626eb2a
chen bin <chenbin.sh@gmail.com>
2012-06-08 10:55:17 +0800

    new hotkeys for undo/redo windows layout

--- init-windows.el
+++ init-windows.el
@@ -2,6 +2,9 @@
 ;; Navigate window layouts with "C-c <left>" and "C-c <right>"
 ;;----------------------------------------------------------------------------
 (winner-mode 1)
+;; copied from http://puntoblogspot.blogspot.com/2011/05/undo-layouts-in-emacs.html
+(global-set-key (kbd "C-x 4 u") 'winner-undo)
+(global-set-key (kbd "C-x 4 r") 'winner-redo)
 
 
 ;;----------------------------------------------------------------------------
