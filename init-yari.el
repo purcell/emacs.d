ac8d5333cb173388832a8e360ecef3eac930a742
chen bin <chenbin.sh@gmail.com>
2012-06-14 17:47:57 +0800

    ruby code in nxhtml mode cannot be detected sometimes

--- init-yari.el
+++ init-yari.el
@@ -1,8 +1,8 @@
-; override `better register` which also uses [f1]
 ; You can use C-u M-x yari to reload all completion targets.
 (require 'yari)
 (defun ri-bind-key ()
   (local-set-key "\C-c;y" 'yari-anything))
 (add-hook 'ruby-mode-hook 'ri-bind-key)
+(add-hook 'nxml-mode-hook 'ri-bind-key)
 
 (provide 'init-yari)
