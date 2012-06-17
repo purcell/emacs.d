81f24b862f2a314b7bf00df6963fcc7eb74d6307
chen bin <chenbin.sh@gmail.com>
2012-06-17 21:59:46 +0800

    yasnippet config

--- init-yasnippet.el
+++ init-yasnippet.el
@@ -1,3 +1,9 @@
 (require 'yasnippet)
 (yas/initialize)
+;; default TAB key is occupied by auto-complete
+(global-set-key (kbd "C-c ; u") 'yas/expand)
+;; default hotkey `C-c & C-s` is still valid
+(global-set-key (kbd "C-c ; s") 'yas/insert-snippet)
+;; give yas/dropdown-prompt in yas/prompt-functions a chance
+(require 'dropdown-list)
 (provide 'init-yasnippet)
