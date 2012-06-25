a46a28520b41e3cf7be5c0b093fb8a016478436b
Steve Purcell <steve@sanityinc.com>
2012-06-25 20:29:56 +0100

    Prefer 'input-decode-map to 'function-key-map where available

--- init-xterm.el
+++ init-xterm.el
@@ -1,14 +1,17 @@
 (require 'init-frame-hooks)
 
 (defun fix-up-xterm-control-arrows ()
-  (define-key function-key-map "\e[1;5A" [C-up])
-  (define-key function-key-map "\e[1;5B" [C-down])
-  (define-key function-key-map "\e[1;5C" [C-right])
-  (define-key function-key-map "\e[1;5D" [C-left])
-  (define-key function-key-map "\e[5A"   [C-up])
-  (define-key function-key-map "\e[5B"   [C-down])
-  (define-key function-key-map "\e[5C"   [C-right])
-  (define-key function-key-map "\e[5D"   [C-left]))
+  (let ((map (if (boundp 'input-decode-map)
+                 input-decode-map
+               function-key-map)))
+    (define-key map "\e[1;5A" [C-up])
+    (define-key map "\e[1;5B" [C-down])
+    (define-key map "\e[1;5C" [C-right])
+    (define-key map "\e[1;5D" [C-left])
+    (define-key map "\e[5A"   [C-up])
+    (define-key map "\e[5B"   [C-down])
+    (define-key map "\e[5C"   [C-right])
+    (define-key map "\e[5D"   [C-left])))
 
 (add-hook 'after-make-console-frame-hooks
           (lambda ()
