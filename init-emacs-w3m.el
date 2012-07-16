b55e72e62f0e274b8a25ca769b5354e8e488f85d
chen bin <chenbin.sh@gmail.com>
2012-07-16 17:14:17 +0800

    google code search relocated

--- init-emacs-w3m.el
+++ init-emacs-w3m.el
@@ -26,7 +26,7 @@
                                  (add-to-list 'w3m-search-engine-alist '("q"
                                                                          "http://www.google.com.au/search?hl=en&q=%s+site:stackoverflow.com" utf-8))
                                  (add-to-list 'w3m-search-engine-alist '("s"
-                                                                         "http://www.google.com.au/codesearch?q=%s" utf-8))
+                                                                         "http://code.google.com/codesearch?q=%s" utf-8))
                                  (add-to-list 'w3m-search-engine-alist '("b"
                                                                          "http://blogsearch.google.com.au/blogsearch?q=%s" utf-8))
                                  (add-to-list 'w3m-search-engine-alist '("w"
