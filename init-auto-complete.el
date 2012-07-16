fac08c79bd957e79fd0e4a686511362752f30c8e
chen bin <chenbin.sh@gmail.com>
2012-07-16 20:57:34 +0800

    cygwin auto-complete stuff

--- init-auto-complete.el
+++ init-auto-complete.el
@@ -1,4 +1,4 @@
-;; @see http://cx4a.org/software/auto-complete/manual.html 
+;; @see http://cx4a.org/software/auto-complete/manual.html
 (require 'auto-complete-config)
 (global-auto-complete-mode) ; recommended way to (setq global-auto-complete-mode), see info
 (setq ac-auto-start nil) ; popup candidates when you press each character is annoying
@@ -33,12 +33,20 @@
   (setq clang-include-dir-str
         (cond
          (*is-a-mac* "
- /usr/llvm-gcc-4.2/bin/../lib/gcc/i686-apple-darwin11/4.2.1/include
- /usr/include/c++/4.2.1
- /usr/include/c++/4.2.1/backward
- /usr/local/include
- /Applications/Xcode.app/Contents/Developer/usr/llvm-gcc-4.2/lib/gcc/i686-apple-darwin11/4.2.1/include
- /usr/include
+/usr/llvm-gcc-4.2/bin/../lib/gcc/i686-apple-darwin11/4.2.1/include
+/usr/include/c++/4.2.1
+/usr/include/c++/4.2.1/backward
+/usr/local/include
+/Applications/Xcode.app/Contents/Developer/usr/llvm-gcc-4.2/lib/gcc/i686-apple-darwin11/4.2.1/include
+/usr/include
+")
+         (*cygwin* "
+/usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/i686-pc-cygwin
+/usr/lib/gcc/i686-pc-cygwin/3.4.4/include/c++/backward
+/usr/local/include
+/usr/lib/gcc/i686-pc-cygwin/3.4.4/include
+/usr/include
+/usr/lib/gcc/i686-pc-cygwin/3.4.4/../../../../include/w32api
 ")
          (t "
 /usr/include
@@ -61,7 +69,7 @@
   (setq ac-clang-flags
         (mapcar (lambda (item) (concat "-I" item))
                 (split-string clang-include-dir-str)))
-  ;(setq ac-clang-auto-save t)
+  (setq ac-clang-auto-save t)
   )
 (add-hook 'c-mode-common-hook 'my-ac-cc-mode-setup)
 
