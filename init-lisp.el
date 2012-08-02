3f4d1b4e56e13bc03fe4fdacd98af6e320be0406
Steve Purcell <steve@sanityinc.com>
2012-08-02 15:47:33 +0100

    Remove face overrides for elisp symbols in autocomplete dropdowns

--- init-lisp.el
+++ init-lisp.el
@@ -67,25 +67,8 @@
 
 
 ;; ----------------------------------------------------------------------------
-;; Auto-complete tweaks for emacs-lisp mode
+;; Automatic byte compilation
 ;; ----------------------------------------------------------------------------
-
-(defface ac-symbol-menu-face
-  '((t (:background "lightgray" :foreground "darkgreen")))
-  "Face for slime candidate menu."
-  :group 'auto-complete)
-
-(defface ac-symbol-selection-face
-  '((t (:background "darkgreen" :foreground "white")))
-  "Face for the slime selected candidate."
-  :group 'auto-complete)
-
-;; Modify ac-source-symbols to add colours
-(eval-after-load 'auto-complete
-  '(progn
-     (add-to-list 'ac-source-symbols '(candidate-face . ac-symbol-menu-face))
-     (add-to-list 'ac-source-symbols '(selection-face . ac-symbol-selection-face))))
-
 
 (defun maybe-byte-compile ()
   (when (and (eq major-mode 'emacs-lisp-mode)
