2bb204f8215c3b7536a17f982e286168a0cd21fe
chen bin <chenbin.sh@gmail.com>
2012-08-16 14:12:16 +0800

    Revert "copy-buffer-file-name-as-kill"
    This reverts commit 001f6263e781e5db4575e4ced9ba3d3a8223f58b.
    The commit's code is unstable under cygwin

--- init-misc.el
+++ init-misc.el
@@ -197,25 +197,6 @@
 (global-set-key (kbd "M-p") 'move-text-up)
 (global-set-key (kbd "M-n") 'move-text-down)
 
-;; copied from http://www.emacswiki.org/emacs/buffer-extension.el
-(defun copy-buffer-file-name-as-kill (choice)
-  "Copy the buffer-file-name to the kill-ring"
-  (interactive "cCopy Buffer Name (F) Full, (D) Directory, (N) Name")
-  (let ((new-kill-string)
-        (name (if (eq major-mode 'dired-mode)
-                  (dired-get-filename)
-                (or (buffer-file-name) ""))))
-    (cond ((eq choice ?f)
-           (setq new-kill-string name))
-          ((eq choice ?d)
-           (setq new-kill-string (file-name-directory name)))
-          ((eq choice ?n)
-           (setq new-kill-string (file-name-nondirectory name)))
-          (t (message "Quit")))
-    (when new-kill-string
-      (message "%s copied" new-kill-string)
-      (kill-new new-kill-string))))
-
 ;; move window
 (require 'window-numbering)
 (window-numbering-mode 1)
