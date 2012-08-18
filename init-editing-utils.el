cfa11128f7cc0ef28e57214c13f191c6d04b6e42
Steve Purcell <steve@sanityinc.com>
2012-08-18 11:04:07 -0400

    Don't enable fci-mode in Emacs 23 (fixes #44)

--- init-editing-utils.el
+++ init-editing-utils.el
@@ -122,25 +122,26 @@
 ;;----------------------------------------------------------------------------
 ;; Fill column indicator
 ;;----------------------------------------------------------------------------
-(defun sanityinc/prog-mode-fci-settings ()
-  (turn-on-fci-mode)
-  (when show-trailing-whitespace
-    (set (make-local-variable 'whitespace-style) '(face trailing))
-    (whitespace-mode 1)))
+(when (boundp 'prog-mode-hook)
+  (defun sanityinc/prog-mode-fci-settings ()
+    (turn-on-fci-mode)
+    (when show-trailing-whitespace
+      (set (make-local-variable 'whitespace-style) '(face trailing))
+      (whitespace-mode 1)))
 
-(add-hook 'prog-mode-hook 'sanityinc/prog-mode-fci-settings)
+  (add-hook 'prog-mode-hook 'sanityinc/prog-mode-fci-settings)
 
-(defvar sanityinc/fci-mode-suppressed nil)
-(defadvice popup-create (before suppress-fci-mode activate)
-  "Suspend fci-mode while popups are visible"
-  (set (make-local-variable 'sanityinc/fci-mode-suppressed) fci-mode)
-  (when fci-mode
-    (turn-off-fci-mode)))
-(defadvice popup-delete (after restore-fci-mode activate)
-  "Restore fci-mode when all popups have closed"
-  (when (and (not popup-instances) sanityinc/fci-mode-suppressed)
-    (setq sanityinc/fci-mode-suppressed nil)
-    (turn-on-fci-mode)))
+  (defvar sanityinc/fci-mode-suppressed nil)
+  (defadvice popup-create (before suppress-fci-mode activate)
+    "Suspend fci-mode while popups are visible"
+    (set (make-local-variable 'sanityinc/fci-mode-suppressed) fci-mode)
+    (when fci-mode
+      (turn-off-fci-mode)))
+  (defadvice popup-delete (after restore-fci-mode activate)
+    "Restore fci-mode when all popups have closed"
+    (when (and (not popup-instances) sanityinc/fci-mode-suppressed)
+      (setq sanityinc/fci-mode-suppressed nil)
+      (turn-on-fci-mode))))
 
 
 ;;----------------------------------------------------------------------------
