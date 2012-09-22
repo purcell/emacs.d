119b1da93ab6f939613302ae8007c75f34d71d5a
chen bin <chenbin.sh@gmail.com>
2012-09-22 22:38:18 +1000

    yasnippet-20120822.52 changed API yas/intialize; yasnippet-20120718 is fine

--- init-yasnippet.el
+++ init-yasnippet.el
@@ -1,5 +1,8 @@
 (require 'yasnippet)
-(yas/initialize)
+(if (fboundp 'yas/initialize)
+  (yas/initialize)
+  (yas-global-mode 1)
+  )
 ;; default TAB key is occupied by auto-complete
 (global-set-key (kbd "C-c k") 'yas/expand)
 ;; default hotkey `C-c & C-s` is still valid
