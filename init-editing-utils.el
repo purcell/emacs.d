7378841866fabe8715653e0bca203e4726a4a470
Steve Purcell <steve@sanityinc.com>
2012-09-24 16:47:31 +0100

    Switch from mark-multiple to multiple-cursors

--- init-editing-utils.el
+++ init-editing-utils.el
@@ -99,11 +99,16 @@
 (global-set-key (kbd "C-:") 'ace-jump-word-mode)
 
 
-;; Mark-multiple and friends
-(global-set-key (kbd "C-x r t") 'inline-string-rectangle)
-(global-set-key (kbd "C-<") 'mark-previous-like-this)
-(global-set-key (kbd "C->") 'mark-next-like-this)
-(global-set-key (kbd "C-M-m") 'mark-more-like-this)
+;; multiple-cursors
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-+") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+;; From active region to multiple cursors:
+(global-set-key (kbd "C-c c r") 'set-rectangular-region-anchor)
+(global-set-key (kbd "C-c c c") 'mc/edit-lines)
+(global-set-key (kbd "C-c c e") 'mc/edit-ends-of-lines)
+(global-set-key (kbd "C-c c a") 'mc/edit-beginnings-of-lines)
 
 
 (defun duplicate-line ()
