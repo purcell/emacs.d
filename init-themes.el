4e12a62833c797a7b879d6e144d12c7728530f17
Steve Purcell <steve@sanityinc.com>
2012-09-26 15:46:39 +0100

    Quote value in 'reapply-themes, which caused an error message at start-up (fixes #50)

--- init-themes.el
+++ init-themes.el
@@ -46,9 +46,8 @@ ignored: use `custom-enabled-themes' instead."
   "Forcibly load the themes listed in `custom-enabled-themes'."
   (dolist (theme custom-enabled-themes)
     (unless (custom-theme-p theme)
-      (load-theme theme)
-      (message "Consider using 'M-x customize-themes' to save your preferred theme.")))
-  (custom-set-variables `(custom-enabled-themes ,custom-enabled-themes)))
+      (load-theme theme)))
+  (custom-set-variables `(custom-enabled-themes (quote ,custom-enabled-themes))))
 
 (add-hook 'after-init-hook 'reapply-themes)
 
