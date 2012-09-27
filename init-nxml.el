11610620b7e31c16f7d47e380e363e4969eb7f40
chen bin <chenbin.sh@gmail.com>
2012-09-27 15:07:44 +1000

    support bakefile xml

--- init-nxml.el
+++ init-nxml.el
@@ -1,6 +1,6 @@
 (load-library "rng-auto")
 (add-to-list 'auto-mode-alist
-              (cons (concat "\\." (regexp-opt '("xml" "xsd" "sch" "rng" "xslt" "svg" "rss") t) "\\'")
+              (cons (concat "\\." (regexp-opt '("xml" "xsd" "sch" "rng" "xslt" "svg" "rss" "bkl" "bkgen") t) "\\'")
                     'nxml-mode))
 (setq magic-mode-alist (cons '("<\\?xml " . nxml-mode) magic-mode-alist))
 (fset 'html-mode 'nxml-mode)
