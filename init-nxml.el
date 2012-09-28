034517d0fc9dcdd1023764a2de9a64ddb2dbaa56
Steve Purcell <steve@sanityinc.com>
2012-09-28 16:25:08 +0100

    Add a handy pp-xml-region command

--- init-nxml.el
+++ init-nxml.el
@@ -17,6 +17,20 @@
            rng-schema-locating-files)))
 
 
+;; See: http://sinewalker.wordpress.com/2008/06/26/pretty-printing-xml-with-emacs-nxml-mode/
+(defun pp-xml-region (begin end)
+  "Pretty format XML markup in region. The function inserts
+linebreaks to separate tags that have nothing but whitespace
+between them.  It then indents the markup by using nxml's
+indentation rules."
+  (interactive "r")
+  (save-excursion
+      (nxml-mode)
+      (goto-char begin)
+      (while (search-forward-regexp "\>[ \\t]*\<" nil t)
+        (backward-char) (insert "\n"))
+      (indent-region begin end)))
+
 ;;----------------------------------------------------------------------------
 ;; Integration with tidy for html + xml
 ;;----------------------------------------------------------------------------
