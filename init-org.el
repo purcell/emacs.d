b6c059075a716940dd3b2a73a50541dd8972fcb9
chen bin <chenbin.sh@gmail.com>
2012-10-21 00:11:19 +1100

    remove org-startup-indent. I donot like it

--- init-org.el
+++ init-org.el
@@ -13,7 +13,8 @@
       org-fast-tag-selection-single-key 'expert
       org-export-kill-product-buffer-when-displayed t
       org-tags-column 80
-      org-startup-indented t)
+      ;org-startup-indented t
+      )
 
 ; Refile targets include this file and any file contributing to the agenda - up to 5 levels deep
 (setq org-refile-targets (quote ((nil :maxlevel . 5) (org-agenda-files :maxlevel . 5))))
