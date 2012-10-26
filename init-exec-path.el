00f9f09612d4175850867ca70911b03e1e1b0bb9
Steve Purcell <steve@sanityinc.com>
2012-10-26 10:40:46 +0100

    Use exec-path-from-shell to slurp SSH_* env vars into Emacs on OS X

--- init-exec-path.el
+++ init-exec-path.el
@@ -1,3 +1,9 @@
+(eval-after-load 'exec-path-from-shell
+  '(progn
+     (dolist (var '("SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO"))
+       (add-to-list var 'exec-path-from-shell-variables))))
+
+
 (when (memq window-system '(mac ns))
   (exec-path-from-shell-initialize))
 
