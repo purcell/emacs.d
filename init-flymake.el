c24bc5ea8b70dad1733d0d1807c856e5dfb35c05
Steve Purcell <steve@sanityinc.com>
2012-11-01 14:05:07 +0000

    Remove obsolete fringe-helper trickery for flymake, which also breaks the latest flymake

--- init-flymake.el
+++ init-flymake.el
@@ -13,30 +13,4 @@
 Return nil if we cannot, non-nil if we can."
        (if (and file-name (flymake-get-init-function file-name)) t nil))))
 
-
-;; http://nschum.de/src/emacs/fringe-helper/
-(eval-after-load 'flymake
-  '(progn
-     (require 'fringe-helper)
-
-     (defvar flymake-fringe-overlays nil)
-     (make-variable-buffer-local 'flymake-fringe-overlays)
-
-     (defadvice flymake-make-overlay (after add-to-fringe first
-                                            (beg end tooltip-text face mouse-face)
-                                            activate compile)
-       (push (fringe-helper-insert-region
-              beg end
-              (fringe-lib-load (if (eq face 'flymake-errline)
-                                   fringe-lib-exclamation-mark
-                                 fringe-lib-question-mark))
-              'left-fringe 'font-lock-warning-face)
-             flymake-fringe-overlays))
-
-     (defadvice flymake-delete-own-overlays (after remove-from-fringe activate
-                                                   compile)
-       (mapc 'fringe-helper-remove flymake-fringe-overlays)
-       (setq flymake-fringe-overlays nil))))
-
-
 (provide 'init-flymake)
