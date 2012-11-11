0dc8c9b170f4e7695d97df11c408aafa130dadb8
chen bin <chenbin.sh@gmail.com>
2012-11-11 16:38:24 +1100

    upgraded org2blog

--- init-themes.el
+++ init-themes.el
@@ -1,5 +1,6 @@
 (if window-system
     ;; (color-theme-sanityinc-solarized-dark)
-    (color-theme-sanityinc-tomorrow-bright)
+    ;; (color-theme-sanityinc-tomorrow-bright)
+    (color-theme-sanityinc-tomorrow-night)
   )
 (provide 'init-themes)
