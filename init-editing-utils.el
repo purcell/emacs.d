3de819d192f01f1504c9a9be707cba40f357a90c
Steve Purcell <steve@sanityinc.com>
2012-11-14 09:49:24 +0000

    Advise enable-theme rather than load-theme when recomputing fci glyph

--- init-editing-utils.el
+++ init-editing-utils.el
@@ -174,7 +174,7 @@
       (turn-on-fci-mode)))
 
   ;; Regenerate fci-mode line images after switching themes
-  (defadvice load-theme (after recompute-fci-face activate)
+  (defadvice enable-theme (after recompute-fci-face activate)
     (dolist (buffer (buffer-list))
       (with-current-buffer buffer
         (when (sanityinc/fci-enabled-p)
