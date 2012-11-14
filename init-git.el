f9300c16b2581ddca59d16bf9902f22bd2fae0dd
Steve Purcell <steve@sanityinc.com>
2012-11-14 16:33:50 +0000

    Remove obsolete gist patch

--- init-git.el
+++ init-git.el
@@ -48,33 +48,4 @@
 
 
 
-
-
-
-(eval-after-load 'gist
-  ;; Fix from https://github.com/defunkt/gist.el/pull/16
-  '(defun gist-region (begin end &optional private &optional callback)
-     "Post the current region as a new paste at gist.github.com
-Copies the URL into the kill ring.
-
-With a prefix argument, makes a private paste."
-     (interactive "r\nP")
-     (let* ((file (or (buffer-file-name) (buffer-name)))
-            (name (file-name-nondirectory file))
-            (ext (or (cdr (assoc major-mode gist-supported-modes-alist))
-                     (file-name-extension file)
-                     "txt")))
-       (gist-request
-        (format "https://%s@gist.github.com/gists" 
-                (or (car (github-auth-info)) ""))
-        (or callback 'gist-created-callback)
-        `(,@(if private '(("action_button" . "private")))
-          ("file_ext[gistfile1]" . ,(concat "." ext))
-          ("file_name[gistfile1]" . ,name)
-          ("file_contents[gistfile1]" . ,(buffer-substring begin end)))))))
-
-
-
-
-
 (provide 'init-git)
