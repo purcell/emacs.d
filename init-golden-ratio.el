92f9b530950032c38d7ff3b516d6d65220ff14d7
chen bin <chenbin.sh@gmail.com>
2012-11-16 17:05:18 +1100

    tweak golden-ration

--- init-golden-ratio.el
+++ init-golden-ratio.el
@@ -1,7 +1,10 @@
 ;; golden-ratio
 (require 'golden-ratio)
 (setq golden-ratio-exclude-modes '("shell-mode" "calendar-mode" "gud-mode" "eshell-mode"))
-
+(setq golden-ratio-exclude-buffer-names
+      '(" *org tags*"
+        " *Org todo*"
+        ))
 (golden-ratio-enable)
 
 (provide 'init-golden-ratio)
