315b348435e52a8ca4d3e93a81d243883301fa6c
chen bin <chenbin.sh@gmail.com>
2013-01-02 01:46:25 +1100

    pomodoro config

--- init-pomodoro.el
+++ init-pomodoro.el
@@ -1,2 +1,7 @@
-(autoload 'pomodoro-start "pomodoro" nil t)
+(setq pomodoro-break-time 2)
+(setq pomodoro-long-break-time 5)
+(setq pomodoro-work-time 15)
+(setq-default mode-line-format
+              (cons '(pomodoro-mode-line-string pomodoro-mode-line-string)
+                    mode-line-format))
 (provide 'init-pomodoro)
