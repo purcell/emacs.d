7508f5ddaa5e6efd11efd5a0b9f251e742fd044f
chen bin <chenbin.sh@gmail.com>
2013-02-06 12:19:08 +1100

    force the aspell use English dictionary

--- init-spelling.el
+++ init-spelling.el
@@ -3,7 +3,10 @@
 
 (when (executable-find "aspell")
   (setq ispell-program-name "aspell"
-        ispell-extra-args '("--sug-mode=ultra")))
+        ;; ispell-extra-args '("--sug-mode=ultra")
+        ;; force the English dictionary
+        ispell-extra-args '("--sug-mode=ultra" "--lang=en_US")
+        ))
 (require 'init-flyspell)
 
 
