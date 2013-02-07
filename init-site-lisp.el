5040df6113f12481d7fb7e77dfd2c05a1df9403f
Steve Purcell <steve@sanityinc.com>
2013-02-07 14:05:37 +0000

    Neater code for adding site-lisp subdirs to load-path

--- init-site-lisp.el
+++ init-site-lisp.el
@@ -2,16 +2,18 @@
 ;; Set load path
 ;;----------------------------------------------------------------------------
 (eval-when-compile (require 'cl))
-(if (fboundp 'normal-top-level-add-to-load-path)
-    (let* ((my-lisp-dir "~/.emacs.d/site-lisp/")
-           (default-directory my-lisp-dir))
-      (progn
-        (setq load-path
-              (append
-               (loop for dir in (directory-files my-lisp-dir)
-                     unless (string-match "^\\." dir)
-                     collecting (expand-file-name dir))
-               load-path)))))
+(defun sanityinc/add-subdirs-to-load-path (parent-dir)
+  "Adds every non-hidden subdir of PARENT-DIR to `load-path'."
+  (let* ((default-directory parent-dir))
+    (progn
+      (setq load-path
+            (append
+             (loop for dir in (directory-files parent-dir)
+                   unless (string-match "^\\." dir)
+                   collecting (expand-file-name dir))
+             load-path)))))
+
+(sanityinc/add-subdirs-to-load-path "~/.emacs.d/site-lisp/")
 
 
 ;;----------------------------------------------------------------------------
