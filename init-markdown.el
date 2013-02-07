beccf8d8a051813fc0b7f2c04f3b3adc3c22bd29
Steve Purcell <steve@sanityinc.com>
2013-02-07 14:31:48 +0000

    Remove redundant autoload for markdown-mode

--- init-markdown.el
+++ init-markdown.el
@@ -1,4 +1,3 @@
-(autoload 'markdown-mode "markdown-mode" "Mode for editing Markdown documents" t)
 (setq auto-mode-alist
       (cons '("\\.\\(md\\|markdown\\)\\'" . markdown-mode) auto-mode-alist))
 
