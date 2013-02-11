e4cff1e0c2f3bfebd746eda8ef2e76a409fb3cef
Steve Purcell <steve@sanityinc.com>
2013-02-11 13:54:25 +0000

    Ensure goto-address-prog-mode is enabled in ruby-mode under Emacs 24

--- init-misc.el
+++ init-misc.el
@@ -4,10 +4,10 @@
 (add-auto-mode 'tcl-mode "Portfile\\'")
 (fset 'yes-or-no-p 'y-or-n-p)
 
-(add-hook (if (fboundp 'prog-mode)
-              'prog-mode-hook
-            'find-file-hooks)
-          'goto-address-prog-mode)
+(dolist (hook (if (fboundp 'prog-mode)
+                  '(prog-mode-hook ruby-mode-hook)
+                '(find-file-hooks)))
+  (add-hook hook 'goto-address-prog-mode))
 (add-hook 'after-save-hook 'executable-make-buffer-file-executable-if-script-p)
 (setq goto-address-mail-face 'link)
 
