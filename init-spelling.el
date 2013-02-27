f7dd176cbe949221d1ea5cc5a41b3f771b84ab59
Steve Purcell <steve@sanityinc.com>
2013-02-27 13:11:36 +0000

    Only enable flyspell if ispell-program-name exists

--- init-spelling.el
+++ init-spelling.el
@@ -1,7 +1,8 @@
 (when (executable-find "aspell")
   (setq ispell-program-name "aspell"
         ispell-extra-args '("--sug-mode=ultra")))
-(require 'init-flyspell)
 
+(when (executable-find ispell-program-name)
+  (require 'init-flyspell))
 
 (provide 'init-spelling)
