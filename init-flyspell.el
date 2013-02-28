bf7107ae58a198b3fbc561a437177e32b2c27bd5
Steve Purcell <steve@sanityinc.com>
2013-02-28 09:41:17 +0000

    Tie flyspell-prog-mode to prog-mode in Emacs >= 24

--- init-flyspell.el
+++ init-flyspell.el
@@ -1,27 +1,29 @@
 ;;----------------------------------------------------------------------------
 ;; Add spell-checking in comments for all programming language modes
 ;;----------------------------------------------------------------------------
-(dolist (hook '(lisp-mode-hook
-                emacs-lisp-mode-hook
-                scheme-mode-hook
-                clojure-mode-hook
-                ruby-mode-hook
-                yaml-mode
-                python-mode-hook
-                shell-mode-hook
-                php-mode-hook
-                css-mode-hook
-                haskell-mode-hook
-                caml-mode-hook
-                nxml-mode-hook
-                crontab-mode-hook
-                perl-mode-hook
-                tcl-mode-hook
-                javascript-mode-hook))
-  (add-hook hook 'flyspell-prog-mode))
-(add-hook 'nxml-mode-hook
-          (lambda ()
-            (add-to-list 'flyspell-prog-text-faces 'nxml-text-face)))
+(if (fboundp 'prog-mode)
+    (add-hook 'prog-mode-hook 'flyspell-prog-mode)
+  (dolist (hook '(lisp-mode-hook
+                  emacs-lisp-mode-hook
+                  scheme-mode-hook
+                  clojure-mode-hook
+                  ruby-mode-hook
+                  yaml-mode
+                  python-mode-hook
+                  shell-mode-hook
+                  php-mode-hook
+                  css-mode-hook
+                  haskell-mode-hook
+                  caml-mode-hook
+                  nxml-mode-hook
+                  crontab-mode-hook
+                  perl-mode-hook
+                  tcl-mode-hook
+                  javascript-mode-hook))
+    (add-hook hook 'flyspell-prog-mode)))
+
+(eval-after-load 'flyspell
+  '(add-to-list 'flyspell-prog-text-faces 'nxml-text-face))
 
 
 (provide 'init-flyspell)
