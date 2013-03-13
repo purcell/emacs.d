697ee8a1dc588ed4f04b27b4ab26e9c3c61fc735
chen bin <chenbin.sh@gmail.com>
2013-03-14 10:21:42 +1100

    dired enhancement

--- init-dired.el
+++ init-dired.el
@@ -2,6 +2,19 @@
   '(progn
      (require 'dired+)
      (setq dired-recursive-deletes 'top)
-     (define-key dired-mode-map [mouse-2] 'dired-find-file)))
+     (define-key dired-mode-map [mouse-2] 'dired-find-file)
+     (dolist (file `(("zathura" "pdf" "dvi" "pdf.gz" "ps" "eps")
+                     ("unrar x" "rar")
+                     ("mplayer -stop-xscreensaver" "avi" "mpg" "rmvb" "rm" "flv" "wmv" "mkv" "mp4")
+                     ("mplayer -playlist" "list" "pls")
+                     ("feh" "gif" "jpeg" "jpg" "tif" "png" )
+                     ("display" "gif" "jpeg" "jpg" "tif" "png")
+                     ("7z x" "7z")
+                     ("djview" "djvu")
+                     ("firefox" "xml" "xhtml" "html" "htm" "mht")))
+       (add-to-list 'dired-guess-shell-alist-default
+                    (list (concat "\\." (regexp-opt (cdr file) t) "$")
+                          (car file))))
+     ))
 
 (provide 'init-dired)
