c165f2fdb9425c681eba3d525eb54157216a8f17
Steve Purcell <steve@sanityinc.com>
2013-03-13 11:13:25 +0000

    Enable more functionality in inferior-haskell-mode

--- init-haskell.el
+++ init-haskell.el
@@ -1,13 +1,14 @@
 (require-package 'haskell-mode)
 
-(add-hook 'haskell-mode-hook
-          (lambda ()
-            (define-key haskell-mode-map (kbd "C-c h") 'hoogle)
-            (turn-on-haskell-doc-mode)
-            (turn-on-haskell-indent)))
+(dolist (hook '(haskell-mode-hook inferior-haskell-mode-hook))
+  (add-hook hook 'turn-on-haskell-doc-mode))
+
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
 
 (eval-after-load 'haskell-mode
-  '(define-key haskell-mode-map (kbd "RET") 'newline))
+  '(progn
+     (define-key haskell-mode-map (kbd "C-c h") 'hoogle)
+     (define-key haskell-mode-map (kbd "RET") 'newline)))
 
 (require-package 'ghci-completion)
 (add-hook 'inferior-haskell-mode-hook 'turn-on-ghci-completion)
