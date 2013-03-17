b8d62b66b99f5e3db5ad78d3e4728c1ca528e844
Steve Purcell <steve@sanityinc.com>
2013-03-17 10:13:13 +0000

    Disable spell-checking by default (see #73)

--- init.el
+++ init.el
@@ -6,7 +6,7 @@
 ;;----------------------------------------------------------------------------
 ;; Which functionality to enable (use t or nil for true and false)
 ;;----------------------------------------------------------------------------
-(defconst *spell-check-support-enabled* t)
+(defconst *spell-check-support-enabled* nil)
 (defconst *is-a-mac* (eq system-type 'darwin))
 (defconst *is-carbon-emacs* (eq window-system 'mac))
 (defconst *is-cocoa-emacs* (and *is-a-mac* (eq window-system 'ns)))
