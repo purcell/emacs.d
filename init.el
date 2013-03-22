746ef521ab078c83be098210f6209b9ac7be4ccf
chen bin <Chen.Bin@optus.com.au>
2013-03-23 10:56:18 +1100

    use web-mode

--- init.el
+++ init.el
@@ -149,7 +149,7 @@
 (require 'init-workgroups)
 (require 'init-move-window-buffer)
 (require 'init-term-mode)
-
+(require 'init-web-mode)
 
 ;;----------------------------------------------------------------------------
 ;; Allow access from emacsclient
@@ -206,7 +206,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(bmkp-last-as-first-bookmark-file "~/.emacs.d/.bookmarks.el"))
+ '(bmkp-last-as-first-bookmark-file "~/.emacs.d/.bookmarks.el")
+ '(session-use-package t nil (session)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
