6cb8af24b4d1d42e1cf729b8305ed571faf37ad0
John Gauss <fourcels@gmail.com>
2013-03-24 13:43:44 +0800

    fix sass-mode load flymake-sass

--- init-haml.el
+++ init-haml.el
@@ -1,5 +1,11 @@
 (require-package 'haml-mode)
 (require-package 'flymake-haml)
-(add-hook 'haml-mode-hook 'flymake-haml-load)
+
+(defun maybe-flymake-haml-load ()
+  "Activate flymake-haml as necessary, but not in derived modes."
+  (when (eq major-mode 'haml-mode)
+    (flymake-haml-load)))
+
+(add-hook 'haml-mode-hook 'maybe-flymake-haml-load)
 
 (provide 'init-haml)
