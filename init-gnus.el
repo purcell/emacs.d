e1e052e1fbd3e38ec164b95ca482d27f5e4129f2
chen bin <Chen.Bin@optus.com.au>
2013-03-26 14:20:10 +1100

    no personal email in my .emacs.d

--- init-gnus.el
+++ init-gnus.el
@@ -8,18 +8,12 @@
            (lambda () (local-set-key "o" 'my-gnus-group-list-subscribed-groups ))
            )
 
-; Personal Information
-(setq user-full-name "Chen Bin"
-      user-mail-address "chenbin.sh@gmail.com"
-      ;message-generate-headers-first t
-      )
-
 (setq message-send-mail-function 'smtpmail-send-it
       smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
       smtpmail-auth-credentials "~/.authinfo.gpg"
-      ;smtpmail-auth-credentials '(("smtp.gmail.com" 587 "chenbin.sh@gmail.com" nil))
       smtpmail-default-smtp-server "smtp.gmail.com"
       smtpmail-smtp-server "smtp.gmail.com"
       smtpmail-smtp-service 587
       smtpmail-local-domain "homepc")
+
 (provide 'init-gnus)
