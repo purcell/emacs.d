a1bdd54f602e1b76dda6d9f0e6d3a2c07fd082bc
Steve Purcell <steve@sanityinc.com>
2013-03-26 22:03:47 +0000

    Fix additional haskell compilation regexp entries

--- init-haskell.el
+++ init-haskell.el
@@ -17,8 +17,13 @@
 (add-hook 'haskell-mode-hook #'flymake-haskell-multi-load)
 
 ;; Make compilation-mode understand "at blah.hs:11:34-50" lines output by GHC
-(add-to-list
- 'compilation-error-regexp-alist-alist
- '(ghc-at-regexp " at \\(.*l?hs\\):\\([0-9]+\\):\\([0-9]+\\)-[0-9]+$" 1 2 3 0 1))
+(eval-after-load 'compile
+  '(progn
+     (let ((alias 'ghc-at-regexp))
+       (add-to-list
+        'compilation-error-regexp-alist-alist
+        (list alias " at \\(.*l?hs\\):\\([0-9]+\\):\\([0-9]+\\)-[0-9]+$" 1 2 3 0 1))
+       (add-to-list
+        'compilation-error-regexp-alist alias))))
 
 (provide 'init-haskell)
