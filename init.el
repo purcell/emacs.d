e7934298c6e8a358de902c24ae09e88161533c56
chen bin <Chen.Bin@optus.com.au>
2013-03-27 10:32:31 +1100

    session-use-pacakge

--- init.el
+++ init.el
@@ -208,7 +208,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(bmkp-last-as-first-bookmark-file "~/.emacs.d/.bookmarks.el")
- '(session-use-package t))
+ '(session-use-package t nil (session)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
