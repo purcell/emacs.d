8c9b04f954882509261f3cc31d8d82f4f507bbe3
Steve Purcell <steve@sanityinc.com>
2013-03-30 20:28:15 +0000

    Disable org-startup-indented, which clashes with auto-complete

--- init-org.el
+++ init-org.el
@@ -19,8 +19,7 @@
       org-agenda-window-setup 'current-window
       org-fast-tag-selection-single-key 'expert
       org-export-kill-product-buffer-when-displayed t
-      org-tags-column 80
-      org-startup-indented t)
+      org-tags-column 80)
 
 
 ; Refile targets include this file and any file contributing to the agenda - up to 5 levels deep
