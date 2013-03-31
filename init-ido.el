abdc8e1cde28f4f134c67ed2545799689ac33a09
Steve Purcell <steve@sanityinc.com>
2013-03-31 18:42:29 +0100

    Only add ido buffer function in older emacsen
    Virtual buffers are better when available!

--- init-ido.el
+++ init-ido.el
@@ -13,21 +13,16 @@
 ;; Allow the same buffer to be open in different frames
 (setq ido-default-buffer-method 'selected-window)
 
-(defun steve-ido-choose-from-recentf ()
-  "Use ido to select a recently opened file from the `recentf-list'"
-  (interactive)
-  (if (and ido-use-virtual-buffers (fboundp 'ido-toggle-virtual-buffers))
-      (ido-switch-buffer)
-    (find-file (ido-completing-read "Open file: " recentf-list nil t))))
+(when (eval-when-compile (< emacs-major-version 24))
+ (defun sanityinc/ido-choose-from-recentf ()
+   "Use ido to select a recently opened file from the `recentf-list'"
+   (interactive)
+   (if (and ido-use-virtual-buffers (fboundp 'ido-toggle-virtual-buffers))
+       (ido-switch-buffer)
+     (find-file (ido-completing-read "Open file: " recentf-list nil t))))
 
-(global-set-key [(meta f11)] 'steve-ido-choose-from-recentf)
+ (global-set-key [(meta f11)] 'sanityinc/ido-choose-from-recentf))
 
-
-;;----------------------------------------------------------------------------
-;; ido completion in M-x
-;;----------------------------------------------------------------------------
-(smex-initialize)
-(global-set-key "\M-x" 'smex)
 
 
 (provide 'init-ido)
