041645f8056d87613fabf6795b24b195bf912a6e
Steve Purcell <steve@sanityinc.com>
2013-04-03 14:07:09 +0100

    Remove non-functional magit-status wrapper function

--- init-git.el
+++ init-git.el
@@ -10,15 +10,8 @@
       magit-process-popup-time 10
       magit-completing-read-function 'magit-ido-completing-read)
 
-(defun magit-status-somedir ()
-  (interactive)
-  (let ((current-prefix-arg t))
-    (magit-status default-directory)))
 
 (global-set-key [(meta f12)] 'magit-status)
-(global-set-key [(shift meta f12)] 'magit-status-somedir)
-
-
 
 (eval-after-load 'magit
   '(progn
