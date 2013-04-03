2c0bf396d1d876222515a66d08e8bd2b19e2d1d5
Steve Purcell <steve@sanityinc.com>
2013-04-03 11:57:30 +0100

    Ignore errors when popping window config after magit-status

--- init-git.el
+++ init-git.el
@@ -34,7 +34,8 @@
        (interactive)
        (kill-buffer)
        (when (get-register :magit-fullscreen)
-         (jump-to-register :magit-fullscreen)))
+         (ignore-errors
+           (jump-to-register :magit-fullscreen))))
 
      (define-key magit-status-mode-map (kbd "q") 'magit-quit-session)))
 
