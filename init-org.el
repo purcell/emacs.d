caf72146c6bcf7ef59da001d6b3f97fa9818f376
Steve Purcell <steve@sanityinc.com>
2013-04-04 10:12:36 +0100

    Bind org-up-element to the keys for backward-up-sexp in org-mode

--- init-org.el
+++ init-org.el
@@ -91,13 +91,12 @@
 
 (eval-after-load 'org
   '(progn
+     (define-key org-mode-map (kbd "C-M-<up>") 'org-up-element)
      (require 'org-exp)
      (require 'org-clock)
      (when *is-a-mac*
        (require 'org-mac-link-grabber)
-       (add-hook 'org-mode-hook
-                 (lambda ()
-                   (define-key org-mode-map (kbd "C-c g") 'omlg-grab-link))))
+       (define-key org-mode-map (kbd "C-c g") 'omlg-grab-link))
      ;;(require 'org-checklist)
      (require 'org-fstree)))
 
