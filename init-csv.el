d06f12bc431336921285d40bbbf4d2b83c0d1345
Steve Purcell <steve@sanityinc.com>
2013-04-09 15:09:05 +0100

    Remove redundant autoload for csv-nav-mode (fixed upstream)

--- init-csv.el
+++ init-csv.el
@@ -2,7 +2,6 @@
 (require-package 'csv-nav)
 
 (add-auto-mode 'csv-mode "\\.[Cc][Ss][Vv]\\'")
-(autoload 'csv-nav-mode "csv-nav" "Major mode for navigating comma-separated value files." t)
 
 (setq csv-separators '("," ";" "|" " "))
 
