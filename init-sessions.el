05f240855b0c9f29f11606aa4ca11a55bea8ed18
Steve Purcell <steve@sanityinc.com>
2013-04-10 13:55:18 +0100

    Leave desktop-save set to the default ('ask-if-new)
    Closes #82

--- init-sessions.el
+++ init-sessions.el
@@ -1,7 +1,6 @@
 ;; save a list of open files in ~/.emacs.d/.emacs.desktop
 ;; save the desktop file automatically if it already exists
 (setq desktop-path '("~/.emacs.d"))
-(setq desktop-save 'if-exists)
 (desktop-save-mode 1)
 (defadvice desktop-read (around trace-desktop-errors)
   (let ((debug-on-error t))
