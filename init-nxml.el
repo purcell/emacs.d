bd0be688ab2aa14f83731116dd54d0eb7387563e
chen bin <Chen.Bin@optus.com.au>
2013-04-10 17:16:31 +1000

    enable nxml-mode for jsp and tag

--- init-nxml.el
+++ init-nxml.el
@@ -1,6 +1,21 @@
 (load-library "rng-auto")
 (add-to-list 'auto-mode-alist
-              (cons (concat "\\." (regexp-opt '("cshtml" "aspx" "xml" "xsd" "sch" "rng" "xslt" "svg" "rss" "bkl" "bkgen" "tpl" "jsp" "inc") t) "\\'")
+              (cons (concat "\\." (regexp-opt '("cshtml"
+                                                "aspx"
+                                                "xml"
+                                                "xsd"
+                                                "sch"
+                                                "rng"
+                                                "xslt"
+                                                "svg"
+                                                "rss"
+                                                "bkl"
+                                                "bkgen"
+                                                "tpl"
+                                                "tmpl"
+                                                "tag"
+                                                "jsp"
+                                                "inc") t) "\\'")
                     'nxml-mode))
 (setq magic-mode-alist (cons '("<\\?xml " . nxml-mode) magic-mode-alist))
 (fset 'html-mode 'nxml-mode)
@@ -34,7 +49,5 @@ indentation rules."
 ;;----------------------------------------------------------------------------
 (add-hook 'nxml-mode-hook (lambda () (tidy-build-menu nxml-mode-map)))
 (add-hook 'html-mode-hook (lambda () (tidy-build-menu html-mode-map)))
-
-(add-auto-mode 'html-mode "\\.(jsp|tmpl)\\'")
 
 (provide 'init-nxml)
