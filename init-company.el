665b82c2a504b7dd7e68e5b19292e2ac25b42079
chen bin <chenbin.sh@gmail.com>
2013-04-11 22:26:03 +1000

    company-mode bug

--- init-company.el
+++ init-company.el
@@ -2,6 +2,7 @@
 ;;(company-mode 1)
 ;; don't know why company-clang-modes does not include c++-mode by default
 (global-set-key (kbd "C-c o") 'company-complete)
+(setq company-require-match nil)
 
 (setq company-clang-modes '(c-mode objc-mode c++-mode))
 (provide 'init-company)
