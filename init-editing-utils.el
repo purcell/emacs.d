f51174244b059519d9ced9a056346ac18838e140
Steve Purcell <steve@sanityinc.com>
2013-04-15 10:29:45 +0100

    Don't define skeleton-insert advice until skeleton is loaded

--- init-editing-utils.el
+++ init-editing-utils.el
@@ -74,11 +74,12 @@
 ;;----------------------------------------------------------------------------
 (require-package 'pointback)
 (global-pointback-mode)
-(defadvice skeleton-insert (before disable-pointback activate)
-  "Disable pointback when using skeleton functions like `sgml-tag'."
-  (when pointback-mode
-    (message "Disabling pointback.")
-    (pointback-mode -1)))
+(eval-after-load 'skeleton
+  '(defadvice skeleton-insert (before disable-pointback activate)
+     "Disable pointback when using skeleton functions like `sgml-tag'."
+     (when pointback-mode
+       (message "Disabling pointback.")
+       (pointback-mode -1))))
 
 
 ;;----------------------------------------------------------------------------
