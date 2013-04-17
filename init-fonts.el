dd9b9879fadda8f00668f7c7350d6010b52a2324
Steve Purcell <steve@sanityinc.com>
2013-04-17 14:44:23 +0100

    Namespace functions in init-fonts.el

--- init-fonts.el
+++ init-fonts.el
@@ -1,38 +1,39 @@
 (require 'cl)
 
 
-(defun font-name-replace-size (font-name new-size)
+(defun sanityinc/font-name-replace-size (font-name new-size)
   (let ((parts (split-string font-name "-")))
     (setcar (nthcdr 7 parts) (format "%d" new-size))
     (mapconcat 'identity parts "-")))
 
-(defun increment-default-font-height (delta)
+(defun sanityinc/increment-default-font-height (delta)
   "Adjust the default font height by DELTA on every frame.
-The pixel size of the frame is kept (approximately) the same.
-DELTA should be a multiple of 10, in the units used by the
-:height face attribute."
+Emacs will keep the pixel size of the frame approximately the
+same.  DELTA should be a multiple of 10, to match the units used
+by the :height face attribute."
   (let* ((new-height (+ (face-attribute 'default :height) delta))
          (new-point-height (/ new-height 10)))
     (dolist (f (frame-list))
       (with-selected-frame f
         ;; Latest 'set-frame-font supports a "frames" arg, but
         ;; we cater to Emacs 23 by looping instead.
-        (set-frame-font (font-name-replace-size (face-font 'default)
-                                                new-point-height)
+        (set-frame-font (sanityinc/font-name-replace-size
+                         (face-font 'default)
+                         new-point-height)
                         t)))
     (set-face-attribute 'default nil :height new-height)
     (message "default font size is now %d" new-point-height)))
 
-(defun increase-default-font-height ()
+(defun sanityinc/increase-default-font-height ()
   (interactive)
-  (increment-default-font-height 10))
+  (sanityinc/increment-default-font-height 10))
 
-(defun decrease-default-font-height ()
+(defun sanityinc/decrease-default-font-height ()
   (interactive)
-  (increment-default-font-height -10))
+  (sanityinc/increment-default-font-height -10))
 
-(global-set-key (kbd "C-M-=") 'increase-default-font-height)
-(global-set-key (kbd "C-M--") 'decrease-default-font-height)
+(global-set-key (kbd "C-M-=") 'sanityinc/increase-default-font-height)
+(global-set-key (kbd "C-M--") 'sanityinc/decrease-default-font-height)
 
 
 
