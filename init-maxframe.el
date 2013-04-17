a09fe1297e9411c4fa6f5042ac0356ce2ca43d62
Steve Purcell <steve@sanityinc.com>
2013-04-17 14:48:13 +0100

    Namespace functions in init-maxframe.el

--- init-maxframe.el
+++ init-maxframe.el
@@ -25,20 +25,20 @@
   "Record the selected frame before creating a new one interactively."
   (setq sanityinc/prev-frame (selected-frame)))
 
-(defun maybe-maximize-frame (&optional frame)
+(defun sanityinc/maybe-maximize-frame (&optional frame)
   (with-selected-frame (or frame (selected-frame))
     (when (and window-system
                sanityinc/prev-frame
-               (maximized-p sanityinc/prev-frame))
+               (sanityinc/maximized-p sanityinc/prev-frame))
       (maximize-frame))))
 
-(add-hook 'after-make-frame-functions 'maybe-maximize-frame)
-(add-hook 'after-init-hook 'maybe-maximize-frame)
+(add-hook 'after-make-frame-functions 'sanityinc/maybe-maximize-frame)
+(add-hook 'after-init-hook 'sanityinc/maybe-maximize-frame)
 
 (defun within-p (a b delta)
   (<= (abs (- b a)) delta))
 
-(defun maximized-p (&optional frame)
+(defun sanityinc/maximized-p (&optional frame)
   (or (not (with-selected-frame (or frame (selected-frame)) window-system))
       (and (within-p (mf-max-display-pixel-width)
                      (frame-pixel-width frame)
