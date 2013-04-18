ff8c9da754db757ce5e05f882e79e7063fc1451c
Steve Purcell <steve@sanityinc.com>
2013-04-18 14:17:28 +0100

    Also set dash docset in sqli buffers

--- init-sql.el
+++ init-sql.el
@@ -9,6 +9,7 @@
          (setq dash-at-point-docset "psql")))
 
      (add-hook 'sql-mode-hook 'sanityinc/maybe-set-dash-db-docset)
+     (add-hook 'sql-interactive-mode-hook 'sanityinc/maybe-set-dash-db-docset)
      (defadvice sql-set-product (after set-dash-docset activate)
        (sanityinc/maybe-set-dash-db-docset))))
 
