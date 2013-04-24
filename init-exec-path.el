8ecdf07ab2b473e4c548e8980536f877797ac379
Steve Purcell <steve@sanityinc.com>
2013-04-24 16:10:44 +0100

    Also copy $LANG and $LC_CTYPE from the shell environment on OS X

--- init-exec-path.el
+++ init-exec-path.el
@@ -1,6 +1,6 @@
 (eval-after-load 'exec-path-from-shell
   '(progn
-     (dolist (var '("SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO"))
+     (dolist (var '("SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO" "LANG" "LC_CTYPE"))
        (add-to-list 'exec-path-from-shell-variables var))))
 
 
