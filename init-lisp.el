ed2346844aec25910be708af9af7618f721070ec
Steve Purcell <steve@sanityinc.com>
2013-04-25 12:28:13 +0100

    Bind C-x C-e to pp-eval-last-sexp in emacs lisp mode

--- init-lisp.el
+++ init-lisp.el
@@ -106,6 +106,7 @@
 (add-to-list 'auto-mode-alist '("archive-contents\\'" . emacs-lisp-mode))
 
 (define-key emacs-lisp-mode-map (kbd "C-x C-a") 'pp-macroexpand-last-sexp)
+(define-key emacs-lisp-mode-map (kbd "C-x C-e") 'pp-eval-last-sexp)
 
 
 ;; ----------------------------------------------------------------------------
