4ab1f8d147ef7f1517afb0974a7f9996e9780209
Steve Purcell <steve@sanityinc.com>
2013-05-09 12:20:16 +0100

    Remove binding of C-` for flymake-goto-next-error

--- init-flymake.el
+++ init-flymake.el
@@ -6,8 +6,6 @@
 ;; at which point buffer-file-name is nil
 (eval-after-load 'flymake
   '(progn
-     (global-set-key (kbd "C-`") 'flymake-goto-next-error)
-
      (defun flymake-can-syntax-check-file (file-name)
        "Determine whether we can syntax check FILE-NAME.
 Return nil if we cannot, non-nil if we can."
