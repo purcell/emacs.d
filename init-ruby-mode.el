f583602de84e4a1513f362f6d6e90c0d8dcf9d65
Steve Purcell <steve@sanityinc.com>
2013-05-14 13:52:30 +0100

    Tidy up init-ruby.el

--- init-ruby-mode.el
+++ init-ruby-mode.el
@@ -1,27 +1,11 @@
+;;; Basic ruby setup
 (require-package 'ruby-mode)
 (require-package 'ruby-hash-syntax)
-(require-package 'flymake-ruby)
-(require-package 'rinari)
-(require-package 'ruby-compilation)
-(require-package 'inf-ruby)
-(require-package 'robe)
-(require-package 'yari)
-(require-package 'yaml-mode)
-(require-package 'flymake-yaml)
-(require-package 'haml-mode)
-(require-package 'mmm-mode)
-
-
-(eval-after-load 'rinari
-  '(diminish 'rinari-minor-mode "Rin"))
 
 (add-auto-mode 'ruby-mode
                "Rakefile\\'" "\\.rake\\'" "\.rxml\\'"
                "\\.rjs\\'" ".irbrc\\'" "\.builder\\'" "\\.ru\\'"
                "\\.gemspec\\'" "Gemfile\\'" "Kirkfile\\'")
-
-
-(autoload 'run-ruby "inf-ruby" "Run an inferior Ruby process")
 
 (setq ruby-use-encoding-map nil)
 
@@ -30,16 +14,40 @@
      (define-key ruby-mode-map (kbd "RET") 'reindent-then-newline-and-indent)
      (define-key ruby-mode-map (kbd "TAB") 'indent-for-tab-command)))
 
+;; Stupidly the non-bundled ruby-mode isn't a derived mode of
+;; prog-mode: we run the latter's hooks anyway in that case.
+(add-hook 'ruby-mode-hook
+          (lambda ()
+            (unless (derived-mode-p 'prog-mode)
+              (run-hooks 'prog-mode-hook))))
 
-;;----------------------------------------------------------------------------
-;; Ruby - flymake
-;;----------------------------------------------------------------------------
+
+
+;;; Inferior ruby
+(require-package 'inf-ruby)
+(autoload 'run-ruby "inf-ruby" "Run an inferior Ruby process")
+
+
+
+;;; Ruby compilation
+(require-package 'ruby-compilation)
+
+(eval-after-load 'ruby-mode
+ '(let ((m ruby-mode-map))
+    (define-key m [S-f7] 'ruby-compilation-this-buffer)
+    (define-key m [f7] 'ruby-compilation-this-test)
+    (define-key m [f6] 'recompile)))
+
+
+
+;;; Ruby flymake
+(require-package 'flymake-ruby)
 (add-hook 'ruby-mode-hook 'flymake-ruby-load)
 
 
-;;----------------------------------------------------------------------------
-;; Ruby - robe
-;;----------------------------------------------------------------------------
+
+;;; Robe
+(require-package 'robe)
 (add-hook 'ruby-mode-hook 'robe-mode)
 (add-hook 'robe-mode-hook
           (lambda ()
@@ -47,16 +55,31 @@
             (set-auto-complete-as-completion-at-point-function)))
 
 
-;;----------------------------------------------------------------------------
-;; Ruby - misc
-;;----------------------------------------------------------------------------
+
+;;; ri support
+(require-package 'yari)
 (defalias 'ri 'yari)
 
+
+
+;;; YAML
+
+(require-package 'yaml-mode)
+(require-package 'flymake-yaml)
 (add-hook 'yaml-mode-hook 'flymake-yaml-load)
 
-;;----------------------------------------------------------------------------
-;; Ruby - erb
-;;----------------------------------------------------------------------------
+
+
+;;; Rails
+(require-package 'rinari)
+(eval-after-load 'rinari
+  '(diminish 'rinari-minor-mode "Rin"))
+(require-package 'haml-mode)
+
+
+
+;;; ERB
+(require-package 'mmm-mode)
 (defun sanityinc/ensure-mmm-erb-loaded ()
   (require 'mmm-erb))
 
@@ -93,6 +116,10 @@
 ;;----------------------------------------------------------------------------
 ;; Ruby - my convention for heredocs containing SQL
 ;;----------------------------------------------------------------------------
+
+;; Needs to run after rinari to avoid clobbering font-lock-keywords?
+
+;; (require-package 'mmm-mode)
 ;; (eval-after-load 'mmm-mode
 ;;   '(progn
 ;;      (mmm-add-classes
@@ -105,27 +132,8 @@
 ;;          :delimiter-mode nil)))
 ;;      (mmm-add-mode-ext-class 'ruby-mode "\\.rb\\'" 'ruby-heredoc-sql)))
 
+;(add-to-list 'mmm-set-file-name-for-modes 'ruby-mode)
 
-;;----------------------------------------------------------------------------
-;; Ruby - compilation
-;;----------------------------------------------------------------------------
-
-; run the current buffer using Shift-F7
-(add-hook 'ruby-mode-hook (lambda () (local-set-key [S-f7] 'ruby-compilation-this-buffer)))
-; run the current test function using F8 key
-(add-hook 'ruby-mode-hook (lambda () (local-set-key [f7] 'ruby-compilation-this-test)))
-
-(add-hook 'ruby-mode-hook (lambda () (local-set-key [f6] 'recompile)))
-
-
-
-
-;; Stupidly the non-bundled ruby-mode isn't a derived mode of
-;; prog-mode: we run the latter's hooks anyway in that case.
-(add-hook 'ruby-mode-hook
-          (lambda ()
-            (unless (derived-mode-p 'prog-mode)
-              (run-hooks 'prog-mode-hook))))
 
 
 (provide 'init-ruby-mode)
