fa99d82223ea3da809aaa36c94076450d939fda4
Steve Purcell <steve@sanityinc.com>
2013-05-15 16:21:43 +0100

    Remove M-c and M-v bindings under OS X.
    M-v clashes with the default key for scrolling up.
    
    It's arguably better to use the standard yank/kill bindings consistently
    within Emacs.

--- init-osx-keys.el
+++ init-osx-keys.el
@@ -11,8 +11,7 @@
     (eval-after-load 'nxml-mode
       '(define-key nxml-mode-map (kbd "M-h") nil))
     (global-set-key (kbd "M-ˍ") 'ns-do-hide-others) ;; what describe-key reports for cmd-option-h
-    (global-set-key (kbd "M-c") 'ns-copy-including-secondary)
-    (global-set-key (kbd "M-v") 'ns-paste-secondary)))
+    ))
 
 
 (provide 'init-osx-keys)
