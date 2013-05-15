49db94466a119c61e9978e69c5a87a82bd202140
Steve Purcell <steve@sanityinc.com>
2013-05-15 16:10:40 +0100

    Enable scroll-preserve-screen-position

--- init-editing-utils.el
+++ init-editing-utils.el
@@ -25,6 +25,7 @@
  make-backup-files nil
  mouse-yank-at-point t
  save-interprogram-paste-before-kill t
+ scroll-preserve-screen-position t
  set-mark-command-repeat-pop t
  show-trailing-whitespace t
  tooltip-delay 1.5
