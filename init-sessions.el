dc0583078268008d221ddd6e8671f10dac5a9c67
Steve Purcell <steve@sanityinc.com>
2013-05-20 16:23:54 +0100

    Record and print the time taken to restore the desktop

--- init-sessions.el
+++ init-sessions.el
@@ -1,9 +1,17 @@
 ;; save a list of open files in ~/.emacs.d/.emacs.desktop
 (setq desktop-path (list user-emacs-directory))
 (desktop-save-mode 1)
-(defadvice desktop-read (around trace-desktop-errors)
+(defadvice desktop-read (around trace-desktop-errors activate)
   (let ((debug-on-error t))
     ad-do-it))
+
+(defadvice desktop-read (around time-restore activate)
+    (let ((start-time (current-time)))
+      (prog1
+          ad-do-it
+        (message "Desktop restored in %.2fms"
+                 (sanityinc/time-subtract-millis (current-time)
+                                                 start-time)))))
 
 
 ;;----------------------------------------------------------------------------
