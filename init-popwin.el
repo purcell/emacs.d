6e9eb461727f767346e47b62cbf98bb37fb47245
chen bin <chenbin.sh@gmail.com>
2013-05-23 14:21:28 +1000

    more conservative popwin configuration

--- init-popwin.el
+++ init-popwin.el
@@ -1,7 +1,19 @@
 (require 'popwin)
+
+(setq popwin:special-display-config nil)
 (setq display-buffer-function 'popwin:display-buffer)
-(push '("^\*helm .+\*$" :regexp t) popwin:special-display-config)
-(push '("^\*helm-.+\*$" :regexp t) popwin:special-display-config)
-(popwin-mode 1)
+
+;; enable popwin-mode globally is too dangerous
+(add-to-list 'popwin:special-display-config '("*Help*"))
+(add-to-list 'popwin:special-display-config '("*Completions*" :noselect t))
+(add-to-list 'popwin:special-display-config '("*compilation*" :noselect t))
+(add-to-list 'popwin:special-display-config '("*Occur*" :noselect t))
+(add-to-list 'popwin:special-display-config '("*Backtrace*"))
+(add-to-list 'popwin:special-display-config '("*Remember*" :stick t))
+(add-to-list 'popwin:special-display-config '("*Org Agenda*"))
+(add-to-list 'popwin:special-display-config '("*sdic*" :noselect))
+(add-to-list 'popwin:special-display-config '("*Apropos*"))
+(add-to-list 'popwin:special-display-config '("*Warnings*"))
+(add-to-list 'popwin:special-display-config '(" *auto-async-byte-compile*" :noselect))
 
 (provide 'init-popwin)
