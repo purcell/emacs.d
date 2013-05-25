fec0977e1e8bacc5739d3564d159c101a3c8cae2
chen bin <chenbin.sh@gmail.com>
2013-05-25 17:32:58 +1000

    use evil in org-mode

--- init-evil.el
+++ init-evil.el
@@ -3,7 +3,41 @@
 ; @see https://github.com/timcharper/evil-surround
 (global-surround-mode 1)
 
-(evil-set-initial-state 'org-mode 'emacs)
+;; (evil-set-initial-state 'org-mode 'emacs)
+;; Remap org-mode meta keys for convenience
+(evil-declare-key 'normal org-mode-map
+    "gh" 'outline-up-heading
+    "gj" 'org-forward-same-level
+    "gk" 'org-backward-same-level
+    "gl" 'outline-next-visible-heading
+    "H" 'org-beginning-of-line ; smarter behaviour on headlines etc.
+    "L" 'org-end-of-line ; smarter behaviour on headlines etc.
+    "t" 'org-todo ; mark a TODO item as DONE
+    ",c" 'org-cycle
+    ",e" 'org-export-dispatch
+    ",n" 'outline-next-visible-heading
+    ",p" 'outline-previous-visible-heading
+    ",t" 'org-set-tags-command
+    "$" 'org-end-of-line ; smarter behaviour on headlines etc.
+    "^" 'org-beginning-of-line ; ditto
+    "-" 'org-ctrl-c-minus ; change bullet style
+    "<" 'org-metaleft ; out-dent
+    ">" 'org-metaright ; indent
+    )
+
+(mapcar (lambda (state)
+          (evil-declare-key state org-mode-map
+                            (kbd "TAB") 'org-cycle
+                            (kbd "M-l") 'org-metaright
+                            (kbd "M-h") 'org-metaleft
+                            (kbd "M-k") 'org-metaup
+                            (kbd "M-j") 'org-metadown
+                            (kbd "M-L") 'org-shiftmetaright
+                            (kbd "M-H") 'org-shiftmetaleft
+                            (kbd "M-K") 'org-shiftmetaup
+                            (kbd "M-J") 'org-shiftmetadown))
+        '(normal insert))
+
 (evil-set-initial-state 'Info-mode 'emacs)
 (evil-set-initial-state 'term-mode 'emacs)
 (evil-set-initial-state 'log-edit-mode 'emacs)
