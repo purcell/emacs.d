2d0c8b003ce525dfb3c48198aed2abfe03a52965
Steve Purcell <steve@sanityinc.com>
2013-06-22 18:54:32 +0100

    Replace an flet with cl-flet

--- init-locales.el
+++ init-locales.el
@@ -1,6 +1,8 @@
+(require 'cl-lib)
+
 (defun locale-is-utf8-p ()
   "Return t iff the \"locale\" command or environment variables prefer UTF-8."
-  (flet ((is-utf8 (v) (and v (string-match "UTF-8" v))))
+  (cl-flet ((is-utf8 (v) (and v (string-match "UTF-8" v))))
     (or (is-utf8 (and (executable-find "locale") (shell-command-to-string "locale")))
         (is-utf8 (getenv "LC_ALL"))
         (is-utf8 (getenv "LC_CTYPE"))
