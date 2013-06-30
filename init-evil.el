c391cc2c90023389d3a54b69782d6c57b06fbabf
Chen Bin <chenbin.sh@gmail.com>
2013-06-30 22:32:01 +1000

    evil hotkey should be similar to emacs hotkey

--- init-evil.el
+++ init-evil.el
@@ -129,9 +129,9 @@ to replace the symbol under cursor"
   "mq" '(lambda () (interactive) (man (concat "-k " (thing-at-point 'symbol))))
   "gg" '(lambda () (interactive) (w3m-search "g" (thing-at-point 'symbol)))
   "q" '(lambda () (interactive) (w3m-search "q" (thing-at-point 'symbol)))
-  "so" 'delete-other-windows
-  "sh" 'split-window-below
-  "sv" 'split-window-right
+  "s1" 'delete-other-windows
+  "s2" 'split-window-below
+  "s3" 'split-window-right
   "su" 'winner-undo
   "hs" 'w3mext-hacker-search
   "gf" 'gtags-find-tag-from-here
