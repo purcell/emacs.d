ccf36aa88132c4a3df0bb770b786fd8392dfb079
Steve Purcell <steve@sanityinc.com>
2013-07-02 13:36:17 +0100

    Log time taken to restore each buffer

--- init-sessions.el
+++ init-sessions.el
@@ -13,6 +13,16 @@
                  (sanityinc/time-subtract-millis (current-time)
                                                  start-time)))))
 
+(defadvice desktop-create-buffer (around time-create activate)
+  (let ((start-time (current-time))
+        (filename (ad-get-arg 1)))
+    (prog1
+        ad-do-it
+      (message "Desktop: %.2fms to restore %s"
+               (sanityinc/time-subtract-millis (current-time)
+                                               start-time)
+               (when filename
+		 (abbreviate-file-name filename))))))
 
 ;;----------------------------------------------------------------------------
 ;; Restore histories and registers after saving
