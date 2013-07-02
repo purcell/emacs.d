85d2989170fed4b61d3e20c2a45efd4a90b362b6
Steve Purcell <steve@sanityinc.com>
2013-07-02 13:41:46 +0100

    A couple of handy keys for buffer navigation

--- init-windows.el
+++ init-windows.el
@@ -43,5 +43,24 @@
 (global-set-key "\C-x|" 'split-window-horizontally-instead)
 (global-set-key "\C-x_" 'split-window-vertically-instead)
 
+
+;; Borrowed from http://postmomentum.ch/blog/201304/blog-on-emacs
+(defun sanityinc/split-window()
+  "Split the window to see the most recent buffer in the other window.
+Call a second time to restore the original window configuration."
+  (interactive)
+  (if (eq last-command 'sanityinc/split-window)
+      (progn
+        (jump-to-register :sanityinc/split-window)
+        (setq this-command 'sanityinc/unsplit-window))
+    (window-configuration-to-register :sanityinc/split-window)
+    (switch-to-buffer-other-window nil)))
+
+(global-set-key (kbd "<f7>") 'sanityinc/split-window)
+(global-set-key (kbd "<f6>")
+                (lambda ()
+                  (interactive)
+                  (switch-to-buffer nil)))
+
 
 (provide 'init-windows)
