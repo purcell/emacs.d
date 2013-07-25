1622e352f183c2f3b2409f0f2e059cc72c53aa13
Steve Purcell <steve@sanityinc.com>
2013-07-25 11:00:46 +0100

    Don't hide details in dired by default

--- init-dired.el
+++ init-dired.el
@@ -1,5 +1,8 @@
 (require-package 'dired+)
 
+(setq diredp-hide-details-initially-flag nil)
+(global-dired-hide-details-mode -1)
+
 (after-load 'dired
   (require 'dired+)
   (setq dired-recursive-deletes 'top)
