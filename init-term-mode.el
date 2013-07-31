2168cbd2e34618334ee4164f89df34bc30e94170
Chen Bin <chenbin.sh@gmail.com>
2013-07-31 23:36:46 +1000

    improve ansi-term

--- init-term-mode.el
+++ init-term-mode.el
@@ -1,5 +1,7 @@
 ;; @see http://stackoverflow.com/questions/2886184/copy-paste-in-emacs-ansi-term-shell/2886539#2886539
 (defun ash-term-hooks ()
+  ;; url clickable
+  (goto-address-mode)
   ;; dabbrev-expand in term
   (define-key term-raw-escape-map "/"
     (lambda ()
@@ -14,4 +16,27 @@
       (interactive)
       (term-send-raw-string (current-kill 0)))))
 (add-hook 'term-mode-hook 'ash-term-hooks)
+
+;; {{@see http://emacs-journey.blogspot.com.au/2012/06/improving-ansi-term.html
+;; kill the buffer when terminal is exited
+(defadvice term-sentinel (around my-advice-term-sentinel (proc msg))
+  (if (memq (process-status proc) '(signal exit))
+      (let ((buffer (process-buffer proc)))
+        ad-do-it
+        (kill-buffer buffer))
+    ad-do-it))
+(ad-activate 'term-sentinel)
+
+;; always use bash
+(defvar my-term-shell "/bin/bash")
+(defadvice ansi-term (before force-bash)
+  (interactive (list my-term-shell)))
+(ad-activate 'ansi-term)
+
+;; utf8
+(defun my-term-use-utf8 ()
+  (set-buffer-process-coding-system 'utf-8-unix 'utf-8-unix))
+(add-hook 'term-exec-hook 'my-term-use-utf8)
+
+;; }}
 (provide 'init-term-mode)
