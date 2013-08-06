f35e1fae5d51f982a859c5a08ac72dad48aab939
Chen Bin <Chen.Bin@optus.com.au>
2013-08-06 17:10:06 +1000

    do NOT delete emails on pop3 server

--- init-gnus.el
+++ init-gnus.el
@@ -3,10 +3,16 @@
   (interactive)
   (gnus-group-list-all-groups 5)
   )
- (add-hook 'gnus-group-mode-hook
-           ;; list all the subscribed groups even they contain zero un-read messages
-           (lambda () (local-set-key "o" 'my-gnus-group-list-subscribed-groups ))
-           )
+
+;; gnus+davmail bug, so I have to use pop3 for davmail
+;; http://permalink.gmane.org/gmane.emacs.gnus.general/83301
+;; but delete all the mails on server is scary
+(setq pop3-leave-mail-on-server t)
+
+(add-hook 'gnus-group-mode-hook
+          ;; list all the subscribed groups even they contain zero un-read messages
+          (lambda () (local-set-key "o" 'my-gnus-group-list-subscribed-groups ))
+          )
 
 (setq message-send-mail-function 'smtpmail-send-it
       smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
