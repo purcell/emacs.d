ccb877ff4939b4d03200da635fafc4326ed109da
Chen Bin <chenbin.sh@gmail.com>
2013-08-08 00:44:41 +1000

    evil hotkey for yas-expand

--- init-evil.el
+++ init-evil.el
@@ -103,6 +103,7 @@
 (define-key evil-insert-state-map (kbd "M-k") 'evil-normal-state)
 (define-key evil-visual-state-map (kbd ",k") 'evil-exit-visual-state)
 (define-key minibuffer-local-map (kbd ",k") 'abort-recursive-edit)
+(define-key evil-insert-state-map (kbd "M-j") 'yas-expand)
 
 (defun evilcvn-change-symbol-in-defun ()
   "mark the region in defun (definition of function) and use string replacing UI in evil-mode
