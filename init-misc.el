c5d49b2582fd39df15543cef2120807f88e89dbf
Chen Bin <chenbin.sh@gmail.com>
2013-08-28 23:30:52 +1000

    vimrc-mode

--- init-misc.el
+++ init-misc.el
@@ -468,6 +468,10 @@ version control automatically"
 
 ;; }}
 
+;; vimrc
+(require 'vimrc-mode)
+(add-to-list 'auto-mode-alist '("\\.?vim\\(rc\\)?$" . vimrc-mode))
+
 (require 'highlight-symbol)
 
 (provide 'init-misc)
