5ebc674d112cbf6ccab2194368844dc363dcfd7e
Chen Bin <chenbin.sh@gmail.com>
2013-08-30 13:16:24 +1000

    email attachment as quickly as possible

--- init-helm.el
+++ init-helm.el
@@ -17,6 +17,7 @@
         (w3m-goto-url . nil)
         (ido-find-file . nil)
         (ido-edit-input . nil)
+        (mml-attach-file . nil)
         (read-file-name . ni)
         (yas/compile-directory . ido)
         (yas-compile-directory . ido)
