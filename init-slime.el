1ae20baece7d5abd74f92103fe6a644e549798e5
Steve Purcell <steve@sanityinc.com>
2013-10-05 19:53:40 +0100

    Make ac-slime work with TAB in the slime REPL

--- init-slime.el
+++ init-slime.el
@@ -8,9 +8,13 @@
 (require-package 'ac-slime)
 (require-package 'hippie-expand-slime)
 
-(defun sanityinc/set-up-slime-repl-auto-complete ()
-  "Bind TAB to `indent-for-tab-command', as in regular Slime buffers."
-  (local-set-key (kbd "TAB") 'indent-for-tab-command))
+
+;;; Lisp buffers
+
+(defun sanityinc/slime-setup ()
+  "Mode setup function for slime lisp buffers."
+  (set-up-slime-hippie-expand)
+  (set-up-slime-ac t))
 
 (after-load 'slime
   (setq slime-protocol-version 'ignore)
@@ -18,22 +22,29 @@
   (slime-setup '(slime-repl slime-fuzzy))
   (setq slime-complete-symbol*-fancy t)
   (setq slime-complete-symbol-function 'slime-fuzzy-complete-symbol)
-  (add-hook 'slime-mode-hook 'set-up-slime-hippie-expand)
-  (add-hook 'slime-mode-hook 'set-up-slime-ac))
+  (add-hook 'slime-mode-hook 'sanityinc/slime-setup))
+
+
+;;; REPL
+
+(defun sanityinc/slime-repl-setup ()
+  "Mode setup function for slime REPL."
+  (sanityinc/lisp-setup)
+  (set-up-slime-hippie-expand)
+  (set-up-slime-ac t)
+  (setq show-trailing-whitespace nil))
 
 (after-load 'slime-repl
   ;; Stop SLIME's REPL from grabbing DEL, which is annoying when backspacing over a '('
-  (defun override-slime-repl-bindings-with-paredit ()
-    (define-key slime-repl-mode-map (read-kbd-macro paredit-backward-delete-key) nil))
+  (define-key slime-repl-mode-map (read-kbd-macro paredit-backward-delete-key) nil)
 
-  (add-hook 'slime-repl-mode-hook 'sanityinc/lisp-setup)
-  (add-hook 'slime-repl-mode-hook 'override-slime-repl-bindings-with-paredit)
-  (add-hook 'slime-repl-mode-hook 'set-up-slime-hippie-expand)
-  (add-hook 'slime-repl-mode-hook (lambda () (setq show-trailing-whitespace nil)))
-  (add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
-  (add-hook 'slime-repl-mode-hook 'sanityinc/set-up-slime-repl-auto-complete)
-  (after-load 'auto-complete
-    (add-to-list 'ac-modes 'slime-repl-mode)))
+  ;; Bind TAB to `indent-for-tab-command', as in regular Slime buffers.
+  (define-key slime-repl-mode-map (kbd "TAB") 'indent-for-tab-command)
+
+  (add-hook 'slime-repl-mode-hook 'sanityinc/slime-repl-setup))
+
+(after-load 'auto-complete
+  (add-to-list 'ac-modes 'slime-repl-mode))
 
 
 (provide 'init-slime)
