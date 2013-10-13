343f04bb3f8a74b3436d54795b77e76763765897
Chen Bin <chenbin.sh@gmail.com>
2013-10-13 21:55:43 +1100

    use mozrepl in right way

--- init-moz.el
+++ init-moz.el
@@ -2,10 +2,16 @@
 
 (defun moz-custom-setup ()
   (moz-minor-mode 1)
+  ;; @see  http://www.emacswiki.org/emacs/MozRepl
+  ;; Example - you may want to add hooks for your own modes.
+  ;; I also add this to python-mode when doing django development.
+  (auto-reload-firefox-on-after-save-hook)
   )
-(add-hook 'javascript-mode-hook 'moz-custom-setup)
+
 (add-hook 'js2-mode-hook 'moz-custom-setup)
 (add-hook 'html-mode-hook 'moz-custom-setup)
+(add-hook 'nxml-mode-hook 'moz-custom-setup)
+(add-hook 'web-mode-hook 'moz-custom-setup)
 
 (eval-after-load 'moz
   '(progn
@@ -15,4 +21,13 @@
                        (comint-send-string (inferior-moz-process)
                                            "BrowserReload();")))
      ))
+
+(defun auto-reload-firefox-on-after-save-hook ()
+  (add-hook 'after-save-hook
+            '(lambda ()
+               (interactive)
+               (comint-send-string (inferior-moz-process)
+                                   "setTimeout(BrowserReload(), '500');"))
+            'append 'local)) ;; buffer-local
+
 (provide 'init-moz)
