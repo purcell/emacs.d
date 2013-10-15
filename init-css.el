17e2fe2e5824f6345d69f7dd2c5e60e339fde3aa
Steve Purcell <steve@sanityinc.com>
2013-10-15 14:13:25 +0100

    Unbundled skewer-less into its own package

--- init-css.el
+++ init-css.el
@@ -45,45 +45,7 @@
 
 ;;; LESS
 (require-package 'less-css-mode)
-
-(require-package 'skewer-mode)
-(autoload 'skewer-eval "skewer-mode")
-
-(defvar sanityinc/skewer-less-mode-map
-  (let ((m (make-sparse-keymap)))
-    ;; for consistency with skewer-css
-    (define-key m (kbd "C-c C-k") 'sanityinc/skewer-less-save-and-reload)
-    m)
-  "Keymap for `sanityinc/skewer-less-mode'.")
-
-(define-minor-mode sanityinc/skewer-less-mode
-  "Minor mode allowing LESS stylesheet manipulation via `skewer-mode'.
-
-Operates by invoking \"less.refresh()\" via skewer whenever the
-buffer is saved.
-
-For this to work properly, the less javascript should be included
-in the target web page, and less should be configured in
-development mode, using:
-
-        var less = {env: \"development\"};"
-  nil
-  " skewer-less"
-  sanityinc/skewer-less-mode-map
-  (if sanityinc/skewer-less-mode
-      (add-hook 'after-save-hook 'sanityinc/skewer-less-reload nil t)
-    (remove-hook 'after-save-hook 'sanityinc/skewer-less-reload t)))
-
-(defun sanityinc/skewer-less-save-and-reload ()
-  "When skewer appears to be active, ask for a reload."
-  (interactive)
-  (save-buffer)
-  (sanityinc/skewer-less-reload))
-
-(defun sanityinc/skewer-less-reload ()
-  "When skewer appears to be active, ask for a reload."
-  (interactive)
-  (skewer-eval "less.refresh();"))
+(require-package 'skewer-less)
 
 
 
