96109d0e050b4a742ca1953fe7df1660854c4a95
Steve Purcell <steve@sanityinc.com>
2013-10-17 12:38:12 +0100

    Update for compatibility with cider (formerly nrepl)

--- init-clojure.el
+++ init-clojure.el
@@ -2,7 +2,7 @@
 (require-package 'clojure-test-mode)
 (require-package 'cljsbuild-mode)
 (require-package 'elein)
-(require-package 'nrepl)
+(require-package 'cider)
 (require-package 'slamhound)
 (require-package 'ac-nrepl)
 
@@ -33,21 +33,21 @@
 
 (setq nrepl-popup-stacktraces nil)
 
-(after-load 'nrepl
-  (add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
-  (add-hook 'nrepl-interaction-mode-hook 'ac-nrepl-setup)
+(after-load 'cider
+  (add-hook 'cider-repl-mode-hook 'ac-nrepl-setup)
+  (add-hook 'cider-interaction-mode-hook 'ac-nrepl-setup)
   (after-load 'auto-complete
-    (add-to-list 'ac-modes 'nrepl-mode))
+    (add-to-list 'ac-modes 'cider-repl-mode))
 
-  (add-hook 'nrepl-mode-hook 'set-auto-complete-as-completion-at-point-function)
-  (add-hook 'nrepl-interaction-mode-hook 'set-auto-complete-as-completion-at-point-function)
-  (add-hook 'nrepl-interaction-mode-hook 'nrepl-turn-on-eldoc-mode)
-  (add-hook 'nrepl-mode-hook 'subword-mode)
-  (add-hook 'nrepl-mode-hook 'paredit-mode)
-  (define-key nrepl-interaction-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc)
+  (add-hook 'cider-repl-mode-hook 'set-auto-complete-as-completion-at-point-function)
+  (add-hook 'cider-interaction-mode-hook 'set-auto-complete-as-completion-at-point-function)
+  (add-hook 'cider-interaction-mode-hook 'nrepl-turn-on-eldoc-mode)
+  (add-hook 'cider-repl-mode-hook 'subword-mode)
+  (add-hook 'cider-repl-mode-hook 'paredit-mode)
+  (define-key cider-interaction-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc)
 
   ;; nrepl isn't based on comint
-  (add-hook 'nrepl-mode-hook
+  (add-hook 'cider-repl-mode-hook
             (lambda () (setq show-trailing-whitespace nil))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
