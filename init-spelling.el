58bf9bd57fc8dcd12bd5fd9cc7893be33418167d
Chen Bin <Chen.Bin@optus.com.au>
2013-10-18 15:14:17 +1100

    do all the flyspell check in memory

--- init-spelling.el
+++ init-spelling.el
@@ -1,6 +1,3 @@
-(require 'flyspell-lazy)
-(flyspell-lazy-mode 1)
-
 ;; if (aspell installed) { use aspell}
 ;; else if (hunspell installed) { use hunspell }
 ;; whatever spell checker I use, I always use English dictionary
@@ -22,6 +19,14 @@
             ispell-extra-args '("-D en_US")
             )
       )))
+
+;; at least at emacs 24.2.1, the *function* flyspell-large-region is buggy!
+;; we set the *flag* flyspell-large-region to the maximum int to make sure that
+;; function is never called.
+;; Besides, *package* flyspell-lazy should be removed because it manipulates on the
+;; flag.
+(setq flyspell-large-region most-positive-fixnum)
+
 (require 'init-flyspell)
 
 
