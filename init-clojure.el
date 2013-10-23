22fd8d0fd578afc8769f3d83718c1cd1b49bc588
Steve Purcell <steve@sanityinc.com>
2013-10-23 07:57:32 +0100

    cider-interaction-mode is now cider-mode

--- init-clojure.el
+++ init-clojure.el
@@ -35,16 +35,16 @@
 
 (after-load 'cider
   (add-hook 'cider-repl-mode-hook 'ac-nrepl-setup)
-  (add-hook 'cider-interaction-mode-hook 'ac-nrepl-setup)
+  (add-hook 'cider-mode-hook 'ac-nrepl-setup)
   (after-load 'auto-complete
     (add-to-list 'ac-modes 'cider-repl-mode))
 
   (add-hook 'cider-repl-mode-hook 'set-auto-complete-as-completion-at-point-function)
-  (add-hook 'cider-interaction-mode-hook 'set-auto-complete-as-completion-at-point-function)
-  (add-hook 'cider-interaction-mode-hook 'nrepl-turn-on-eldoc-mode)
+  (add-hook 'cider-mode-hook 'set-auto-complete-as-completion-at-point-function)
+  (add-hook 'cider-mode-hook 'nrepl-turn-on-eldoc-mode)
   (add-hook 'cider-repl-mode-hook 'subword-mode)
   (add-hook 'cider-repl-mode-hook 'paredit-mode)
-  (define-key cider-interaction-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc)
+  (define-key cider-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc)
 
   ;; nrepl isn't based on comint
   (add-hook 'cider-repl-mode-hook
