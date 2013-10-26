f21db3b0575ff5af42479ddbe53f343daa63a3f6
Chen Bin <chenbin.sh@gmail.com>
2013-10-26 16:37:58 +1100

    flyspell evil hotkey

--- init-evil.el
+++ init-evil.el
@@ -177,6 +177,7 @@ to replace the symbol under cursor"
   "gp" 'gtags-pop-stack
   "gr" 'gtags-find-rtag
   "fb" 'flyspell-buffer
+  "fg" 'flyspell-goto-next-error
   "gy" 'gtags-find-symbol
   "dg" 'djcb-gtags-create-or-update
   "bc" '(lambda () (interactive) (wxhelp-browse-class-or-api (thing-at-point 'symbol)))
