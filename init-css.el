f30563563e3259e3641089880dba9516af79970d
Chen Bin <chenbin.sh@gmail.com>
2013-10-30 15:40:44 +1100

    init-css.el should be compatible with web-mode

--- init-css.el
+++ init-css.el
@@ -1,32 +1,5 @@
-(eval-after-load 'mmm-vars
-  '(progn
-     (mmm-add-group
-      'html-css
-      '((css-cdata
-         :submode css-mode
-         :face mmm-code-submode-face
-         :front "<style[^>]*>[ \t\n]*\\(//\\)?<!\\[CDATA\\[[ \t]*\n?"
-         :back "[ \t]*\\(//\\)?]]>[ \t\n]*</style>"
-         :insert ((?j js-tag nil @ "<style type=\"text/css\">"
-                      @ "\n" _ "\n" @ "</script>" @)))
-        (css
-         :submode css-mode
-         :face mmm-code-submode-face
-         :front "<style[^>]*>[ \t]*\n?"
-         :back "[ \t]*</style>"
-         :insert ((?j js-tag nil @ "<style type=\"text/css\">"
-                      @ "\n" _ "\n" @ "</style>" @)))
-        (css-inline
-         :submode css-mode
-         :face mmm-code-submode-face
-         :front "style=\""
-         :back "\"")))
-     (dolist (mode (list 'html-mode 'web-mode))
-       (mmm-add-mode-ext-class mode "\\.r?html\\(\\.erb\\)?\\'" 'html-css))))
-
-
-
 ;; Colourise CSS colour literals
+;; web-mode does not like rainbow-mode
 (dolist (hook '(css-mode-hook html-mode-hook sass-mode-hook))
   (add-hook hook 'rainbow-mode)
   )
