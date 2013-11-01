ef25d0e276cd0c482a848f84c2fe4114d6179215
Steve Purcell <steve@sanityinc.com>
2013-11-01 08:32:01 +0000

    When ag is available, bind M-? to ag-project globally

--- init-editing-utils.el
+++ init-editing-utils.el
@@ -317,7 +317,8 @@ With arg N, insert N newlines."
 (when (executable-find "ag")
   (require-package 'ag)
   (require-package 'wgrep-ag)
-  (setq-default ag-highlight-search t))
+  (setq-default ag-highlight-search t)
+  (global-set-key (kbd "M-?") 'ag-project))
 
 
 
