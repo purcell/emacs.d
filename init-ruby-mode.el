eb5e5606dc9777218d4d884631f82be543b17c4f
Steve Purcell <steve@sanityinc.com>
2013-11-05 10:42:34 +0000

    Use my new ac-inf-ruby package for auto-complete in inf-ruby

--- init-ruby-mode.el
+++ init-ruby-mode.el
@@ -29,6 +29,12 @@
 
 ;;; Inferior ruby
 (require-package 'inf-ruby)
+(require-package 'ac-inf-ruby)
+(after-load 'auto-complete
+  (add-to-list 'ac-modes 'inf-ruby-mode))
+(add-hook 'inf-ruby-mode-hook 'ac-inf-ruby-enable)
+(after-load 'inf-ruby
+  (define-key inf-ruby-mode-map (kbd "TAB") 'auto-complete))
 
 
 
