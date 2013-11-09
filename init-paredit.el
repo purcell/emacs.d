e894f7201b49441f1306c3df727015e1437684b7
Steve Purcell <steve@sanityinc.com>
2013-11-09 15:41:45 +0000

    Don't override M-? when paredit is active

--- init-paredit.el
+++ init-paredit.el
@@ -17,7 +17,10 @@
   ;; Disable kill-sentence, which is easily confused with the kill-sexp
   ;; binding, but doesn't preserve sexp structure
   (define-key paredit-mode-map [remap kill-sentence] nil)
-  (define-key paredit-mode-map [remap backward-kill-sentence] nil))
+  (define-key paredit-mode-map [remap backward-kill-sentence] nil)
+
+  ;; Allow my global binding of M-? to work when paredit is active
+  (define-key paredit-mode-map (kbd "M-?") nil))
 
 
 ;; Compatibility with other modes
