d43c1f7c7e67dbb833b467063ba84cba829ea3c5
Steve Purcell <steve@sanityinc.com>
2013-11-13 11:37:22 +0000

    Add hint about magit-repo-dirs

--- init-git.el
+++ init-git.el
@@ -12,6 +12,8 @@
  magit-diff-refine-hunk t
  magit-completing-read-function 'magit-ido-completing-read)
 
+;; Hint: customize `magit-repo-dirs' so that you can use C-u M-F12 to
+;; quickly open magit on any one of your projects.
 (global-set-key [(meta f12)] 'magit-status)
 
 (after-load 'magit
