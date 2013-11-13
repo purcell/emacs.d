b81e9be26b779af8d03c0348758d476185cd337b
Steve Purcell <steve@sanityinc.com>
2013-11-13 12:01:51 +0000

    Add git-commit-mode to ac-modes

--- init-auto-complete.el
+++ init-auto-complete.el
@@ -47,7 +47,9 @@
                ac-source-words-in-same-mode-buffers
                ac-source-words-in-all-buffer))
 
-(dolist (mode '(magit-log-edit-mode log-edit-mode org-mode text-mode haml-mode
+(dolist (mode '(magit-log-edit-mode
+                log-edit-mode org-mode text-mode haml-mode
+                git-commit-mode
                 sass-mode yaml-mode csv-mode espresso-mode haskell-mode
                 html-mode nxml-mode sh-mode smarty-mode clojure-mode
                 lisp-mode textile-mode markdown-mode tuareg-mode
