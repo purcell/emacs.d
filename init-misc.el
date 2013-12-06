6eb8ea26b291b7cdf62f6e53b1e67b1462f748c7
Chen Bin <chenbin.sh@gmail.com>
2013-12-06 23:54:50 +1100

    remove after-save-hook

--- init-misc.el
+++ init-misc.el
@@ -4,7 +4,6 @@
 (add-auto-mode 'tcl-mode "Portfile\\'")
 (fset 'yes-or-no-p 'y-or-n-p)
 (add-hook 'find-file-hooks 'goto-address-prog-mode)
-(add-hook 'after-save-hook 'executable-make-buffer-file-executable-if-script-p)
 (setq goto-address-mail-face 'link)
 
 (column-number-mode 1)
