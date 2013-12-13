200e56495a79a95e072d6d9ee3b3d21436e04d56
Chen Bin <chenbin.sh@gmail.com>
2013-12-13 21:34:58 +1100

    upgrade evil-matchit

--- init.el
+++ init.el
@@ -206,6 +206,18 @@
     (time-to-seconds (time-since emacs-load-start-time)))
    )
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(bmkp-last-as-first-bookmark-file "~/.emacs.bmk"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(window-numbering-face ((t (:foreground "DeepPink" :underline "DeepPink" :weight bold))) t))
 ;;; Local Variables:
 ;;; no-byte-compile: t
 ;;; End:
