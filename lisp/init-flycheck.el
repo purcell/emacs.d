27d6a746230e80a7064b2f1dd73bb78f16372bce
Steve Purcell <steve@sanityinc.com>
2013-12-16 12:19:16 +0000

    Revert "Add ruby flycheck checker locally until it is restored upstream"
    This reverts commit 4c387b5e3426f5bbddbbf3054961a2b45a3a27bc.

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -5,25 +5,4 @@
 ;; Override default flycheck triggers
 (setq flycheck-check-syntax-automatically '(save idle-change mode-enabled)
       flycheck-idle-change-delay 0.8)
-
-
-(after-load 'flycheck
-  (flycheck-define-checker ruby
-    "A Ruby syntax checker using the standard (MRI) Ruby interpreter.
-
- See URL `http://www.ruby-lang.org/'."
-    :command ("ruby" "-w" "-c" source)
-    :error-patterns
-    ;; These patterns support output from JRuby, too, to deal with RVM or Rbenv
-    ((error line-start
-            "SyntaxError in " (file-name) ":" line ": " (message)
-            line-end)
-     (warning line-start
-              (file-name) ":" line ":" (optional column ":")
-              " warning: " (message) line-end)
-     (error line-start (file-name) ":" line ": " (message) line-end))
-    :modes (enh-ruby-mode ruby-mode))
-  (add-to-list 'flycheck-checkers 'ruby t))
-
-
 (provide 'init-flycheck)
