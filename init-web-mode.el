5539ceefe48ad26317f6b4fa59708e3d02618b55
Chen Bin <chenbin.sh@gmail.com>
2013-12-19 14:50:38 +1100

    why I disable web-mode for jsp?

--- init-web-mode.el
+++ init-web-mode.el
@@ -2,7 +2,7 @@
 
 (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
-;; (add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
