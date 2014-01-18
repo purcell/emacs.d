500b7db0d05e453f57e01cc0f507f07cd90549da
lutx0528 <lutx0528@thundersoft.com>
2014-01-18 14:54:42 +0800

    先将init.el里面的内容都注释掉，一点点的引入原生配置。

--- init.el
+++ init.el
@@ -10,104 +10,104 @@
 ;;----------------------------------------------------------------------------
 ;; Bootstrap config
 ;;----------------------------------------------------------------------------
-(require 'init-compat)
-(require 'init-utils)
-(require 'init-site-lisp) ;; Must come before elpa, as it may provide package.el
-(require 'init-elpa)      ;; Machinery for installing required packages
-(require 'init-exec-path) ;; Set up $PATH
+;;; (require 'init-compat)
+;;; (require 'init-utils)
+;;; (require 'init-site-lisp) ;; Must come before elpa, as it may provide package.el
+;;; (require 'init-elpa)      ;; Machinery for installing required packages
+;;; (require 'init-exec-path) ;; Set up $PATH
 
 ;;----------------------------------------------------------------------------
 ;; Load configs for specific features and modes
 ;;----------------------------------------------------------------------------
 
-(require-package 'wgrep)
-(require-package 'project-local-variables)
-(require-package 'diminish)
-(require-package 'scratch)
-(require-package 'mwe-log-commands)
+;;; (require-package 'wgrep)
+;;; (require-package 'project-local-variables)
+;;; (require-package 'diminish)
+;;; (require-package 'scratch)
+;;; (require-package 'mwe-log-commands)
 
-(require 'init-frame-hooks)
-(require 'init-xterm)
-(require 'init-themes)
-(require 'init-osx-keys)
-(require 'init-gui-frames)
-(require 'init-maxframe)
-(require 'init-proxies)
-(require 'init-dired)
-(require 'init-isearch)
-(require 'init-uniquify)
-(require 'init-ibuffer)
-(require 'init-flycheck)
+;;; (require 'init-frame-hooks)
+;;; (require 'init-xterm)
+;;; (require 'init-themes)
+;;; (require 'init-osx-keys)
+;;; (require 'init-gui-frames)
+;;; (require 'init-maxframe)
+;;; (require 'init-proxies)
+;;; (require 'init-dired)
+;;; (require 'init-isearch)
+;;; (require 'init-uniquify)
+;;; (require 'init-ibuffer)
+;;; (require 'init-flycheck)
 
-(require 'init-recentf)
-(require 'init-ido)
-(require 'init-hippie-expand)
-(require 'init-auto-complete)
-(require 'init-windows)
-(require 'init-sessions)
-(require 'init-fonts)
-(require 'init-mmm)
+;;; (require 'init-recentf)
+;;; (require 'init-ido)
+;;; (require 'init-hippie-expand)
+;;; (require 'init-auto-complete)
+;;; (require 'init-windows)
+;;; (require 'init-sessions)
+;;; (require 'init-fonts)
+;;; (require 'init-mmm)
 
-(require 'init-editing-utils)
+;;; (require 'init-editing-utils)
 
-(require 'init-darcs)
-(require 'init-git)
+;;; (require 'init-darcs)
+;;; (require 'init-git)
 
-(require 'init-crontab)
-(require 'init-textile)
-(require 'init-markdown)
-(require 'init-csv)
-(require 'init-erlang)
-(require 'init-javascript)
-(require 'init-php)
-(require 'init-org)
-(require 'init-nxml)
-(require 'init-css)
-(require 'init-haml)
-(require 'init-python-mode)
-(require 'init-haskell)
-(require 'init-ruby-mode)
-(require 'init-rails)
-(require 'init-sql)
+;;; (require 'init-crontab)
+;;; (require 'init-textile)
+;;; (require 'init-markdown)
+;;; (require 'init-csv)
+;;; (require 'init-erlang)
+;;; (require 'init-javascript)
+;;; (require 'init-php)
+;;; (require 'init-org)
+;;; (require 'init-nxml)
+;;; (require 'init-css)
+;;; (require 'init-haml)
+;;; (require 'init-python-mode)
+;;; (require 'init-haskell)
+;;; (require 'init-ruby-mode)
+;;; (require 'init-rails)
+;;; (require 'init-sql)
 
-(require 'init-paredit)
-(require 'init-lisp)
-(require 'init-slime)
-(require 'init-clojure)
-(require 'init-common-lisp)
+;;; (require 'init-paredit)
+;;; (require 'init-lisp)
+;;; (require 'init-slime)
+;;; (require 'init-clojure)
+;;; (require 'init-common-lisp)
 
-(when *spell-check-support-enabled*
-  (require 'init-spelling))
+;;; (when *spell-check-support-enabled*
+;;;   (require 'init-spelling))
 
-(require 'init-marmalade)
-(require 'init-misc)
+;;; (require 'init-marmalade)
+;;; (require 'init-misc)
 
-(require 'init-dash)
-(require 'init-ledger)
+;;; (require 'init-dash)
+;;; (require 'init-ledger)
 ;; Extra packages which don't require any configuration
 
-(require-package 'gnuplot)
-(require-package 'lua-mode)
-(require-package 'htmlize)
-(require-package 'dsvn)
-(when *is-a-mac*
-  (require-package 'osx-location))
-(require-package 'regex-tool)
+;;; (require-package 'gnuplot)
+;;; (require-package 'lua-mode)
+;;; (require-package 'htmlize)
+;;; (require-package 'dsvn)
+;;; (when *is-a-mac*
+;;;   (require-package 'osx-location))
+;;; (require-package 'regex-tool)
 
 ;;----------------------------------------------------------------------------
 ;; Allow access from emacsclient
 ;;----------------------------------------------------------------------------
-(require 'server)
-(unless (server-running-p)
-  (server-start))
+;;; (require 'server)
+;;; (unless (server-running-p)
+;;;   (server-start))
 
 
 ;;----------------------------------------------------------------------------
 ;; Variables configured via the interactive 'customize' interface
 ;;----------------------------------------------------------------------------
-(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-(when (file-exists-p custom-file)
-  (load custom-file))
+;;; (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+;;; (when (file-exists-p custom-file)
+;;;   (load custom-file))
 
 
 ;;----------------------------------------------------------------------------
@@ -124,9 +124,9 @@
 (require 'init-locales)
 
 (add-hook 'after-init-hook
-          (lambda ()
-            (message "init completed in %.2fms"
-                     (sanityinc/time-subtract-millis after-init-time before-init-time))))
+ (lambda ()
+  (message "init completed in %.2fms"
+   (sanityinc/time-subtract-millis after-init-time before-init-time))))
 
 
 (provide 'init)
---
+++ lisp/init-local.el
@@ -0,0 +1,66 @@
+
+;;去掉工具栏
+(tool-bar-mode nil)
+
+;;不要滚动栏，现在都用滚轴鼠标了，可以不用滚动栏了
+(scroll-bar-mode nil)
+
+;;# Begin # 状态栏设置 
+;; 在状态栏显示日期时间
+;;(setq display-time-day-and-date t)
+;;(display-time)
+
+;; 在mode-line上用彩色显示当前buffer数
+(when window-system
+  (copy-face 'region 'region-invert)
+  (invert-face 'region-invert))
+(size-indication-mode 1)
+(setq-default 
+ mode-line-buffer-identification 
+ (propertized-buffer-identification "%b"))
+(setq mode-line-format-bak mode-line-format)
+(setq mode-line t)
+(defun toggle-mode-line ()
+  "Toggle mode-line."
+  (interactive)
+  (if mode-line
+      (setq-default mode-line-format nil)
+    (setq-default mode-line-format mode-line-format-bak))
+  (setq mode-line (not mode-line)))
+
+;; 标题栏设置
+(defun frame-title-string ()
+  "Return the file name of current buffer, using ~ if under home directory"
+  (let
+      ((fname (or 
+	       (buffer-file-name (current-buffer))
+	       (buffer-name)))
+       (max-len 100))
+    (when (string-match (getenv "HOME") fname)
+      (setq fname (replace-match "~" t t fname)))
+    (if (&gt; (length fname) max-len)
+	(setq fname
+	      (concat "..."
+		      (substring fname (- (length fname) max-len)))))
+    fname)))
+(setq frame-title-format 
+      '(""
+	(:eval (or (buffer-file-name) (buffer-name)))))
+;; End 标题栏设置
+
+(setq auto-save-default nil)
+(setq font-lock-maximum-decoration t)
+(setq global-font-lock-mode t)
+(setq line-prefix (quote(80 . t)))
+(setq line-prefix (quote(80 . t)))
+(setq wrap-prefix (quote(80 . t)))
+(size-indication-mode t)
+(setq truncate-partial-width-windows nil)
+(setq visual-line-mode nil)
+(setq linum-mode t)
+
+;;按Tab的时候，嘘～～，闭嘴!!
+(setq visible-bell t)
+
+
+(provide 'init-local)
