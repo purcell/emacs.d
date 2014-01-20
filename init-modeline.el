d436cb9c4378dc15cb30bb8cdf104d9fbae76a9a
Chen Bin <chenbin.sh@gmail.com>
2014-01-20 13:54:56 +1100

    use default mode-line default font

--- init-modeline.el
+++ init-modeline.el
@@ -5,7 +5,7 @@
 (setq-default mode-line-format
   (list
     ;; the buffer name; the file name as a tool tip
-    '(:eval (propertize "%b " 'face 'font-lock-type-face
+    '(:eval (propertize "%b " 'face 'mode-line-default-face
         'help-echo (buffer-file-name)))
 
     ;; line and column
@@ -18,32 +18,32 @@
     ;; the current major mode for the buffer.
     "["
 
-    '(:eval (propertize "%m" 'face 'font-lock-string-face
+    '(:eval (propertize "%m" 'face 'mode-line-default-face
               'help-echo buffer-file-coding-system))
     " "
 
 
     ;; insert vs overwrite mode, input-method in a tooltip
     '(:eval (propertize (if overwrite-mode "Ovr" "Ins")
-              'face 'font-lock-preprocessor-face
+              'face 'mode-line-default-face
               'help-echo (concat "Buffer is in "
                            (if overwrite-mode "overwrite" "insert") " mode")))
 
     ;; was this buffer modified since the last save?
     '(:eval (when (buffer-modified-p)
               (concat ","  (propertize "Mod"
-                             'face 'font-lock-warning-face
+                             'face 'mode-line-default-face
                              'help-echo "Buffer has been modified"))))
 
     ;; is this buffer read-only?
     '(:eval (when buffer-read-only
               (concat ","  (propertize "RO"
-                             'face 'font-lock-type-face
+                             'face 'mode-line-default-face
                              'help-echo "Buffer is read-only"))))
     "] "
 
     ;;global-mode-string, org-timer-set-timer in org-mode need this
-    (propertize "%M" 'face 'font-lock-type-face)
+    (propertize "%M" 'face 'mode-line-default-face)
 
     " --"
     ;; i don't want to see minor-modes; but if you want, uncomment this:
