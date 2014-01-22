9e787ba5e7fe8ae7cb26708ed9fea12c3a33949a
Chen Bin <chenbin.sh@gmail.com>
2014-01-22 21:56:31 +1100

    simpler mode-line

--- init-modeline.el
+++ init-modeline.el
@@ -5,7 +5,7 @@
 (setq-default mode-line-format
   (list
     ;; the buffer name; the file name as a tool tip
-    '(:eval (propertize "%b " 'face 'mode-line-default-face
+    '(:eval (propertize "%b " 'face nil
         'help-echo (buffer-file-name)))
 
     ;; line and column
@@ -18,32 +18,32 @@
     ;; the current major mode for the buffer.
     "["
 
-    '(:eval (propertize "%m" 'face 'mode-line-default-face
+    '(:eval (propertize "%m" 'face nil
               'help-echo buffer-file-coding-system))
     " "
 
 
     ;; insert vs overwrite mode, input-method in a tooltip
     '(:eval (propertize (if overwrite-mode "Ovr" "Ins")
-              'face 'mode-line-default-face
+              'face nil
               'help-echo (concat "Buffer is in "
                            (if overwrite-mode "overwrite" "insert") " mode")))
 
     ;; was this buffer modified since the last save?
     '(:eval (when (buffer-modified-p)
               (concat ","  (propertize "Mod"
-                             'face 'mode-line-default-face
+                             'face nil
                              'help-echo "Buffer has been modified"))))
 
     ;; is this buffer read-only?
     '(:eval (when buffer-read-only
               (concat ","  (propertize "RO"
-                             'face 'mode-line-default-face
+                             'face nil
                              'help-echo "Buffer is read-only"))))
     "] "
 
     ;;global-mode-string, org-timer-set-timer in org-mode need this
-    (propertize "%M" 'face 'mode-line-default-face)
+    (propertize "%M" 'face nil)
 
     " --"
     ;; i don't want to see minor-modes; but if you want, uncomment this:
--- init-spelling.el
+++ init-spelling.el
@@ -42,13 +42,12 @@
 ;; whatever spell checker I use, I always use English dictionary
 ;; I prefer use aspell because:
 ;; 1. aspell is older
-;; 2. looks Kevin Atkinson still get some roadmap for aspell:
+;; 2. looks Kevin Atkinson still get some road map for aspell:
 ;; @see http://lists.gnu.org/archive/html/aspell-announce/2011-09/msg00000.html
 (if (executable-find "aspell")
     (setq ispell-program-name "aspell"
-          ;; ispell-extra-args '("--sug-mode=ultra")
-          ;; force the English dictionary
-          ispell-extra-args '("--sug-mode=ultra" "--lang=en_US"  "--run-together" "--run-together-limit=5" "--run-together-min=2"))
+          ;; force the English dictionary, support Camel Case spelling check (tested with aspell 0.6)
+          ispell-extra-args '("--sug-mode=ultra" "--lang=en_US" "--run-together" "--run-together-limit=5" "--run-together-min=2"))
   (if (executable-find "hunspell")
       (setq ispell-program-name "hunspell"
             ispell-extra-args '("-D en_US")
