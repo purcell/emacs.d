7647073ef6be2c6be0a1456f350ea8abbdeca6c8
Steve Purcell <steve@sanityinc.com>
2014-02-08 08:04:00 +0000

    Add check for emacs version on startup (see #161)

--- init.el
+++ init.el
@@ -1,5 +1,10 @@
+
 ;;; This file bootstraps the configuration, which is divided into
 ;;; a number of other files.
+
+(let ((minver 23))
+  (unless (>= emacs-major-version minver)
+    (error "Your Emacs is too old -- this config requires v%s or higher" minver)))
 
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 (require 'init-benchmarking) ;; Measure startup time
