e87aeaf24fa2a6e6f3a504e6c7266a69b1407f04
Chen Bin <chenbin.sh@gmail.com>
2014-02-13 19:47:26 +1100

    clean ruby-mode

--- init-ruby-mode.el
+++ init-ruby-mode.el
@@ -1,6 +1,3 @@
-(eval-after-load 'rinari
-  '(diminish 'rinari-minor-mode "Rin"))
-
 (add-auto-mode 'ruby-mode "\\.rb\\'" "Rakefile\\'" "\.rake\\'" "\.rxml\\'" "\.rjs\\'" ".irbrc\\'" "\.builder\\'" "\.ru\\'" "\.gemspec\\'" "Gemfile\\'")
 
 
@@ -8,54 +5,22 @@
 
 (setq ruby-use-encoding-map nil)
 
-(eval-after-load 'ruby-mode
-  '(progn
-     (define-key ruby-mode-map (kbd "RET") 'reindent-then-newline-and-indent)
-     (define-key ruby-mode-map (kbd "TAB") 'indent-for-tab-command)
-     (setq compile-command "rake ")
-     ))
-
-
-;;----------------------------------------------------------------------------
-;; Ruby - flymake
-;;----------------------------------------------------------------------------
-(add-hook 'ruby-mode-hook 'flymake-ruby-load)
-
-
-;;----------------------------------------------------------------------------
-;; Ruby - robe
-;;----------------------------------------------------------------------------
-(add-hook 'ruby-mode-hook 'robe-mode)
 (add-hook 'robe-mode-hook
           (lambda ()
-            ;;(add-to-list 'ac-sources 'ac-source-robe)
-            (setq completion-at-point-functions '(auto-complete))))
+            ;; {{ rails stuff
+            (require 'rinari)
+            (setq compile-command "rake ")
+            (robe-mode)
+            (flymake-ruby-load)))
 
-;;----------------------------------------------------------------------------
-;; Ruby - misc
-;;----------------------------------------------------------------------------
+;; doc look up
 (defalias 'ri 'yari)
-
 
 ;;----------------------------------------------------------------------------
 ;; Ruby - erb
 ;;----------------------------------------------------------------------------
 (add-to-list 'auto-mode-alist '("\\.rhtml\\(\\.erb\\)?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.jst\\.ejs\\'"  . web-mode))
-
-;;----------------------------------------------------------------------------
-;; Ruby - compilation
-;;----------------------------------------------------------------------------
-
-; run the current buffer using Shift-F7
-(add-hook 'ruby-mode-hook (lambda () (local-set-key [S-f7] 'ruby-compilation-this-buffer)))
-; run the current test function using F8 key
-(add-hook 'ruby-mode-hook (lambda () (local-set-key [f7] 'ruby-compilation-this-test)))
-
-(add-hook 'ruby-mode-hook (lambda () (local-set-key [f6] 'recompile)))
-
-;; {{ rails stuff
-(require 'rinari)
 
 (defun update-rails-ctags ()
   (interactive)
