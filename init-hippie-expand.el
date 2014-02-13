ee1da182bd254b7bb3743c2adee3cd8898120fc4
Chen Bin <chenbin.sh@gmail.com>
2014-02-13 21:26:48 +1100

    clean code

--- init-hippie-expand.el
+++ init-hippie-expand.el
@@ -17,31 +17,6 @@
            (point))))
     p))
 
-(defun lob/try-complete-ispell (old)
-  "Making hippie-expand a little better for text-mode and stuff.
-I based this code off `try-complete-lisp-symbol` and
-`ispell-complete-word`"
-  (when (lob/is-editing-english)
-    (unless old
-      (he-init-string (he-lisp-symbol-beg) (point))
-      (if (not (he-string-member he-search-string he-tried-table))
-          (setq he-tried-table (cons he-search-string he-tried-table)))
-      (setq he-expand-list
-            (and (not (equal he-search-string ""))
-                 (lookup-words he-search-string ispell-complete-word-dict))))
-
-      (while (and he-expand-list
-                  (he-string-member (car he-expand-list) he-tried-table))
-        (setq he-expand-list (cdr he-expand-list)))
-      (if (null he-expand-list)
-          (progn
-            (if old (he-reset-string))
-            ())
-        (progn
-          (he-substitute-string (car he-expand-list))
-          (setq he-expand-list (cdr he-expand-list))
-          t))))
-
 
 ;; The actual expansion function
 (defun try-expand-tag (old)
