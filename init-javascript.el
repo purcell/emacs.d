85092f9e70b982623b8ccd30fc219895d3af32a9
Chen Bin <chenbin.sh@gmail.com>
2014-03-02 16:37:13 +1100

    js2-mode respect jslint config

--- init-javascript.el
+++ init-javascript.el
@@ -61,6 +61,8 @@
   )
 ;; }}
 
+(add-hook 'coffee-mode-hook 'flymake-coffee-load)
+
 ;; standard javascript-mode
 (setq javascript-indent-level preferred-javascript-indent-level)
 
@@ -82,6 +84,21 @@ sudo pip install jsbeautifier"
     (goto-char orig-point)))
 ;; }}
 
-(add-hook 'coffee-mode-hook 'flymake-coffee-load)
+;; After js2 has parsed a js file, we look for jslint globals decl comment ("/* global Fred, _, Harry */") and
+;; add any symbols to a buffer-local var of acceptable global vars
+;; Note that we also support the "symbol: true" way of specifying names via a hack (remove any ":true"
+;; to make it look like a plain decl, and any ':false' are left behind so they'll effectively be ignored as
+;; you can;t have a symbol called "someName:false"
+(add-hook 'js2-post-parse-callbacks
+          (lambda ()
+            (when (> (buffer-size) 0)
+              (let ((btext (replace-regexp-in-string
+                            ": *true" " "
+                            (replace-regexp-in-string "[\n\t ]+" " " (buffer-substring-no-properties 1 (buffer-size)) t t))))
+                (mapc (apply-partially 'add-to-list 'js2-additional-externs)
+                      (split-string
+                       (if (string-match "/\\* *global *\\(.*?\\) *\\*/" btext) (match-string-no-properties 1 btext) "")
+                       " *, *" t))
+                ))))
 
 (provide 'init-javascript)
