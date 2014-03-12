860e60d59f7bef74646fed8db59044d9f8ae9ca9
Chen Bin <chenbin.sh@gmail.com>
2014-03-12 22:56:32 +1100

    flymake temp file should be in /tmp/

--- init-elisp.el
+++ init-elisp.el
@@ -9,12 +9,39 @@
 
 (add-hook 'emacs-lisp-mode-hook 'elisp-mode-hooks)
 
+;; @see http://blog.urth.org/2011/06/02/flymake-versus-the-catalyst-restarter/
+(defun flymake-create-temp-intemp (file-name prefix)
+  "Return file name in temporary directory for checking
+   FILE-NAME. This is a replacement for
+   `flymake-create-temp-inplace'. The difference is that it gives
+   a file name in `temporary-file-directory' instead of the same
+   directory as FILE-NAME.
+
+   For the use of PREFIX see that function.
+
+   Note that not making the temporary file in another directory
+   \(like here) will not if the file you are checking depends on
+   relative paths to other files \(for the type of checks flymake
+   makes)."
+  (unless (stringp file-name)
+    (error "Invalid file-name"))
+  (or prefix
+      (setq prefix "flymake"))
+  (let* ((name (concat
+                (file-name-nondirectory
+                 (file-name-sans-extension file-name))
+                "_" prefix))
+         (ext  (concat "." (file-name-extension file-name)))
+         (temp-name (make-temp-file name nil ext))
+         )
+    (flymake-log 3 "create-temp-intemp: file=%s temp=%s" file-name temp-name)
+    temp-name))
 
 ;; do not use elisplint
 (defun flymake-elisp-init ()
   (unless (string-match "^ " (buffer-name))
     (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                         'flymake-create-temp-inplace))
+                         'flymake-create-temp-intemp))
            (local-file (file-relative-name
                          temp-file
                          (file-name-directory buffer-file-name))))
