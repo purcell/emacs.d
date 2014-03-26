3fe5fd2fbbf45879f34781eeef3aa30f4f79a1fc
Steve Purcell <steve@sanityinc.com>
2014-03-26 12:02:23 +0000

    Return to using switch-window on C-x o

--- lisp/init-windows.el
+++ lisp/init-windows.el
@@ -9,6 +9,7 @@
 (require-package 'switch-window)
 (require 'switch-window)
 (setq switch-window-shortcut-style 'alphabet)
+(global-set-key (kbd "C-x o") 'switch-window)
 
 
 ;;----------------------------------------------------------------------------
