4f375a8d2d06d438be6f7cbc5f6343fce86c8791
Chen Bin <chenbin.sh@gmail.com>
2014-04-08 12:43:48 +1000

    more angular scripts

---
+++ snippets/rhtml-mode/script-angular.js
@@ -0,0 +1,6 @@
+# -*- mode: snippet -*-
+# name: <script src="angular.js">...</script>
+# key: script
+# contributor: Chen Bin <chenbin DOT sh AT gmail>
+# --
+<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/angular.js/1.3.0-beta.3/angular${1:.min}.js"></script>
