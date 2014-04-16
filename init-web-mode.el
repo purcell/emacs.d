6dcf45a415e9e245f02da23b2c21f38dc2bfd7c2
Chen Bin <chenbin.sh@gmail.com>
2014-04-16 13:17:28 +1000

    ember.js templates

--- init-web-mode.el
+++ init-web-mode.el
@@ -2,6 +2,7 @@
 
 (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.tpl\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
