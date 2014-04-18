38fa84215d4b5a534f0611f04cd65d776126d54c
Chen Bin <chenbin.sh@gmail.com>
2014-04-18 22:17:37 +1000

    ~/.bashrc use sh-mode

--- init-misc.el
+++ init-misc.el
@@ -151,8 +151,9 @@
 ;; Misc config - yet to be placed in separate files
 ;;----------------------------------------------------------------------------
 ;; {{ shell and conf
-(add-to-list 'auto-mode-alist '("\\.zsh\\'" . sh-mode))
 (add-to-list 'auto-mode-alist '("\\.[a-zA-Z]+rc$" . conf-mode))
+(add-to-list 'auto-mode-alist '("\\.zsh\\'" . sh-mode))
+(add-to-list 'auto-mode-alist '("\\.bashrc\\'" . sh-mode))
 ;; }}
 
 
