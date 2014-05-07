f819735dc3b652c45da1ca5fb1d33b407b52fc31
Sherman Hsu <sherman_hsu@5vtechnologies.com>
2014-05-07 13:58:47 +0800

    init-pomodoro: Disable sound notification

--- lisp/init-pomodoro.el
+++ lisp/init-pomodoro.el
@@ -8,6 +8,7 @@
 (setq pomodoro-break-time 5)
 (setq pomodoro-long-break-time 20)
 (setq pomodoro-work-time 25)
+(setq pomodoro-play-sounds nil)
 (setq-default mode-line-format
               (cons '(pomodoro-mode-line-string pomodoro-mode-line-string)
                     mode-line-format))
