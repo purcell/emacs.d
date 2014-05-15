6dce1e6596a1283d1689da6f69ff1fff57c5469b
Chen Bin <chenbin.sh@gmail.com>
2014-05-15 21:42:06 +1000

    company-ropemacs sucks

--- init-company.el
+++ init-company.el
@@ -17,8 +17,13 @@
 (eval-after-load 'company
   '(progn
      (add-to-list 'company-backends 'company-cmake)
-     ;; I donot like the downcase code in company-dabbrev
-     (setq company-backends (delete 'company-dabbrev company-backends))
+     ;; can't work with TRAMP
+     (setq company-backends (delete 'company-ropemacs company-backends))
+     (setq company-backends (delete 'company-capf company-backends))
+     ;; I don't like the downcase word in company-dabbrev
+     ;; for languages use camel case naming convention
+     (setq company-dabbrev-downcase nil)
+     (setq company-show-numbers t)
      (setq company-begin-commands '(self-insert-command))
      (setq company-idle-delay 0.2)
      (setq company-clang-insert-arguments nil)
