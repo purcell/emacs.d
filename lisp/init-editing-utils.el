972d74909a9405e3b8661325eea8c3f352a85912
Steve Purcell <steve@sanityinc.com>
2014-05-20 10:17:03 +0100

    Use shift-ret for newline at end of line

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -65,14 +65,25 @@
 (global-whitespace-cleanup-mode t)
 
 
+;;; Newline behaviour
 
 (global-set-key (kbd "RET") 'newline-and-indent)
+(defun sanityinc/newline-at-end-of-line ()
+  "Move to end of line, enter a newline, and reindent."
+  (interactive)
+  (move-end-of-line 1)
+  (newline-and-indent))
+
+(global-set-key (kbd "<S-return>") 'sanityinc/newline-at-end-of-line)
+
+
 
 (when (eval-when-compile (string< "24.3.1" emacs-version))
   ;; https://github.com/purcell/emacs.d/issues/138
   (after-load 'subword
     (diminish 'subword-mode)))
 
+
 
 (when (fboundp 'global-prettify-symbols-mode)
   (global-prettify-symbols-mode))
