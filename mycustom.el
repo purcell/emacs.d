a59ae44cae633e8f891037ac4c814c5f560b53a6
Sherman Hsu <sherman_hsu@5vtechnologies.com>
2014-05-20 11:11:33 +0800

    evil:custom: Enable evil-symbol-word-search

--- mycustom.el
+++ mycustom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(evil-symbol-word-search t)
  '(evil-toggle-key "C-c z")
  '(evil-want-C-w-delete t)
  '(evil-want-C-w-in-emacs-state nil)
