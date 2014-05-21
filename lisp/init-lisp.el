75f2a8f7c19ace39d105026297dc8e4a31f7284a
Steve Purcell <steve@sanityinc.com>
2014-05-21 13:02:12 +0100

    Disable auto-compile pending an upstream fix for #181

--- lisp/init-lisp.el
+++ lisp/init-lisp.el
@@ -64,9 +64,9 @@
 ;; Automatic byte compilation
 ;; ----------------------------------------------------------------------------
 
-(require-package 'auto-compile)
-(auto-compile-on-save-mode 1)
-(auto-compile-on-load-mode 1)
+;; (require-package 'auto-compile)
+;; (auto-compile-on-save-mode 1)
+;; (auto-compile-on-load-mode 1)
 
 ;; ----------------------------------------------------------------------------
 ;; Highlight current sexp
