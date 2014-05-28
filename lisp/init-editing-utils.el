61053e9329938d57764f85f1b99d340f78b33181
Sherman Hsu <sherman_hsu@5vtechnologies.com>
2014-05-28 19:03:20 +0800

    Revert "editing-utils: Bind M-RET to c-indent-new-comment-line"
    This reverts commit 9c4e210e83cb89e3fbb13c6c2f962da3dd6fb80e.

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -351,8 +351,4 @@ With arg N, insert N newlines."
   (setq buffer-display-table (make-display-table))
   (aset buffer-display-table ?\^M []))
 
-
-;; Bind M-RET to c-indent-new-comment-line
-(global-set-key (kbd "M-RET") 'c-indent-new-comment-line)
-
 (provide 'init-editing-utils)
