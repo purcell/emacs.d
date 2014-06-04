4cbc24ceac7d4f109d12d7c630a6044e3db48782
Steve Purcell <steve@sanityinc.com>
2014-06-04 19:16:36 +0100

    Prefix "pp-xml-region" function

--- lisp/init-nxml.el
+++ lisp/init-nxml.el
@@ -13,7 +13,7 @@
 
 
 ;; See: http://sinewalker.wordpress.com/2008/06/26/pretty-printing-xml-with-emacs-nxml-mode/
-(defun pp-xml-region (begin end)
+(defun sanityinc/pp-xml-region (begin end)
   "Pretty format XML markup in region. The function inserts
 linebreaks to separate tags that have nothing but whitespace
 between them.  It then indents the markup by using nxml's
