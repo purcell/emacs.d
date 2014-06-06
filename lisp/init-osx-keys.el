679e2e3b23d452cbf703a4a6d702a0c6310162c8
Steve Purcell <steve@sanityinc.com>
2014-06-06 08:08:44 +0100

    Drop superfluous translation-map entry (see #72)

--- lisp/init-osx-keys.el
+++ lisp/init-osx-keys.el
@@ -2,7 +2,6 @@
   (setq mac-command-modifier 'meta)
   (setq mac-option-modifier 'none)
   (setq default-input-method "MacOSX")
-  (define-key key-translation-map "\e[21~" [f10])
   ;; Make mouse wheel / trackpad scrolling less jerky
   (setq mouse-wheel-scroll-amount '(1
                                     ((shift) . 5)
