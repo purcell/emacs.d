9ec0b223869782456a62552119fd563f01d78868
Chen Bin <chenbin.sh@gmail.com>
2014-06-08 00:06:47 +1000

    minor tip in README

--- init-elpa.el
+++ init-elpa.el
@@ -154,6 +154,8 @@ ARCHIVE is the string name of the package archive.")
              (not (string-equal archive "melpa")))
          )))
 
+;; un-comment below code if you prefer use all the package on melpa (unstable) without limitation
+;; (setq package-filter-function nil)
 
 ;;------------------------------------------------------------------------------
 ;; Fire up package.el and ensure the following packages are installed.
