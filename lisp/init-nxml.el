b0b863832b72697021840151ff3cb6c5f77370f4
Steve Purcell <steve@sanityinc.com>
2014-06-10 21:08:45 +0100

    plist files are xml

--- lisp/init-nxml.el
+++ lisp/init-nxml.el
@@ -3,7 +3,7 @@
  (concat "\\."
          (regexp-opt
           '("xml" "xsd" "sch" "rng" "xslt" "svg" "rss"
-            "gpx" "tcx"))
+            "gpx" "tcx" "plist"))
          "\\'"))
 (setq magic-mode-alist (cons '("<\\?xml " . nxml-mode) magic-mode-alist))
 (fset 'xml-mode 'nxml-mode)
