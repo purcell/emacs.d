42cc0fd7f6462e33afa0ed1b1589cb4aa65fce86
Chen Bin <chenbin.sh@gmail.com>
2014-06-12 10:31:31 +1000

    sh-mode supports more files

--- init-sh.el
+++ init-sh.el
@@ -1,4 +1,11 @@
 (add-hook 'sh-set-shell-hook 'flymake-shell-load)
 
+(add-to-list 'auto-mode-alist '("\\.bash_profile\\'" . sh-mode))
+(add-to-list 'auto-mode-alist '("\\.bash_history\\'" . sh-mode))
+(add-to-list 'auto-mode-alist '("\\.sh\\'" . sh-mode))
+(add-to-list 'auto-mode-alist '("\\.bash\\'" . sh-mode))
+(add-to-list 'auto-mode-alist '("\\.bashrc.local\\'" . sh-mode))
+(add-to-list 'auto-mode-alist '("\\.zsh\\'" . sh-mode))
+(add-to-list 'auto-mode-alist '("\\.bashrc\\'" . sh-mode))
 
 (provide 'init-sh)
