b45eec758b674e8825debf54f54f9fe51c704ee6
Chen Bin <chenbin.sh@gmail.com>
2014-06-25 01:53:34 +1000

    Revert "prevent workgroups2 to load profile when start up"
    This reverts commit 44cdfcb5a64062b5ba494efd02c3d882f3510389.

--- init-workgroups2.el
+++ init-workgroups2.el
@@ -1,5 +1,4 @@
 (require 'workgroups2)
-(setq wg-use-default-session-file nil)
 (workgroups-mode 1) ; put this one at the bottom of .emacs
 ;; by default, the sessions are saved in "~/.emacs_workgroups"
 (provide 'init-workgroups2)
