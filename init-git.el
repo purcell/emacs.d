20fec812305dca4fcc2aa5722f35b96e9c6e7682
Chen Bin <chenbin.sh@gmail.com>
2014-06-29 19:49:08 +1000

    git-gutter setup

--- init-git.el
+++ init-git.el
@@ -47,6 +47,8 @@
 ;; If you enable global minor mode
 (global-git-gutter-mode t)
 
+(git-gutter:linum-setup)
+
 (global-set-key (kbd "C-x C-g") 'git-gutter:toggle)
 (global-set-key (kbd "C-x v =") 'git-gutter:popup-hunk)
 
