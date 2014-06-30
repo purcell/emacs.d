2716ba319d695bd4dee22aa5c215615d91187912
Chen Bin <chenbin.sh@gmail.com>
2014-06-30 12:51:27 +1000

    git-gutter v0.63 is not stable enough

--- init-git.el
+++ init-git.el
@@ -42,25 +42,25 @@
      ))
 
 ;; {{ git-gutter
-(require 'git-gutter)
+;(require 'git-gutter)
 
-;; If you enable global minor mode
-(global-git-gutter-mode t)
+; If you enable global minor mode
+;(global-git-gutter-mode t)
 
-(git-gutter:linum-setup)
+;(git-gutter:linum-setup)
 
-(global-set-key (kbd "C-x C-g") 'git-gutter:toggle)
-(global-set-key (kbd "C-x v =") 'git-gutter:popup-hunk)
+;(global-set-key (kbd "C-x C-g") 'git-gutter:toggle)
+;(global-set-key (kbd "C-x v =") 'git-gutter:popup-hunk)
 
-;; Jump to next/previous hunk
-(global-set-key (kbd "C-x p") 'git-gutter:previous-hunk)
-(global-set-key (kbd "C-x n") 'git-gutter:next-hunk)
+; Jump to next/previous hunk
+;(global-set-key (kbd "C-x p") 'git-gutter:previous-hunk)
+;(global-set-key (kbd "C-x n") 'git-gutter:next-hunk)
 
-;; Stage current hunk
-(global-set-key (kbd "C-x v s") 'git-gutter:stage-hunk)
+; Stage current hunk
+;(global-set-key (kbd "C-x v s") 'git-gutter:stage-hunk)
 
-;; Revert current hunk
-(global-set-key (kbd "C-x v r") 'git-gutter:revert-hunk)
+; Revert current hunk
+;(global-set-key (kbd "C-x v r") 'git-gutter:revert-hunk)
 ;; }}
 
 ;;----------------------------------------------------------------------------
