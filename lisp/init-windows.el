5ab437c887f907c98c0ec044d973521efc1dc7f4
Steve Purcell <steve@sanityinc.com>
2014-07-03 11:09:36 +0100

    Use winner-undo instead of zygospore

--- lisp/init-windows.el
+++ lisp/init-windows.el
@@ -25,8 +25,15 @@
 (global-set-key "\C-x2" (split-window-func-with-other-buffer 'split-window-vertically))
 (global-set-key "\C-x3" (split-window-func-with-other-buffer 'split-window-horizontally))
 
-(require-package 'zygospore)
-(global-set-key "\C-x1" 'zygospore-toggle-delete-other-windows)
+(defun sanityinc/toggle-delete-other-windows ()
+  "Delete other windows in frame if any, or restore previous window config."
+  (interactive)
+  (if (and winner-mode
+           (equal (selected-window) (next-window)))
+      (winner-undo)
+    (delete-other-windows)))
+
+(global-set-key "\C-x1" 'sanityinc/toggle-delete-other-windows)
 
 ;;----------------------------------------------------------------------------
 ;; Rearrange split windows
