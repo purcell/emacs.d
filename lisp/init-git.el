81b08a51bcaa70c1db7576b9485efc02ad51abbc
Steve Purcell <steve@sanityinc.com>
2014-07-03 08:13:53 +0100

    Add git-timemachine

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -4,7 +4,8 @@
 (require-package 'git-rebase-mode)
 (require-package 'gitignore-mode)
 (require-package 'gitconfig-mode)
-(require-package 'git-messenger)
+(require-package 'git-messenger) ;; Though see also vc-annotate's "n" & "p" bindings
+(require-package 'git-timemachine)
 
 (setq-default
  magit-save-some-buffers nil
