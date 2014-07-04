43100982db24d3a7fd7a46ef5a8a5026601db311
Steve Purcell <steve@sanityinc.com>
2014-07-04 13:06:04 +0100

    Suppress minibuffer display of flycheck errors if the error list buffer is visible

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -5,4 +5,21 @@
 ;; Override default flycheck triggers
 (setq flycheck-check-syntax-automatically '(save idle-change mode-enabled)
       flycheck-idle-change-delay 0.8)
+
+(defun sanityinc/flycheck-errors-visible-p ()
+  "Returns t if a window in the current frame is displaying \"*Flycheck errors*\"."
+  (let (found-flycheck-errors-buf)
+    (walk-window-tree (lambda (w)
+                        (when (string-equal "*Flycheck errors*"
+                          (buffer-name (window-buffer w)))
+        (setq found-flycheck-errors-buf t))))
+    (selected-frame)
+    found-flycheck-errors-buf))
+
+(defun sanityinc/flycheck-maybe-display-errors (errors)
+  (unless (sanityinc/flycheck-errors-visible-p)
+    (flycheck-display-error-messages errors)))
+
+(setq flycheck-display-errors-function 'sanityinc/flycheck-maybe-display-errors)
+
 (provide 'init-flycheck)
