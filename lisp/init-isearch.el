aee93d95c800d402a69b6305dff80ef1304d1637
Steve Purcell <steve@sanityinc.com>
2014-07-14 11:25:50 +0100

    DEL during isearch should edit the search string, not jump back to the previous result

--- lisp/init-isearch.el
+++ lisp/init-isearch.el
@@ -9,6 +9,9 @@
 ;; Activate occur easily inside isearch
 (define-key isearch-mode-map (kbd "C-o") 'isearch-occur)
 
+;; DEL during isearch should edit the search string, not jump back to the previous result
+(define-key isearch-mode-map [remap isearch-delete-char] 'isearch-del-char)
+
 ;; Search back/forth for the symbol at point
 ;; See http://www.emacswiki.org/emacs/SearchAtPoint
 (defun isearch-yank-symbol ()
