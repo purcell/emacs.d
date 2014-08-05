2412e0794ec096eced09db1b9a87a604a1502aab
Steve Purcell <steve@sanityinc.com>
2014-08-05 18:05:13 +0100

    Misc haskell improvements

--- lisp/init-haskell.el
+++ lisp/init-haskell.el
@@ -1,19 +1,34 @@
 (require-package 'haskell-mode)
 
+;;; Flycheck specifics
 (when (> emacs-major-version 23)
   (require-package 'flycheck-hdevtools)
   (require-package 'flycheck-haskell)
   (after-load 'flycheck
-    (add-hook 'flycheck-mode-hook #'flycheck-haskell-setup)
+    (add-hook 'haskell-mode-hook #'flycheck-haskell-setup)
+
+    (defun sanityinc/flycheck-haskell-reconfigure ()
+      "Reconfigure flycheck haskell settings, e.g. after changing cabal file."
+      (interactive)
+      (unless (eq major-mode 'haskell-mode)
+        (error "Expected to be in haskell-mode"))
+      (flycheck-haskell-clear-config-cache)
+      (flycheck-haskell-configure)
+      (flycheck-mode -1)
+      (flycheck-mode))
 
     (defadvice haskell-mode-stylish-buffer (around skip-if-flycheck-errors activate)
-      "Don't run stylish-buffer if the buffer appears to have a syntax error."
+      "Don't run stylish-buffer if the buffer appears to have a syntax error.
+This isn't a hard guarantee, since flycheck might sometimes not run until the file has
+been saved."
       (unless (flycheck-has-current-errors-p 'error)
-        ad-do-it))))
+        ad-do-it))
+
+    (require 'flycheck-hdevtools)))
 
 
 (dolist (hook '(haskell-mode-hook inferior-haskell-mode-hook haskell-interactive-mode-hook))
-  (add-hook hook 'turn-on-haskell-doc-mode))
+  (add-hook hook 'turn-on-haskell-doc-mode)
 (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
 
 (after-load 'haskell-interactive-mode
@@ -24,7 +39,6 @@
 (require-package 'hi2)
 (add-hook 'haskell-mode-hook 'turn-on-hi2)
 
-(add-hook 'haskell-mode-hook (lambda () (subword-mode +1)))
 (add-hook 'haskell-mode-hook 'haskell-auto-insert-module-template)
 
 (setq-default haskell-stylish-on-save t)
