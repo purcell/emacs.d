911095bda225cf2842f62c91997bf57fcd519c5c
Steve Purcell <steve@sanityinc.com>
2014-08-11 10:14:07 +0100

    Enable rainbow-mode when editing any *-theme.el files

--- lisp/init-lisp.el
+++ lisp/init-lisp.el
@@ -207,5 +207,18 @@
 ;; A quick way to jump to the definition of a function given its key binding
 (global-set-key (kbd "C-h K") 'find-function-on-key)
 
+
+
+(when (eval-when-compile (>= emacs-major-version 24))
+  ;; rainbow-mode needs color.el, bundled with Emacs >= 24.
+  (require-package 'rainbow-mode)
+
+  (defun sanityinc/enable-rainbow-mode-if-theme ()
+    (when (string-match "-theme\\.el" (buffer-name))
+      (rainbow-mode 1)))
+
+  (add-hook 'emacs-lisp-mode-hook 'sanityinc/enable-rainbow-mode-if-theme))
+
+
 
 (provide 'init-lisp)
