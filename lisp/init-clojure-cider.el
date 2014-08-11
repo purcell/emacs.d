9e6b8c0a0667e10a2a25049d95f62e4cc96514df
Steve Purcell <steve@sanityinc.com>
2014-08-11 17:10:57 +0100

    Fix trailing reference to ac-nrepl

--- lisp/init-clojure-cider.el
+++ lisp/init-clojure-cider.el
@@ -21,7 +21,7 @@
   (add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
   (add-hook 'cider-repl-mode-hook 'subword-mode)
   (add-hook 'cider-repl-mode-hook 'paredit-mode)
-  (define-key cider-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc)
+  (define-key cider-mode-map (kbd "C-c C-d") 'ac-cider-popup-doc)
 
   ;; nrepl isn't based on comint
   (add-hook 'cider-repl-mode-hook
