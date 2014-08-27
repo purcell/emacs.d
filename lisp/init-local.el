c5117d5f9b8a5a0644efc990e86828a11ad27cbb
Justin Wu <justin.zhengyi.wu@gmail.com>
2014-08-27 14:01:28 +0800

    Update init-local.el

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,4 +1,8 @@
-;;; Show line numbers.
+;;; package --- Summary
+;;; Commentary:
+;;; Code:
+
+;;; Enable line number.
 (global-linum-mode t)
 
 (add-to-list 'exec-path "/usr/local/bin")
@@ -6,6 +10,19 @@
 (setenv "PATH" (concat (getenv "PATH") ":" "/usr/local/bin" ":" "/usr/local/go/bin" ":" "/Users/justin/bin"))
 
 ;; use Meta-Return keys on Mac OS X to toggle maxmize full screen.
-(global-set-key (kbd "<M-RET>") 'toggle-frame-fullscreen)
+(when *is-a-mac*
+  (global-set-key (kbd "<M-RET>") 'toggle-frame-fullscreen))
 ;; use C-tab to do auto complete
 (global-set-key [C-tab] 'auto-complete)
+
+;; turn on soft wrapping mode for org mode
+(add-hook 'org-mode-hook
+  (lambda () (setq truncate-lines nil)))
+
+;; Enable yasnippet globally.
+(yas-global-mode 1)
+
+(put 'dired-find-alternate-file 'disabled nil)
+
+(provide 'init-local)
+;;; init-local.el ends here
