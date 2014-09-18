bc02e05df44ff81645ec2374b700280824c707ba
Steve Purcell <steve@sanityinc.com>
2014-09-18 10:38:20 +0100

    Disable session.el in git-commit-mode, to avoid spurious point placement

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -1,3 +1,5 @@
+;; TODO: link commits from vc-log to magit-show-commit
+;; TODO: smerge-mode
 (require-package 'magit)
 (require-package 'git-blame)
 (require-package 'git-commit-mode)
@@ -25,6 +27,8 @@
   (fullframe magit-status magit-mode-quit-window))
 
 (add-hook 'git-commit-mode-hook 'goto-address-mode)
+(after-load 'session
+  (add-to-list 'session-mode-disable-list 'git-commit-mode))
 
 
 ;;; When we start working on git-backed files, use git-wip if available
