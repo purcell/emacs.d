e9ca768956e60f0849579e8c2509647c36481bbc
Steve Purcell <steve@sanityinc.com>
2014-09-30 10:56:53 +0100

    Add dired-sort

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -1,10 +1,12 @@
 (require-package 'dired+)
+(require-package 'dired-sort)
 
 (setq-default diredp-hide-details-initially-flag nil
               dired-dwim-target t)
 
 (after-load 'dired
   (require 'dired+)
+  (require 'dired-sort)
   (when (fboundp 'global-dired-hide-details-mode)
     (global-dired-hide-details-mode -1))
   (setq dired-recursive-deletes 'top)
