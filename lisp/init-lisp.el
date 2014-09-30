168cd09e01518cde9af72df09ea54fd396b88b47
Steve Purcell <steve@sanityinc.com>
2014-09-30 14:13:33 +0100

    Just add check-parens via a local after-save-hook entry, not global

--- lisp/init-lisp.el
+++ lisp/init-lisp.el
@@ -149,7 +149,8 @@
   (rainbow-delimiters-mode t)
   (enable-paredit-mode)
   (turn-on-eldoc-mode)
-  (redshank-mode))
+  (redshank-mode)
+  (add-hook 'after-save-hook #'check-parens nil t))
 
 (defun sanityinc/emacs-lisp-setup ()
   "Enable features useful when working with elisp."
@@ -174,12 +175,6 @@
 (dolist (hook (mapcar #'derived-mode-hook-name sanityinc/elispy-modes))
   (add-hook hook 'sanityinc/emacs-lisp-setup))
 
-(defun sanityinc/maybe-check-parens ()
-  "Run `check-parens' if this is a lispy mode."
-  (when (memq major-mode sanityinc/lispy-modes)
-    (check-parens)))
-
-(add-hook 'after-save-hook #'sanityinc/maybe-check-parens)
 
 (require-package 'eldoc-eval)
 (require 'eldoc-eval)
