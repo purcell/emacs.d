cfda145ac610e8159c4690835d9711af3a627d3c
Sherman Hsu <sherman_hsu@5vtechnologies.com>
2014-10-01 18:14:12 +0800

    mycustom: Fix init. warning: Unknown "color-16"

--- mycustom.el
+++ mycustom.el
@@ -17,7 +17,7 @@
  '(evil-want-C-w-delete t)
  '(evil-want-C-w-in-emacs-state nil)
  '(evil-want-visual-char-semi-exclusive t)
- '(init-color-theme-no-window-system-bg-color "color-16")
+ '(init-color-theme-no-window-system-bg-color "#000000")
  '(init-color-theme-window-system-bg-color "black")
  '(session-use-package t nil (session))
  '(term-bind-key-alist (quote (("C-c C-c" . term-interrupt-subjob) ("C-p" . previous-line) ("C-n" . next-line) ("C-s" . isearch-forward) ("C-r" . isearch-backward) ("C-m" . term-send-raw) ("C-<right>" . term-send-forward-word) ("C-<left>" . term-send-backward-word) ("M-o" . term-send-backspace) ("M-p" . term-send-up) ("M-n" . term-send-down) ("M-d" . term-send-forward-kill-word) ("M-<backspace>" . term-send-backward-kill-word) ("M-r" . term-send-reverse-search-history) ("M-," . term-send-input) ("M-." . comint-dynamic-complete))))
