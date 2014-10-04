1b7f5d50e4fba0d63e23ba375511147d12defbb6
Steve Purcell <steve@sanityinc.com>
2014-10-04 12:45:40 +0100

    Tidy up code for setting tabulated-list-mode column widths

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -71,14 +71,18 @@ re-downloaded in order to locate PACKAGE."
 (require-package 'cl-lib)
 (require 'cl-lib)
 
+(defun sanityinc/set-tabulated-list-column-width (col-name width)
+  "Set any column with name COL-NAME to the given WIDTH."
+  (cl-loop for column across tabulated-list-format
+           when (string= col-name (car column))
+           do (setf (elt column 1) width)))
+
 (defun sanityinc/maybe-widen-package-menu-columns ()
   "Widen some columns of the package menu table to avoid truncation."
   (when (boundp 'tabulated-list-format)
-    (when (string= "Version" (elt (elt tabulated-list-format 1) 0))
-      (setf (elt (elt tabulated-list-format 1) 1) 13))
+    (sanityinc/set-tabulated-list-column-width "Version" 13)
     (let ((longest-archive-name (apply 'max (mapcar 'length (mapcar 'car package-archives)))))
-     (when (string= "Archive" (elt (elt tabulated-list-format 3) 0))
-       (setf (elt (elt tabulated-list-format 3) 1) longest-archive-name)))))
+      (sanityinc/set-tabulated-list-column-width "Archive" longest-archive-name))))
 
 (add-hook 'package-menu-mode-hook 'sanityinc/maybe-widen-package-menu-columns)
 
