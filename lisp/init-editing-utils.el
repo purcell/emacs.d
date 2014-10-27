4c1493490edea19bb3d09c67ef5e907a4fd3e432
Steve Purcell <steve@sanityinc.com>
2014-10-27 17:51:48 +0000

    Restore accidentally removed sanityinc/newline-at-end-of-line

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -57,6 +57,17 @@
 (require-package 'whitespace-cleanup-mode)
 (global-whitespace-cleanup-mode t)
 
+
+;;; Newline behaviour
+
+(global-set-key (kbd "RET") 'newline-and-indent)
+(defun sanityinc/newline-at-end-of-line ()
+  "Move to end of line, enter a newline, and reindent."
+  (interactive)
+  (move-end-of-line 1)
+  (newline-and-indent))
+
+(global-set-key (kbd "S-<return>") 'sanityinc/newline-at-end-of-line)
 
 
 
