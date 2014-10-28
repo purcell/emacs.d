246213dc61852057636e070b1f3a7582c866ff02
Steve Purcell <steve@sanityinc.com>
2014-10-28 15:52:30 +0000

    Provide maybe-require-package: like require-package, but fails softly

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -56,6 +56,18 @@ re-downloaded in order to locate PACKAGE."
         (require-package package min-version t)))))
 
 
+(defun maybe-require-package (package &optional min-version no-refresh)
+  "Try to install PACKAGE, and return non-nil if successful.
+In the event of failure, return nil and print a warning message.
+Optionally require MIN-VERSION.  If NO-REFRESH is non-nil, the
+available package lists will not be re-downloaded in order to
+locate PACKAGE."
+  (condition-case err
+      (require-package package min-version no-refresh)
+    (error
+     (message "Couldn't install package `%s': %S" package err)
+     nil)))
+
 
 ;;; Fire up package.el
 
