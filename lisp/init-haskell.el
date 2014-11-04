4c51832363374ac92bc882684be4a14428ba2b2b
Steve Purcell <steve@sanityinc.com>
2014-11-04 21:03:23 +0000

    Reorganise init-haskell

--- lisp/init-haskell.el
+++ lisp/init-haskell.el
@@ -1,9 +1,29 @@
 (require-package 'haskell-mode)
-(maybe-require-package 'hayoo)
+
+
+;; Completion
 
 (add-to-list 'completion-ignored-extensions ".hi")
 
-;;; Flycheck specifics
+;; Hook auto-complete into the completions provided by the inferior
+;; haskell process, if any.
+
+(require-package 'ac-haskell-process)
+
+(add-hook 'interactive-haskell-mode-hook 'ac-haskell-process-setup)
+(add-hook 'haskell-interactive-mode-hook 'ac-haskell-process-setup)
+
+(after-load 'haskell-mode
+  (define-key haskell-mode-map (kbd "C-c C-d") 'ac-haskell-process-popup-doc))
+
+(after-load 'auto-complete
+  (add-to-list 'ac-modes 'haskell-interactive-mode)
+  (add-hook 'haskell-interactive-mode-hook 'set-auto-complete-as-completion-at-point-function))
+
+
+
+;; Flycheck specifics
+
 (when (and (maybe-require-package 'flycheck-haskell)
            (require-package 'flycheck-hdevtools))
   (after-load 'flycheck
@@ -28,6 +48,8 @@ been saved."
 
     (require 'flycheck-hdevtools)))
 
+
+;; Docs
 
 (dolist (hook '(haskell-mode-hook inferior-haskell-mode-hook haskell-interactive-mode-hook))
   (add-hook hook 'turn-on-haskell-doc-mode)
@@ -36,27 +58,42 @@ been saved."
 
 (add-hook 'haskell-interactive-mode-hook 'sanityinc/no-trailing-whitespace)
 
+
+;; Interaction
+
 (after-load 'haskell-process
   (diminish 'interactive-haskell-mode " IntHS"))
 
 (add-auto-mode 'haskell-mode "\\.ghci\\'")
 
+(when (maybe-require-package 'ghci-completion)
+  (add-hook 'inferior-haskell-mode-hook 'turn-on-ghci-completion))
+
+
+
+;; Indentation
 (require-package 'hi2)
 (add-hook 'haskell-mode-hook 'turn-on-hi2)
+
+(when (fboundp 'electric-indent-mode)
+  (add-hook 'haskell-mode-hook (lambda () (electric-indent-mode -1))))
+
+
+
+;; Source code helpers
 
 (add-hook 'haskell-mode-hook 'haskell-auto-insert-module-template)
 
 (setq-default haskell-stylish-on-save t)
 
+(maybe-require-package 'hayoo)
 (after-load 'haskell-mode
   (define-key haskell-mode-map (kbd "C-c h") 'hoogle)
   (define-key haskell-mode-map (kbd "C-o") 'open-line))
 
-(when (maybe-require-package 'ghci-completion)
-  (add-hook 'inferior-haskell-mode-hook 'turn-on-ghci-completion))
 
-(eval-after-load 'page-break-lines
-  '(push 'haskell-mode page-break-lines-modes))
+(after-load 'page-break-lines
+  (push 'haskell-mode page-break-lines-modes))
 
 ;; Make compilation-mode understand "at blah.hs:11:34-50" lines output by GHC
 (after-load 'compile
@@ -67,22 +104,6 @@ been saved."
     (add-to-list
      'compilation-error-regexp-alist alias)))
 
-
-
-;; Hook auto-complete into the completions provided by the inferior
-;; haskell process, if any.
-
-(require-package 'ac-haskell-process)
-
-(add-hook 'interactive-haskell-mode-hook 'ac-haskell-process-setup)
-(add-hook 'haskell-interactive-mode-hook 'ac-haskell-process-setup)
-
-(after-load 'haskell-mode
-  (define-key haskell-mode-map (kbd "C-c C-d") 'ac-haskell-process-popup-doc))
-
-(after-load 'auto-complete
-  (add-to-list 'ac-modes 'haskell-interactive-mode)
-  (add-hook 'haskell-interactive-mode-hook 'set-auto-complete-as-completion-at-point-function))
 
 
 (provide 'init-haskell)
