8556088fb560f2a942b2ff80a6381f04adc4060c
Steve Purcell <steve@sanityinc.com>
2014-11-15 08:28:15 +0000

    Alias after-load to with-eval-after-load when available

--- lisp/init-utils.el
+++ lisp/init-utils.el
@@ -1,8 +1,10 @@
-(defmacro after-load (feature &rest body)
-  "After FEATURE is loaded, evaluate BODY."
-  (declare (indent defun))
-  `(eval-after-load ,feature
-     '(progn ,@body)))
+(if (fboundp 'with-eval-after-load)
+    (defalias 'after-load 'with-eval-after-load)
+  (defmacro after-load (feature &rest body)
+    "After FEATURE is loaded, evaluate BODY."
+    (declare (indent defun))
+    `(eval-after-load ,feature
+       '(progn ,@body))))
 
 
 ;;----------------------------------------------------------------------------
