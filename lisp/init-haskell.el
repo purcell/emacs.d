3e247fdbfb512694021cb08bb9ef83ff3d6a7a0d
Steve Purcell <steve@sanityinc.com>
2014-11-19 14:32:44 +0000

    haskell-mode and haml-mode have been fixed upstream to inhibit electric indentation

--- lisp/init-haskell.el
+++ lisp/init-haskell.el
@@ -75,9 +75,6 @@ been saved."
 (require-package 'hi2)
 (add-hook 'haskell-mode-hook 'turn-on-hi2)
 
-(when (fboundp 'electric-indent-mode)
-  (add-hook 'haskell-mode-hook (lambda () (electric-indent-mode -1))))
-
 
 
 ;; Source code helpers
