7fce6c314b4cbdb42cc508d4eb6e2f35ab4374d5
Steve Purcell <steve@sanityinc.com>
2014-11-29 16:12:37 +0000

    Don't override RET in ruby-mode

--- lisp/init-ruby-mode.el
+++ lisp/init-ruby-mode.el
@@ -10,7 +10,6 @@
 (setq ruby-use-encoding-map nil)
 
 (after-load 'ruby-mode
-  (define-key ruby-mode-map (kbd "RET") 'reindent-then-newline-and-indent)
   (define-key ruby-mode-map (kbd "TAB") 'indent-for-tab-command)
 
   ;; Stupidly the non-bundled ruby-mode isn't a derived mode of
