3b49c4e2c6b3a6119ddcf3a6a454d29bdebbbab1
Steve Purcell <steve@sanityinc.com>
2014-11-29 16:13:37 +0000

    Enable guide-key for more prefix keys

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -339,7 +339,7 @@ With arg N, insert N newlines."
 
 
 (require-package 'guide-key)
-(setq guide-key/guide-key-sequence '("C-x r" "C-x 4" "C-x 5" "C-c ;" "C-c ; f" "C-c ' f" "C-x n"))
+(setq guide-key/guide-key-sequence '("C-x" "C-c" "C-x 4" "C-x 5" "C-c ;" "C-c ; f" "C-c ' f" "C-x n"))
 (guide-key-mode 1)
 (diminish 'guide-key-mode)
 
