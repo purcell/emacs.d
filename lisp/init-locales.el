1550ec2cd5d30d42889d11ebd0a897392e4bd3b1
Steve Purcell <steve@sanityinc.com>
2015-02-07 13:57:02 +0000

    Use utf-16-le for selection-coding-system on windows
    See https://stackoverflow.com/questions/2901541/which-coding-system-should-i-use-in-emacs/2903256#comment20730840_2903256

--- lisp/init-locales.el
+++ lisp/init-locales.el
@@ -15,8 +15,7 @@
   (setq locale-coding-system 'utf-8)
   (set-default-coding-systems 'utf-8)
   (set-terminal-coding-system 'utf-8)
-  (unless (eq system-type 'windows-nt)
-   (set-selection-coding-system 'utf-8))
+  (set-selection-coding-system (if (eq system-type 'windows-nt) 'utf-16-le 'utf-8))
   (prefer-coding-system 'utf-8))
 
 (provide 'init-locales)
