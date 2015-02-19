78c3f187b98c80bd00a0b865aa1d7ac47cbf40e6
Steve Purcell <steve@sanityinc.com>
2015-02-19 09:07:13 +0000

    Add "C-x C-r" prefix to guide-key sequences

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -347,7 +347,7 @@ With arg N, insert N newlines."
 
 
 (require-package 'guide-key)
-(setq guide-key/guide-key-sequence '("C-x" "C-c" "C-x 4" "C-x 5" "C-c ;" "C-c ; f" "C-c ' f" "C-x n"))
+(setq guide-key/guide-key-sequence '("C-x" "C-c" "C-x 4" "C-x 5" "C-c ;" "C-c ; f" "C-c ' f" "C-x n" "C-x C-r"))
 (guide-key-mode 1)
 (diminish 'guide-key-mode)
 
