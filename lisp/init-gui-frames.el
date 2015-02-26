5ccaf3ff1628eaf30b8c1d422bac73ea191cc115
Steve Purcell <steve@sanityinc.com>
2015-02-26 15:07:31 +0000

    Fix frame transparency adjustment for latest Emacs versions

--- lisp/init-gui-frames.el
+++ lisp/init-gui-frames.el
@@ -37,7 +37,13 @@
   (add-to-list 'initial-frame-alist no-border))
 
 (defun sanityinc/adjust-opacity (frame incr)
+  "Adjust the background opacity of FRAME by increment INCR."
+  (unless (display-graphic-p frame)
+    (error "Cannot adjust opacity of this frame"))
   (let* ((oldalpha (or (frame-parameter frame 'alpha) 100))
+         ;; The 'alpha frame param became a pair at some point in
+         ;; emacs 24.x, e.g. (100 100)
+         (oldalpha (if (listp oldalpha) (car oldalpha) oldalpha))
          (newalpha (+ incr oldalpha)))
     (when (and (<= frame-alpha-lower-limit newalpha) (>= 100 newalpha))
       (modify-frame-parameters frame (list (cons 'alpha newalpha))))))
