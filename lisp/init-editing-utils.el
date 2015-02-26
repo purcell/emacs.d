6a6271169fdbdfe3eae87efded0cbc6bc309d2f8
Steve Purcell <steve@sanityinc.com>
2015-02-26 17:04:33 +0000

    Enable highlight-symbol-nav-mode in org-mode

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -93,6 +93,7 @@
 (dolist (hook '(prog-mode-hook html-mode-hook css-mode-hook))
   (add-hook hook 'highlight-symbol-mode)
   (add-hook hook 'highlight-symbol-nav-mode))
+(add-hook 'org-mode-hook 'highlight-symbol-nav-mode)
 (after-load 'highlight-symbol
   (diminish 'highlight-symbol-mode)
   (defadvice highlight-symbol-temp-highlight (around sanityinc/maybe-suppress activate)
