e6a95a9d9dc663a68e6ffe1c3806911307ec4fa1
Steve Purcell <steve@sanityinc.com>
2015-03-09 16:00:45 +0000

    Make after-compilation alert function more efficient

--- lisp/init-compile.el
+++ lisp/init-compile.el
@@ -6,14 +6,13 @@
 
 (defun sanityinc/alert-after-compilation-finish (buf result)
   "Use `alert' to report compilation RESULT if BUF is hidden."
-  (let ((buf-is-visible nil))
-    (walk-windows (lambda (w)
-                    (when (eq (window-buffer w) buf)
-                      (setq buf-is-visible t))))
-    (unless buf-is-visible
-      (alert (concat "Compilation " result)
-             :buffer buf
-             :category 'compilation))))
+  (unless (catch 'is-visible
+            (walk-windows (lambda (w)
+                            (when (eq (window-buffer w) buf)
+                              (throw 'is-visible t)))))
+    (alert (concat "Compilation " result)
+           :buffer buf
+           :category 'compilation)))
 
 (after-load 'compile
   (add-hook 'compilation-finish-functions
