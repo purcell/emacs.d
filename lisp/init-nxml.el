4c09e4a03e52f5aa2d54b76dc9ded342c4b513be
Steve Purcell <steve@sanityinc.com>
2015-03-24 20:54:13 +0000

    Tidy up pp-xml function

--- lisp/init-nxml.el
+++ lisp/init-nxml.el
@@ -13,18 +13,24 @@
 
 
 ;; See: http://sinewalker.wordpress.com/2008/06/26/pretty-printing-xml-with-emacs-nxml-mode/
-(defun sanityinc/pp-xml-region (begin end)
+(defun sanityinc/pp-xml-region (beg end)
   "Pretty format XML markup in region. The function inserts
 linebreaks to separate tags that have nothing but whitespace
 between them.  It then indents the markup by using nxml's
 indentation rules."
   (interactive "r")
+  (unless (use-region-p)
+    (setq beg (point-min)
+          end (point-max)))
+  ;; Use markers because our changes will move END
+  (setq beg (set-marker (make-marker) begin)
+        end (set-marker (make-marker) end))
   (save-excursion
-      (nxml-mode)
-      (goto-char begin)
-      (while (search-forward-regexp "\>[ \\t]*\<" nil t)
-        (backward-char) (insert "\n"))
-      (indent-region begin end)))
+    (goto-char beg)
+    (while (search-forward-regexp "\>[ \\t]*\<" end t)
+      (backward-char) (insert "\n"))
+    (nxml-mode)
+    (indent-region begin end)))
 
 ;;----------------------------------------------------------------------------
 ;; Integration with tidy for html + xml
