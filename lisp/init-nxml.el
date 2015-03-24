26851fa6ed01e408ee661e22baf56f644fe0d094
Steve Purcell <steve@sanityinc.com>
2015-03-24 20:54:22 +0000

    Provide a simple wrapper for "tidy" for use with xml

--- lisp/init-nxml.el
+++ lisp/init-nxml.el
@@ -38,5 +38,13 @@ indentation rules."
 (require-package 'tidy)
 (add-hook 'nxml-mode-hook (lambda () (tidy-build-menu nxml-mode-map)))
 
+(defun sanityinc/tidy-buffer-xml (beg end)
+  "Run \"tidy -xml\" on the region from BEG to END, or whole buffer."
+  (interactive "r")
+  (unless (use-region-p)
+    (setq beg (point-min)
+          end (point-max)))
+  (shell-command-on-region beg end "tidy -xml -q -i" (current-buffer) t "*tidy-errors*" t))
+
 
 (provide 'init-nxml)
