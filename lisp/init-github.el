4461eae65b195d58ac16b935b695d618335e68bc
Steve Purcell <steve@sanityinc.com>
2015-04-25 11:25:25 +0100

    Soft-require yagist, due to upstream dependency issue on Emacs 23
    https://github.com/mhayashi1120/yagist.el/pull/12

--- lisp/init-github.el
+++ lisp/init-github.el
@@ -1,6 +1,6 @@
 (require 'init-git)
 
-(require-package 'yagist)
+(maybe-require-package 'yagist)
 (require-package 'github-browse-file)
 (require-package 'bug-reference-github)
 (add-hook 'prog-mode-hook 'bug-reference-prog-mode)
