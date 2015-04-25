b755a737ec92cbcb69a1ae61a8df3efe650f312c
Steve Purcell <steve@sanityinc.com>
2015-04-25 11:55:23 +0100

    Suppress package download status messages

--- test-startup.sh
+++ test-startup.sh
@@ -2,6 +2,7 @@
 echo "Attempting startup..."
 ${EMACS:=emacs} -nw --batch \
                 --eval '(let ((debug-on-error t)
+                              (url-show-status nil)
                               (user-emacs-directory default-directory)
                               (user-init-file (expand-file-name "init.el"))
                               (load-path (delq default-directory load-path)))
