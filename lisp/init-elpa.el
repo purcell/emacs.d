8ba5741e8cfddb4483748aef0312689e0e6ceb94
Steve Purcell <steve@sanityinc.com>
2015-04-25 11:41:56 +0100

    Enable marmalade for Emacs 23, mainly for ruby-mode

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -14,7 +14,9 @@
 
 ;;; Standard package repositories
 
-;;(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(when (< emacs-major-version 24)
+  ;; Mainly for ruby-mode
+  (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/")))
 
 ;; We include the org repository for completeness, but don't normally
 ;; use it.
