81aaaca9826de24f269d523cb746714ea1f33cbb
Steve Purcell <steve.purcell@powershop.co.nz>
2015-06-09 17:04:22 +1200

    Set custom-file earlier, for package-selected-packages

--- init.el
+++ init.el
@@ -17,6 +17,7 @@
 ;;----------------------------------------------------------------------------
 ;; Bootstrap config
 ;;----------------------------------------------------------------------------
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (require 'init-compat)
 (require 'init-utils)
 (require 'init-site-lisp) ;; Must come before elpa, as it may provide package.el
@@ -123,7 +124,6 @@
 ;;----------------------------------------------------------------------------
 ;; Variables configured via the interactive 'customize' interface
 ;;----------------------------------------------------------------------------
-(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (when (file-exists-p custom-file)
   (load custom-file))
 
