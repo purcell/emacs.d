a2bae3a79732a582790a47982be819a9aed87c57
Jonas Bernoulli <jonas@bernoul.li>
2015-06-27 16:33:58 +0200

    Use suggested bindings for magit-status and magit-dispatch-popup

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -17,6 +17,8 @@
 ;; Hint: customize `magit-repo-dirs' so that you can use C-u M-F12 to
 ;; quickly open magit on any one of your projects.
 (global-set-key [(meta f12)] 'magit-status)
+(global-set-key (kbd "C-x g") 'magit-status)
+(global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
 
 (after-load 'magit
   (define-key magit-status-mode-map (kbd "C-M-<up>") 'magit-section-up))
