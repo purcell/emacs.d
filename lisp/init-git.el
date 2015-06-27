fa7caab051424db1a684028bff930652687bc9b5
Jonas Bernoulli <jonas@bernoul.li>
2015-06-27 16:31:54 +0200

    Don't set removed variable magit-save-some-buffers
    The new variable is `magit-save-repository-buffers'
    and I recommend that you keep the default value of `t'.

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -10,7 +10,6 @@
 (require-package 'git-timemachine)
 
 (setq-default
- magit-save-some-buffers nil
  magit-process-popup-time 10
  magit-diff-refine-hunk t
  magit-completing-read-function 'magit-ido-completing-read)
