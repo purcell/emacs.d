53da53df7f78a638b0162f40a06c4d921db55fd2
Steve Purcell <steve@sanityinc.com>
2015-07-05 13:03:13 +1200

    Disable session.el point memory for git commit messages etc.
    See https://github.com/magit/magit/issues/1954
    
    D#	lisp/init-twitter.el

--- lisp/init-sessions.el
+++ lisp/init-sessions.el
@@ -31,6 +31,7 @@
 (require-package 'session)
 
 (setq session-save-file (expand-file-name ".session" user-emacs-directory))
+(setq session-name-disable-regexp "\\(?:\\`'/tmp\\|\\.git/[A-Z_]+\\'\\)")
 (add-hook 'after-init-hook 'session-initialize)
 
 ;; save a bunch of variables to the desktop file
