84947cd1e40e04a002a70053d5f874e1ef792bb1
Steve Purcell <steve@sanityinc.com>
2015-07-18 16:47:12 +1200

    Remove MELPA Stable from the default config
    I always use packages from MELPA, and never from MELPA Stable.

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -27,7 +27,6 @@
 
 ;;; Also use Melpa for most packages
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
 
 
 
