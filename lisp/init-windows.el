640ba4b8aa722fdab3efcd001420b057ab268de5
Steve Purcell <steve.purcell@powershop.co.nz>
2015-07-31 09:51:41 +1200

    Remove timeout for switch-window

--- lisp/init-windows.el
+++ lisp/init-windows.el
@@ -8,7 +8,8 @@
 ;; Make "C-x o" prompt for a target window when there are more than 2
 (require-package 'switch-window)
 (require 'switch-window)
-(setq switch-window-shortcut-style 'alphabet)
+(setq-default switch-window-shortcut-style 'alphabet)
+(setq-default switch-window-timeout nil)
 (global-set-key (kbd "C-x o") 'switch-window)
 
 
