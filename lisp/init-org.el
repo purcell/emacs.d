4f3218c89fd4e6db922c5c5e895d4401f0b50a33
Steve Purcell <steve@sanityinc.com>
2015-09-21 21:09:48 +1200

    Avoid loading org at startup

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -225,7 +225,8 @@ typical word processor."
 ;;; Org clock
 
 ;; Save the running clock and all clock history when exiting Emacs, load it on startup
-(org-clock-persistence-insinuate)
+(after-load 'org
+  (org-clock-persistence-insinuate))
 (setq org-clock-persist t)
 (setq org-clock-in-resume t)
 
@@ -276,7 +277,8 @@ typical word processor."
     (beginning-of-line 0)
     (org-remove-empty-drawer-at "LOGBOOK" (point))))
 
-(add-hook 'org-clock-out-hook 'sanityinc/remove-empty-drawer-on-clock-out 'append)
+(after-load 'org-clock
+  (add-hook 'org-clock-out-hook 'sanityinc/remove-empty-drawer-on-clock-out 'append))
 
 
 
