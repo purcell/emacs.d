02b285ce17189fa7d45acb82fde507a135b35142
Steve Purcell <steve@sanityinc.com>
2015-10-03 15:37:23 +1300

    clojure-mode now requires Emacs 24.3, so soft-fail on that version

--- lisp/init-clojure.el
+++ lisp/init-clojure.el
@@ -1,17 +1,12 @@
-;; Basic clojure support, even in Emacs 23
 ;; See also init-clojure-cider.el
 
-(require-package 'clojure-mode)
-(require-package 'cljsbuild-mode)
-(require-package 'elein)
+(when (maybe-require-package 'clojure-mode)
+  (require-package 'cljsbuild-mode)
+  (require-package 'elein)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Misc clojure tweaks
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(after-load 'clojure-mode
-  (add-hook 'clojure-mode-hook 'sanityinc/lisp-setup)
-  (add-hook 'clojure-mode-hook 'subword-mode))
+  (after-load 'clojure-mode
+    (add-hook 'clojure-mode-hook 'sanityinc/lisp-setup)
+    (add-hook 'clojure-mode-hook 'subword-mode)))
 
 
 (provide 'init-clojure)
