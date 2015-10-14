56ba369cda1888655ab0d5a3785861187e328f95
Steve Purcell <steve@sanityinc.com>
2015-10-14 21:52:57 +1300

    Emacs 24.1's "kbd" doesn't evaluate its argument

--- lisp/init-osx-keys.el
+++ lisp/init-osx-keys.el
@@ -8,7 +8,7 @@
                                     ((control))))
   (dolist (multiple '("" "double-" "triple-"))
     (dolist (direction '("right" "left"))
-      (global-set-key (kbd (concat "<" multiple "wheel-" direction ">")) 'ignore)))
+      (global-set-key (read-kbd-macro (concat "<" multiple "wheel-" direction ">")) 'ignore)))
   (global-set-key (kbd "M-`") 'ns-next-frame)
   (global-set-key (kbd "M-h") 'ns-do-hide-emacs)
   (global-set-key (kbd "M-˙") 'ns-do-hide-others)
