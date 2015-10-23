d121ccacf324ffc0b9e2d26fb7d90f99d6ceec3c
Steve Purcell <steve@sanityinc.com>
2015-10-24 11:35:48 +1300

    Tidy up the require-times benchmarking advice

--- lisp/init-benchmarking.el
+++ lisp/init-benchmarking.el
@@ -6,19 +6,17 @@
   "A list of (FEATURE . LOAD-DURATION).
 LOAD-DURATION is the time taken in milliseconds to load FEATURE.")
 
-(defadvice require
-  (around build-require-times (feature &optional filename noerror) activate)
+(defadvice require (around sanityinc/build-require-times (feature &optional filename noerror) activate)
   "Note in `sanityinc/require-times' the time taken to require each feature."
   (let* ((already-loaded (memq feature features))
          (require-start-time (and (not already-loaded) (current-time))))
     (prog1
         ad-do-it
       (when (and (not already-loaded) (memq feature features))
-        (add-to-list 'sanityinc/require-times
-                     (cons feature
-                           (sanityinc/time-subtract-millis (current-time)
-                                                           require-start-time))
-                     t)))))
+        (let ((time (sanityinc/time-subtract-millis (current-time) require-start-time)))
+          (add-to-list 'sanityinc/require-times
+                       (cons feature time)
+                       t))))))
 
 
 
