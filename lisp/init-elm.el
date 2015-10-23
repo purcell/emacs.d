2c922c1dba6c37079da41c5d0b1f1d775c0cb45a
Steve Purcell <steve@sanityinc.com>
2015-10-24 11:34:38 +1300

    Auto-completion in elm-mode buffers

--- lisp/init-elm.el
+++ lisp/init-elm.el
@@ -1,4 +1,9 @@
 (when (maybe-require-package 'elm-mode)
+  (after-load 'auto-complete
+    (add-to-list 'ac-modes 'elm-mode))
+  (after-load 'elm-mode
+    (when (executable-find "elm-oracle")
+      (add-hook 'elm-mode-hook 'elm-oracle-setup-ac)))
   (when (maybe-require-package 'flycheck-elm)
     (after-load 'elm-mode
       (flycheck-elm-setup))))
