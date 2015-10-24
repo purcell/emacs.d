5b85477eb0498c5ec52834ba0c415f4a9a2de9bf
Steve Purcell <steve@sanityinc.com>
2015-10-24 15:31:44 +1300

    Namespace a config-local function

--- lisp/init-locales.el
+++ lisp/init-locales.el
@@ -2,14 +2,13 @@
   "Return whether locale string V relates to a UTF-8 locale."
   (and v (string-match "UTF-8" v)))
 
-(defun locale-is-utf8-p ()
+(defun sanityinc/locale-is-utf8-p ()
   "Return t iff the \"locale\" command or environment variables prefer UTF-8."
   (or (sanityinc/utf8-locale-p (and (executable-find "locale") (shell-command-to-string "locale")))
       (sanityinc/utf8-locale-p (getenv "LC_ALL"))
       (sanityinc/utf8-locale-p (getenv "LC_CTYPE"))
       (sanityinc/utf8-locale-p (getenv "LANG"))))
 
-(when (or window-system (locale-is-utf8-p))
 (when (or window-system (sanityinc/locale-is-utf8-p))
   (set-language-environment 'utf-8)
   (setq locale-coding-system 'utf-8)
