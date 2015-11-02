190528091fd8f72e2fa5bbf89f8492c29f31db78
Steve Purcell <steve@sanityinc.com>
2015-11-02 21:19:10 +1300

    Neater formulation of fci-mode / popup interaction code

--- lisp/init-fci.el
+++ lisp/init-fci.el
@@ -10,14 +10,16 @@
   ;;(add-hook 'prog-mode-hook 'sanityinc/prog-mode-fci-settings)
 
   (defun sanityinc/fci-enabled-p ()
-    (and (boundp 'fci-mode) fci-mode))
+    (bound-and-true-p fci-mode))
 
   (defvar sanityinc/fci-mode-suppressed nil)
+  (make-variable-buffer-local 'sanityinc/fci-mode-suppressed)
+
   (defadvice popup-create (before suppress-fci-mode activate)
     "Suspend fci-mode while popups are visible"
     (let ((fci-enabled (sanityinc/fci-enabled-p)))
       (when fci-enabled
-        (set (make-local-variable 'sanityinc/fci-mode-suppressed) fci-enabled)
+        (setq sanityinc/fci-mode-suppressed fci-enabled)
         (turn-off-fci-mode))))
   (defadvice popup-delete (after restore-fci-mode activate)
     "Restore fci-mode when all popups have closed"
