38ebc1f81f911dddbe872fde1a4947a0b066f055
Steve Purcell <steve@sanityinc.com>
2015-11-02 21:20:53 +1300

    Abbreviate "Emacs-Lisp" mode name to "ELisp"

--- lisp/init-lisp.el
+++ lisp/init-lisp.el
@@ -1,6 +1,7 @@
 (require-package 'elisp-slime-nav)
 (dolist (hook '(emacs-lisp-mode-hook ielm-mode-hook))
   (add-hook hook 'turn-on-elisp-slime-nav-mode))
+(add-hook 'emacs-lisp-mode-hook (lambda () (setq mode-name "ELisp")))
 
 (require-package 'lively)
 
