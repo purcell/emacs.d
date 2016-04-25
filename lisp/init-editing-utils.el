305e82fd5ef894acf9941107f91a6a2095de9090
Steve Purcell <steve@sanityinc.com>
2016-04-25 21:15:37 +1200

    Don't override C-x C-m (fixes #341)

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -138,9 +138,6 @@
 ;;----------------------------------------------------------------------------
 ;; Handy key bindings
 ;;----------------------------------------------------------------------------
-;; To be able to M-x without meta
-(global-set-key (kbd "C-x C-m") 'execute-extended-command)
-
 ;; Vimmy alternatives to M-^ and C-u M-^
 (global-set-key (kbd "C-c j") 'join-line)
 (global-set-key (kbd "C-c J") (lambda () (interactive) (join-line 1)))
