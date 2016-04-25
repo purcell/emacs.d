82f1678cbdf2069be56e7a3b1d980e3480c02953
Steve Purcell <steve@sanityinc.com>
2016-04-25 19:42:15 +1200

    Don't try to outwit package-selected-packages logic

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -53,10 +53,7 @@ re-downloaded in order to locate PACKAGE."
   (if (package-installed-p package min-version)
       t
     (if (or (assoc package package-archive-contents) no-refresh)
-        (if (boundp 'package-selected-packages)
-            ;; Record this as a package the user installed explicitly
-            (package-install package nil)
-          (package-install package))
+        (package-install package)
       (progn
         (package-refresh-contents)
         (require-package package min-version t)))))
