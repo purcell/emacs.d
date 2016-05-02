2c4f93bf5142c2ec9f19000d8ca1fefa47c0572d
Steve Purcell <steve@sanityinc.com>
2016-05-02 20:04:05 +1200

    markdown-mode may not be installable in older emacsen

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -1,7 +1,6 @@
-(require-package 'markdown-mode)
-
-(after-load 'whitespace-cleanup-mode
-  (push 'markdown-mode whitespace-cleanup-mode-ignore-modes))
+(when (maybe-require-package 'markdown-mode)
+  (after-load 'whitespace-cleanup-mode
+    (push 'markdown-mode whitespace-cleanup-mode-ignore-modes)))
 
 
 (provide 'init-markdown)
