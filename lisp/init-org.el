918ae0c6bf5a183c4cb6e81d6bc06e7e73ea70e5
Steve Purcell <steve@sanityinc.com>
2016-05-03 19:04:52 +1200

    Handle when ob-ditaa doesn't define org-ditaa-jar-path
    The org-mode in older Emacsen (e.g. 24.3) doesn't actually define this
    variable, rather expecting it to be set by org blocks.
    
    Fixes #338, which was the result of this shortcoming.

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -40,7 +40,8 @@
         (delete-file zip-temp)))))
 
 (after-load 'ob-ditaa
-  (unless (file-exists-p org-ditaa-jar-path)
+  (unless (and (boundp 'org-ditaa-jar-path)
+               (file-exists-p org-ditaa-jar-path))
     (let ((jar-name "ditaa0_9.jar")
           (url "http://jaist.dl.sourceforge.net/project/ditaa/ditaa/0.9/ditaa0_9.zip"))
       (setq org-ditaa-jar-path (expand-file-name jar-name (file-name-directory user-init-file)))
