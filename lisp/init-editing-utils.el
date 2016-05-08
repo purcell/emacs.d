0394deca575a2c744b4a1328eca38883f32a26a7
Steve Purcell <steve@sanityinc.com>
2016-05-08 17:06:37 +1200

    Don't interfere with the new Emacs M-s binding

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -296,7 +296,7 @@ With arg N, insert N newlines."
 
 
 (require-package 'guide-key)
-(setq guide-key/guide-key-sequence '("C-x" "C-c" "C-x 4" "C-x 5" "C-c ;" "C-c ; f" "C-c ' f" "C-x n" "C-x C-r" "C-x r"))
+(setq guide-key/guide-key-sequence '("C-x" "C-c" "C-x 4" "C-x 5" "C-c ;" "C-c ; f" "C-c ' f" "C-x n" "C-x C-r" "C-x r" "M-s"))
 (guide-key-mode 1)
 (diminish 'guide-key-mode)
 
