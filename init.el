8bf863522616b445e4ca82fb75fd418443eb3d72
Alex Green <greenulito@gmail.com>
2016-06-04 11:24:24 +0300

    Solved flycheck rubocop issue on rvm based environment

--- init.el
+++ init.el
@@ -59,6 +59,7 @@
 (require 'init-grep)
 (require 'init-uniquify)
 (require 'init-ibuffer)
+(require 'init-rvm)
 (require 'init-flycheck)
 
 (require 'init-recentf)
---
+++ lisp/init-rvm.el
@@ -0,0 +1,6 @@
+(require-package 'rvm)
+
+(require 'rvm)
+(rvm-use-default) ;; use rvm's default ruby for the current Emacs session
+
+(provide 'init-rvm)
