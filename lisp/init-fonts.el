ce0d3968a8f9fcd4d3bc46f86183666dae6fb36f
Steve Purcell <steve@sanityinc.com>
2016-07-18 13:05:27 +1200

    Drop option to force default font for symbols
    As noted in Emacs bug 11450, this was resolved at some point since Emacs
    24.1.

--- lisp/init-fonts.el
+++ lisp/init-fonts.el
@@ -1,17 +1,6 @@
 ;;; Character sets
 
-(defcustom sanityinc/force-default-font-for-symbols nil
-  "When non-nil, force Emacs to use your default font for symbols."
-  :type 'boolean)
 
-(defun sanityinc/maybe-use-default-font-for-symbols ()
-  "Force Emacs to render symbols using the default font, if so configured."
-  (when sanityinc/force-default-font-for-symbols
-    (set-fontset-font t 'symbol (face-attribute 'default :family))))
-
-(add-hook 'after-init-hook 'sanityinc/maybe-use-default-font-for-symbols)
-
-
 ;;; Changing font sizes
 
 (require-package 'default-text-scale)
