58ccbbd4355e74e8a864e47367b4f12b82e9efb3
Steve Purcell <steve@sanityinc.com>
2016-07-20 10:16:11 +1200

    Use "C-c d" & "C-c D" for md/duplicate-{down/up}

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -195,8 +195,8 @@
 (global-set-key [M-S-up] 'md/move-lines-up)
 (global-set-key [M-S-down] 'md/move-lines-down)
 
-(global-set-key (kbd "C-c p") 'md/duplicate-down)
-(global-set-key (kbd "C-c P") 'md/duplicate-up)
+(global-set-key (kbd "C-c d") 'md/duplicate-down)
+(global-set-key (kbd "C-c D") 'md/duplicate-up)
 
 ;;----------------------------------------------------------------------------
 ;; Fix backward-up-list to understand quotes, see http://bit.ly/h7mdIL
