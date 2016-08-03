98a86042ee0d27869f9a12eda206fe6147712558
Steve Purcell <steve@sanityinc.com>
2016-08-03 20:53:51 +1200

    Add C-h to guide-key's trigger sequences
    Fixes #363

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -295,7 +295,7 @@ With arg N, insert N newlines."
 
 
 (require-package 'guide-key)
-(setq guide-key/guide-key-sequence '("C-x" "C-c" "C-x 4" "C-x 5" "C-c ;" "C-c ; f" "C-c ' f" "C-x n" "C-x C-r" "C-x r" "M-s"))
+(setq guide-key/guide-key-sequence '("C-x" "C-c" "C-x 4" "C-x 5" "C-c ;" "C-c ; f" "C-c ' f" "C-x n" "C-x C-r" "C-x r" "M-s" "C-h"))
 (add-hook 'after-init-hook
           (lambda ()
             (guide-key-mode 1)
