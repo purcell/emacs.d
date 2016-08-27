84fb7d0eb8adf204f6f13bc9989e152ae56b39cb
Steve Purcell <steve@sanityinc.com>
2016-08-27 17:06:45 +1200

    Handle that company-quickhelp-mode-map has been removed upstream
    Fixes #373

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -16,8 +16,6 @@
     (setq-default company-backends '((company-capf company-dabbrev-code) company-dabbrev)))
   (global-set-key (kbd "M-C-/") 'company-complete)
   (when (maybe-require-package 'company-quickhelp)
-    (after-load 'company-quickhelp
-      (define-key company-quickhelp-mode-map (kbd "M-h") nil))
     (add-hook 'after-init-hook 'company-quickhelp-mode))
 
   (defun sanityinc/local-push-company-backend (backend)
