941719032d8954e460f1323dd378ebf0609d0f03
Steve Purcell <steve@sanityinc.com>
2016-09-04 17:38:00 +1200

    Rely on autoload for switch-window

--- lisp/init-windows.el
+++ lisp/init-windows.el
@@ -7,7 +7,6 @@
 
 ;; Make "C-x o" prompt for a target window when there are more than 2
 (require-package 'switch-window)
-(require 'switch-window)
 (setq-default switch-window-shortcut-style 'alphabet)
 (setq-default switch-window-timeout nil)
 (global-set-key (kbd "C-x o") 'switch-window)
