e22e51def985f4f30c271f8e46b6ef832dd8fc91
Steve Purcell <steve@sanityinc.com>
2016-09-04 17:37:13 +1200

    Don't change default magit-process-popup-time

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -8,9 +8,7 @@
 
 
 (when (maybe-require-package 'magit)
-  (setq-default
-   magit-process-popup-time 10
-   magit-diff-refine-hunk t)
+  (setq-default magit-diff-refine-hunk t)
 
   ;; Hint: customize `magit-repo-dirs' so that you can use C-u M-F12 to
   ;; quickly open magit on any one of your projects.
