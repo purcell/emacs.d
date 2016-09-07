931a4fa55ddb2ca6f597d7a2a2d9287093fd9a1e
Steve Purcell <steve@sanityinc.com>
2016-09-07 21:12:15 +1200

    Hook loading of elisp-mode, not lisp-mode
    In newer Emacsen, modifying emacs-lisp-mode-map in an after-load block
    for lisp-mode doesn't work. The new elisp-mode feature is the one to
    look for. This change probably breaks C-c C-z in older Emacsen.

--- lisp/init-lisp.el
+++ lisp/init-lisp.el
@@ -70,7 +70,7 @@
       (funcall sanityinc/repl-switch-function sanityinc/repl-original-buffer)
     (error "No original buffer.")))
 
-(after-load 'lisp-mode
+(after-load 'elisp-mode
   (define-key emacs-lisp-mode-map (kbd "C-c C-z") 'sanityinc/switch-to-ielm))
 (after-load 'ielm
   (define-key ielm-map (kbd "C-c C-z") 'sanityinc/repl-switch-back))
