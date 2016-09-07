6481a5906642759cea426de4bbd6785ef632ac70
Steve Purcell <steve@sanityinc.com>
2016-09-07 21:31:40 +1200

    Add namespace to config-local function name

--- lisp/init-isearch.el
+++ lisp/init-isearch.el
@@ -30,15 +30,14 @@
 
 
 ;; http://www.emacswiki.org/emacs/ZapToISearch
-;; http://www.emacswiki.org/emacs/ZapToISearch
-(defun isearch-exit-other-end (rbeg rend)
+(defun sanityinc/isearch-exit-other-end (rbeg rend)
   "Exit isearch, but at the other end of the search string.
 This is useful when followed by an immediate kill."
   (interactive "r")
   (isearch-exit)
   (goto-char isearch-other-end))
 
-(define-key isearch-mode-map [(control return)] 'isearch-exit-other-end)
+(define-key isearch-mode-map [(control return)] 'sanityinc/isearch-exit-other-end)
 
 
 (provide 'init-isearch)
