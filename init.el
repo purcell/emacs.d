e883c101ca99e824a516c65e84080d35d8657622
Steve Purcell <steve@sanityinc.com>
2016-09-11 13:22:59 +1200

    Add note about ivy vs ido

--- init.el
+++ init.el
@@ -63,6 +63,8 @@
 
 (require 'init-recentf)
 (require 'init-smex)
+;; If you really prefer ido to ivy, change the comments below. I will
+;; likely remove the ido config in due course, though.
 ;; (require 'init-ido)
 (require 'init-ivy)
 (require 'init-hippie-expand)
