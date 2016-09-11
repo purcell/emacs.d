fd5a142318157ee3dc7f4f7d2c70eb9d64357ddf
Steve Purcell <steve@sanityinc.com>
2016-09-11 13:23:50 +1200

    Remove defunct error check

--- init.el
+++ init.el
@@ -150,8 +150,6 @@
 ;;----------------------------------------------------------------------------
 ;; Allow users to provide an optional "init-local" containing personal settings
 ;;----------------------------------------------------------------------------
-(when (file-exists-p (expand-file-name "init-local.el" user-emacs-directory))
-  (error "Please move init-local.el to ~/.emacs.d/lisp"))
 (require 'init-local nil t)
 
 
