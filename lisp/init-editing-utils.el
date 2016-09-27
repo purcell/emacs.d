147e07248d035e5044fa46e7abf6ece4f9fc519d
Steve Purcell <steve@sanityinc.com>
2016-09-28 07:36:36 +1300

    Change "C-c c" multiple-cursors bindings to "C-c m"
    Eliminates clash with org-capture, thanks @creamidea, closes #384.

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -159,10 +159,10 @@
 (global-set-key (kbd "C-+") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
 ;; From active region to multiple cursors:
-(global-set-key (kbd "C-c c r") 'set-rectangular-region-anchor)
-(global-set-key (kbd "C-c c c") 'mc/edit-lines)
-(global-set-key (kbd "C-c c e") 'mc/edit-ends-of-lines)
-(global-set-key (kbd "C-c c a") 'mc/edit-beginnings-of-lines)
+(global-set-key (kbd "C-c m r") 'set-rectangular-region-anchor)
+(global-set-key (kbd "C-c m c") 'mc/edit-lines)
+(global-set-key (kbd "C-c m e") 'mc/edit-ends-of-lines)
+(global-set-key (kbd "C-c m a") 'mc/edit-beginnings-of-lines)
 
 
 ;; Train myself to use M-f and M-b instead
