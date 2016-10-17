3bc26ebe54c943f97c4547cb0e0899c88f6f0b89
Steve Purcell <steve@sanityinc.com>
2016-10-17 18:57:09 +1300

    Don't assume rinari is installable in Emacs 23

--- lisp/init-rails.el
+++ lisp/init-rails.el
@@ -1,7 +1,7 @@
-(require-package 'rinari)
-(after-load 'rinari
-  (diminish 'rinari-minor-mode "Rin"))
-(global-rinari-mode)
+(when (maybe-require-package 'rinari)
+  (after-load 'rinari
+    (diminish 'rinari-minor-mode "Rin"))
+  (global-rinari-mode))
 
 (defun update-rails-ctags ()
   (interactive)
