88c190c44b468b1c3063a9a330e608c5521f2205
Steve Purcell <steve@sanityinc.com>
2017-01-06 21:42:14 +1300

    Kill accidentally-added init-helm and init-local files

--- lisp/init-helm.el
+++
@@ -1,6 +0,0 @@
-(when (maybe-require-package 'helm)
-  (when (maybe-require-package 'helm-smex)
-    (global-set-key [remap execute-extended-command] #'helm-smex)
-    (global-set-key (kbd "M-X") #'helm-smex-major-mode-commands)))
-
-(provide 'init-helm)
--- lisp/init-local.el
+++
@@ -1,9 +0,0 @@
-(setq
- magit-repository-directories
- '(("~" . 1)
-   ("~/Projects" . 1)
-   ("~/Projects/External" . 1))
- exec-path-from-shell-arguments '("-l")
- org-default-notes-file "~/Documents/org/inbox.org")
-
-(provide 'init-local)
