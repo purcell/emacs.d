40bdfa8536e6251e6d81f3a6d6f894193b34f2d5
Steve Purcell <steve@sanityinc.com>
2017-03-24 20:15:10 +1300

    Default elm-format-on-save to t if elm-format is installed

--- lisp/init-elm.el
+++ lisp/init-elm.el
@@ -2,7 +2,9 @@
   (after-load 'elm-mode
     (diminish 'elm-indent-mode)
     (when (executable-find "elm-oracle")
-      (add-hook 'elm-mode-hook 'elm-oracle-setup-completion)))
+      (add-hook 'elm-mode-hook 'elm-oracle-setup-completion))
+    (when (executable-find "elm-format")
+      (setq-default elm-format-on-save t)))
   (when (maybe-require-package 'flycheck-elm)
     (after-load 'elm-mode
       (flycheck-elm-setup))))
