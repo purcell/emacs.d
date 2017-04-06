0a2c2bdb7ff6ffaa446f8d6ca940d62d4342150f
Steve Purcell <steve@sanityinc.com>
2017-04-06 18:55:19 +1200

    regex-tool now requires Emacs 24.x

--- init.el
+++ init.el
@@ -130,7 +130,7 @@
 (require-package 'dsvn)
 (when *is-a-mac*
   (require-package 'osx-location))
-(require-package 'regex-tool)
+(maybe-require-package 'regex-tool)
 
 ;;----------------------------------------------------------------------------
 ;; Allow access from emacsclient
