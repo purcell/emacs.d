f48c73a7eb74877c06b2e5503b5ab6711f91ee88
Steve Purcell <steve@sanityinc.com>
2017-04-09 14:08:38 +1200

    Warn about possibly-disabled functionality for all Emacs < 24.4

--- init.el
+++ init.el
@@ -5,7 +5,7 @@
 (let ((minver "24.1"))
   (when (version<= emacs-version minver)
     (error "Your Emacs is too old -- this config requires v%s or higher" minver)))
-(when (version<= emacs-version "24")
+(when (version<= emacs-version "24.4")
   (message "Your Emacs is old, and some functionality in this config will be disabled. Please upgrade if possible."))
 
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
