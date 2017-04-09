f161043f0bd48de2c98df3b8950ef67f35983916
Steve Purcell <steve@sanityinc.com>
2017-04-09 14:12:58 +1200

    Autoload mwheel-install

--- lisp/init-xterm.el
+++ lisp/init-xterm.el
@@ -3,6 +3,8 @@
 (global-set-key [mouse-4] (lambda () (interactive) (scroll-down 1)))
 (global-set-key [mouse-5] (lambda () (interactive) (scroll-up 1)))
 
+(autoload 'mwheel-install "mwheel")
+
 (defun sanityinc/console-frame-setup ()
   (xterm-mouse-mode 1) ; Mouse in a terminal (Use shift to paste with middle button)
   (mwheel-install))
