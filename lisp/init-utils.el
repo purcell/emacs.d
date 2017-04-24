0171dbd58b3ee6840b7654582647b9f77557859a
Steve Purcell <steve@sanityinc.com>
2017-04-24 13:14:31 +1200

    Tidy up delete-this-file

--- lisp/init-utils.el
+++ lisp/init-utils.el
@@ -36,7 +36,8 @@
 (defun delete-this-file ()
   "Delete the current file, and kill the buffer."
   (interactive)
-  (or (buffer-file-name) (error "No file is currently being edited"))
+  (unless (buffer-file-name)
+    (error "No file is currently being edited"))
   (when (yes-or-no-p (format "Really delete '%s'?"
                              (file-name-nondirectory buffer-file-name)))
     (delete-file (buffer-file-name))
