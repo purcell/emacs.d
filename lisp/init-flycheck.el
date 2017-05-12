c1d46ec3837708e79b0ce3a8efebfe2eefe6f4d0
Steve Purcell <steve@sanityinc.com>
2017-05-13 11:13:48 +1200

    Enable flycheck-color-mode-line-mode

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -1,6 +1,9 @@
 (when (maybe-require-package 'flycheck)
   (add-hook 'after-init-hook 'global-flycheck-mode)
-  (setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list))
+  (setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list)
+
+  (when (maybe-require-package 'flycheck-color-mode-line)
+    (add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode)))
 
 
 (provide 'init-flycheck)
