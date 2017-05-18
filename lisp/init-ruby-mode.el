7e59719e7d302829502d43d5b78d6447093204c9
Steve Purcell <steve@sanityinc.com>
2017-05-18 23:12:09 +1200

    Soft-require robe

--- lisp/init-ruby-mode.el
+++ lisp/init-ruby-mode.el
@@ -49,13 +49,13 @@
 
 
 ;;; Robe
-(require-package 'robe)
-(after-load 'ruby-mode
-  (add-hook 'ruby-mode-hook 'robe-mode))
-(after-load 'company
-  (dolist (hook '(ruby-mode-hook inf-ruby-mode-hook html-erb-mode-hook haml-mode))
-    (add-hook hook
-              (lambda () (sanityinc/local-push-company-backend 'company-robe)))))
+(when (maybe-require-package 'robe)
+  (after-load 'ruby-mode
+    (add-hook 'ruby-mode-hook 'robe-mode))
+  (after-load 'company
+    (dolist (hook '(ruby-mode-hook inf-ruby-mode-hook html-erb-mode-hook haml-mode))
+      (add-hook hook
+                (lambda () (sanityinc/local-push-company-backend 'company-robe))))))
 
 
 
