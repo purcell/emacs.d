27143a80a848b3e3ae5ba39d64b8e272e6fcc42d
Steve Purcell <steve@sanityinc.com>
2017-05-19 07:40:46 +1200

    Soft-require ledger-mode, which now need Emacs 24.3

--- lisp/init-ledger.el
+++ lisp/init-ledger.el
@@ -1,23 +1,23 @@
-(require-package 'ledger-mode)
+(when (maybe-require-package 'ledger-mode)
 
-(add-to-list 'auto-mode-alist '("\\.ledger$" . ledger-mode))
+  (add-to-list 'auto-mode-alist '("\\.ledger$" . ledger-mode))
 
-(when (maybe-require-package 'flycheck-ledger)
-  (after-load 'flycheck
-    (after-load 'ledger-mode
-      (require 'flycheck-ledger))))
+  (when (maybe-require-package 'flycheck-ledger)
+    (after-load 'flycheck
+      (after-load 'ledger-mode
+        (require 'flycheck-ledger))))
 
-(after-load 'ledger-mode
-  (define-key ledger-mode-map (kbd "RET") 'newline)
-  (define-key ledger-mode-map (kbd "C-o") 'open-line))
+  (after-load 'ledger-mode
+    (define-key ledger-mode-map (kbd "RET") 'newline)
+    (define-key ledger-mode-map (kbd "C-o") 'open-line))
 
-(setq ledger-highlight-xact-under-point nil
-      ledger-use-iso-dates nil)
+  (setq ledger-highlight-xact-under-point nil
+        ledger-use-iso-dates nil)
 
-(after-load 'ledger-mode
-  (when (memq window-system '(mac ns))
-    (exec-path-from-shell-copy-env "LEDGER_FILE")))
+  (after-load 'ledger-mode
+    (when (memq window-system '(mac ns))
+      (exec-path-from-shell-copy-env "LEDGER_FILE")))
 
-(add-hook 'ledger-mode-hook 'goto-address-prog-mode)
+  (add-hook 'ledger-mode-hook 'goto-address-prog-mode))
 
 (provide 'init-ledger)
