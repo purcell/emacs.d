593e9f3f58c070a01cf9b8d17b96a3a10b8ec0c6
Steve Purcell <steve@sanityinc.com>
2017-05-20 18:25:43 +1200

    In diff-hl mode, make clicking the fringe show the diff

--- lisp/init-vc.el
+++ lisp/init-vc.el
@@ -1,6 +1,11 @@
 (when (maybe-require-package 'diff-hl)
-  (add-hook 'prog-mode-hook 'turn-on-diff-hl-mode)
-  (add-hook 'vc-dir-mode-hook 'turn-on-diff-hl-mode))
+  (add-hook 'prog-mode-hook 'diff-hl-mode)
+  (add-hook 'vc-dir-mode-hook 'diff-hl-mode)
+
+  (after-load 'diff-hl
+    (define-key diff-hl-mode-map
+      (kbd "<left-fringe> <mouse-1>")
+      'diff-hl-diff-goto-hunk)))
 
 (maybe-require-package 'browse-at-remote)
 
