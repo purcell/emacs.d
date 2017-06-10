df7f601904da59eb30de64beff5c53f3060a321d
Steve Purcell <steve@sanityinc.com>
2017-06-10 15:55:30 +1200

    Change emacs version bound checks
    Closes #456. Thanks @YuanPres.

--- init.el
+++ init.el
@@ -3,9 +3,9 @@
 ;;; a number of other files.
 
 (let ((minver "24.1"))
-  (when (version<= emacs-version minver)
+  (when (version< emacs-version minver)
     (error "Your Emacs is too old -- this config requires v%s or higher" minver)))
-(when (version<= emacs-version "24.4")
+(when (version< emacs-version "24.4")
   (message "Your Emacs is old, and some functionality in this config will be disabled. Please upgrade if possible."))
 
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
