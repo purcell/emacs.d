ecf17becbdb3d6e50b62938ff4ec5dfba3c915ff
Steve Purcell <steve@sanityinc.com>
2017-08-08 12:27:39 +1200

    Disable flyspell's binding of C-; if enabled (fixes #476)

--- lisp/init-flyspell.el
+++ lisp/init-flyspell.el
@@ -4,6 +4,7 @@
 (add-hook 'prog-mode-hook 'flyspell-prog-mode)
 
 (after-load 'flyspell
+  (define-key flyspell-mode-map (kbd "C-;") nil)
   (add-to-list 'flyspell-prog-text-faces 'nxml-text-face))
 
 
