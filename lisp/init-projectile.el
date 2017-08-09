f536acf0448c018b7bcc0fe32fa6f2168318a683
Steve Purcell <steve@sanityinc.com>
2017-08-09 16:12:13 +1200

    Change projectile modeline lighter and use non-aliased mode name

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -1,5 +1,5 @@
 (when (maybe-require-package 'projectile)
-  (add-hook 'after-init-hook 'projectile-global-mode)
+  (add-hook 'after-init-hook 'projectile-mode)
 
   ;; The following code means you get a menu if you hit "C-c p" and wait
   (after-load 'guide-key
@@ -11,8 +11,8 @@
      projectile-mode-line
      '(:eval
        (if (file-remote-p default-directory)
-           " Pr"
-         (format " Pr[%s]" (projectile-project-name)))))))
+           " Proj"
+         (format " Proj[%s]" (projectile-project-name)))))))
 
 
 (provide 'init-projectile)
