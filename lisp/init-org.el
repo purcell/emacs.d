393fe510795c4d2cfb958f2863a0ec93431979d7
Steve Purcell <steve@sanityinc.com>
2017-08-24 17:38:38 +1200

    Mark up variable name in comment

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -93,7 +93,7 @@ typical word processor."
 (global-set-key (kbd "C-c c") 'org-capture)
 
 (setq org-capture-templates
-      `(("t" "todo" entry (file "")  ; "" => org-default-notes-file
+      `(("t" "todo" entry (file "")  ; "" => `org-default-notes-file'
          "* NEXT %?\n%U\n" :clock-resume t)
         ("n" "note" entry (file "")
          "* %? :NOTE:\n%U\n%a\n" :clock-resume t)
