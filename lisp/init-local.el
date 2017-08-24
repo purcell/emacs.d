50f6e108363435a389391de85f02a36091a2202b
Steve Purcell <steve@sanityinc.com>
2017-08-24 21:38:06 +1200

    Remove accidentally-committed init-local.el

--- lisp/init-local.el
+++
@@ -1,6 +0,0 @@
-(setq magit-repository-directories '(("~/Projects" . 1) ("~/Projects/External" . 1))
-      org-default-notes-file "~/Documents/org/inbox.org"
-      org-directory "~/Documents/org"
-      exec-path-from-shell-arguments '("-i"))
-
-(provide 'init-local)
