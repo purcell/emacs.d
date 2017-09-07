71857828f69bb23584b0dade6c109b1bb87bd2d9
Brian Ramos <brirams@users.noreply.github.com>
2017-09-07 15:38:08 -0700

    copy/paste to/from macosx

---
+++ lisp/init-local.el
@@ -0,0 +1,14 @@
+;; copy shit into osx clipboard
+(defun copy-from-osx ()
+  (shell-command-to-string "pbpaste"))
+
+(defun paste-to-osx (text &optional push)
+  (let ((process-connection-type nil))
+    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+      (process-send-string proc text)
+      (process-send-eof proc))))
+
+(setq interprogram-cut-function 'paste-to-osx)
+(setq interprogram-paste-function 'copy-from-osx)
+
+(provide 'init-local)
