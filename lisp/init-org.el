2c5d553cd68aaf532e87e459083a3727f9306eba
Steve Purcell <steve@sanityinc.com>
2017-09-08 22:31:29 +1200

    Use the same sourceforge mirror for both plantuml.jar and ditaa.jar

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -49,7 +49,7 @@
 
 (after-load 'ob-plantuml
   (let ((jar-name "plantuml.jar")
-        (url "https://phoenixnap.dl.sourceforge.net/project/plantuml/plantuml.jar"))
+        (url "https://jaist.dl.sourceforge.net/project/plantuml/plantuml.jar"))
     (setq org-plantuml-jar-path (expand-file-name jar-name (file-name-directory user-init-file)))
     (unless (file-exists-p org-plantuml-jar-path)
       (url-copy-file url org-plantuml-jar-path))))
