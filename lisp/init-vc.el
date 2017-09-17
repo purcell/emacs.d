3a8e68ba5b7e6b367db46af59d9beea27db25352
Steve Purcell <steve@sanityinc.com>
2017-09-18 11:00:53 +1200

    Refresh diff-hl after magit commits

--- lisp/init-vc.el
+++ lisp/init-vc.el
@@ -1,6 +1,7 @@
 (when (maybe-require-package 'diff-hl)
   (add-hook 'prog-mode-hook 'diff-hl-mode)
   (add-hook 'vc-dir-mode-hook 'diff-hl-mode)
+  (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh)
 
   (after-load 'diff-hl
     (define-key diff-hl-mode-map
