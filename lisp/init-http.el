ee2eab9692c5b4b1dc55c3bbf17ec01ccb17df8b
Steve Purcell <steve@sanityinc.com>
2017-10-09 20:29:36 +1300

    Use restclient-mode for .rest files

--- lisp/init-http.el
+++ lisp/init-http.el
@@ -1,5 +1,7 @@
 (maybe-require-package 'httprepl)
 (when (maybe-require-package 'restclient)
+  (add-auto-mode 'restclient-mode "\\.rest\\'")
+
   (defun sanityinc/restclient ()
     (interactive)
     (with-current-buffer (get-buffer-create "*restclient*")
