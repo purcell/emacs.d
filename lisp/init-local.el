c79dae02b3cd10593bde603be13c8c85613dfbe7
Brian Ramos <brirams@users.noreply.github.com>
2017-10-25 14:45:04 -0700

    delete file and close buffer

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -22,4 +22,19 @@
 
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
+
+(defun delete-file-and-buffer ()
+  "Kill the current buffer and deletes the file it is visiting."
+  (interactive)
+  (let ((filename (buffer-file-name)))
+    (when filename
+      (if (vc-backend filename)
+          (vc-delete-file filename)
+        (progn
+          (delete-file filename)
+          (message "Deleted file %s" filename)
+          (kill-buffer))))))
+
+(global-set-key (kbd "C-c D")  'delete-file-and-buffer)
+
 (provide 'init-local)
