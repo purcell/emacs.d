2d774a2e1147dff069997ce485898e1282659484
Brian Ramos <brirams@users.noreply.github.com>
2017-10-25 13:54:46 -0700

    add a few niceties, as well as comments of go utils that you'll need

--- lisp/init-go.el
+++ lisp/init-go.el
@@ -1,10 +1,18 @@
 (require 'init-company)
 
+;; Things you're going to need:
+;; go get github.com/rogpeppe/godef
+;; go get -u github.com/golang/lint/golint
+;; go get -u golang.org/x/tools/cmd/godoc
+
 (when (maybe-require-package 'go-mode)
   (add-hook 'before-save-hook #'gofmt-before-save)
   (add-hook 'go-mode-hook 'flycheck-mode)
+  (setq compile-command "go build -v && go test -v && golint")
+  (local-set-key (kbd "M-.") 'godef-jump)
 
   (require-package `company-go)
+  (global-set-key (kbd "\C-c\C-c") 'compile)
   (add-hook 'go-mode-hook 'company-mode))
 
 (provide 'init-go)
