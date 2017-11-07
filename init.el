976c726ce6b16bb1df3d6f900a770a4b3cb834fc
slucx <slucfly@gmail.com>
2017-11-07 22:20:22 +0800

    update

--- init.el
+++ init.el
@@ -18,7 +18,7 @@
 ;;----------------------------------------------------------------------------
 ;; Adjust garbage collection thresholds during startup, and thereafter
 ;;----------------------------------------------------------------------------
-(let ((normal-gc-cons-threshold (* 128 1024 1024))
+(let ((normal-gc-cons-threshold (* 20 1024 1024))
       (init-gc-cons-threshold (* 128 1024 1024)))
   (setq gc-cons-threshold init-gc-cons-threshold)
   (add-hook 'after-init-hook
