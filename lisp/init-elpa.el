a8481a2179ba2c38e51c28504a04713dd33b1fa2
Steve Purcell <steve@sanityinc.com>
2017-11-14 21:18:04 +1300

    No longer rename old "elpa" dirs to versioned ones
    Fixes #515

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -13,11 +13,6 @@
 (let ((versioned-package-dir
        (expand-file-name (format "elpa-%s.%s" emacs-major-version emacs-minor-version)
                          user-emacs-directory)))
-  (when (file-directory-p package-user-dir)
-    (message "Default package locations have changed in this config: renaming old package dir %s to %s."
-             package-user-dir
-             versioned-package-dir)
-    (rename-file package-user-dir versioned-package-dir))
   (setq package-user-dir versioned-package-dir))
 
 
