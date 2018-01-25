342baea11838ba7f22ccee43e8b267048b8e6962
Steve Purcell <steve@sanityinc.com>
2018-01-25 20:35:11 +1300

    Remove another use of the tidy package
    See #540

--- lisp/init-html.el
+++ lisp/init-html.el
@@ -1,6 +1,3 @@
-(require-package 'tidy)
-(add-hook 'html-mode-hook (lambda () (tidy-build-menu html-mode-map)))
-
 (require-package 'tagedit)
 (after-load 'sgml-mode
   (tagedit-add-paredit-like-keybindings)
