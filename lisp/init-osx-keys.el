0c7d2cb3ddd336e02a17b28f6ac6afd44beaa0e6
Steve Purcell <steve@sanityinc.com>
2018-01-27 13:32:57 +1300

    Setting default-input-method on OS X is a waste of time
    See #320

--- lisp/init-osx-keys.el
+++ lisp/init-osx-keys.el
@@ -1,7 +1,6 @@
 (when *is-a-mac*
   (setq mac-command-modifier 'meta)
   (setq mac-option-modifier 'none)
-  (setq-default default-input-method "MacOSX")
   ;; Make mouse wheel / trackpad scrolling less jerky
   (setq mouse-wheel-scroll-amount '(1
                                     ((shift) . 5)
