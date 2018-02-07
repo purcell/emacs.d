76e2147b4e0c4e7e33e72ba189f48894f4b24283
Steve Purcell <steve@sanityinc.com>
2018-02-08 09:25:08 +1300

    Enable dimmer-mode by default

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -31,7 +31,9 @@
   (reapply-themes))
 
 
-(maybe-require-package 'dimmer)
+(when (maybe-require-package 'dimmer)
+  (setq-default dimmer-fraction 0.1)
+  (add-hook 'after-init-hook 'dimmer-mode))
 
 
 (provide 'init-themes)
