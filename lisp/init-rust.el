70df4197cd1184f45b7c5ca35616b949e56c142f
David Corking <dmc-source@dcorking.com>
2018-02-26 11:00:47 +0000

    Configure flycheck-rust package

--- lisp/init-rust.el
+++ lisp/init-rust.el
@@ -3,5 +3,7 @@
     (add-hook 'rust-mode-hook #'racer-mode)
     (add-hook 'racer-mode-hook #'eldoc-mode)))
 
+(when (maybe-require-package 'flycheck-rust)
+  (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
 
 (provide 'init-rust)
