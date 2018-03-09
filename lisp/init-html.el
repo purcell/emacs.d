48ce625a170a585b8d86b30d979a901a783a0426
Steve Purcell <steve@sanityinc.com>
2018-03-10 10:52:51 +1300

    Disable tagedit's M-? binding to avoid shadowing mine
    Fixes #550

--- lisp/init-html.el
+++ lisp/init-html.el
@@ -3,6 +3,9 @@
   (tagedit-add-paredit-like-keybindings)
   (add-hook 'sgml-mode-hook (lambda () (tagedit-mode 1))))
 
+(after-load 'tagedit
+  (define-key tagedit-mode-map (kbd "M-?") nil))
+
 (add-auto-mode 'html-mode "\\.\\(jsp\\|tmpl\\)\\'")
 
 ;; Note: ERB is configured in init-ruby
