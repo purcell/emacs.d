3e0ca7471083bfa7a507cefc9d1f1886604f8d51
Steve Purcell <steve@sanityinc.com>
2018-03-19 10:08:35 +1300

    Stricter auto-mode-alist patterns

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -1,7 +1,7 @@
 ;;----------------------------------------------------------------------------
 ;; Misc config - yet to be placed in separate files
 ;;----------------------------------------------------------------------------
-(add-auto-mode 'tcl-mode "Portfile\\'")
+(add-auto-mode 'tcl-mode "^Portfile\\'")
 (fset 'yes-or-no-p 'y-or-n-p)
 
 (add-hook 'prog-mode-hook 'goto-address-prog-mode)
@@ -38,7 +38,7 @@
   ;; Support a slightly more idiomatic quit binding in re-builder
   (define-key reb-mode-map (kbd "C-c C-k") 'reb-quit))
 
-(add-auto-mode 'conf-mode "Procfile")
+(add-auto-mode 'conf-mode "^Procfile\\'")
 
 
 (provide 'init-misc)
