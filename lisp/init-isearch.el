bedd5f69d492f891d8bef247d877ee6baa5d4ec7
Steve Purcell <steve@sanityinc.com>
2018-03-26 22:32:16 +1300

    Defer loading of some isearch config

--- lisp/init-isearch.el
+++ lisp/init-isearch.el
@@ -6,11 +6,13 @@
   (global-set-key [remap query-replace] 'anzu-query-replace))
 
 ;; Activate occur easily inside isearch
-(when (maybe-require-package 'emacs '(24 3))
-  (define-key isearch-mode-map (kbd "C-c C-o") 'isearch-occur)) ;; to match ivy conventions
+(after-load 'isearch
+  ;; DEL during isearch should edit the search string, not jump back to the previous result
+  (define-key isearch-mode-map [remap isearch-delete-char] 'isearch-del-char)
 
-;; DEL during isearch should edit the search string, not jump back to the previous result
-(define-key isearch-mode-map [remap isearch-delete-char] 'isearch-del-char)
+  (when (fboundp 'isearch-occur)
+    ;; to match ivy conventions
+    (define-key isearch-mode-map (kbd "C-c C-o") 'isearch-occur)))
 
 ;; Search back/forth for the symbol at point
 ;; See http://www.emacswiki.org/emacs/SearchAtPoint
