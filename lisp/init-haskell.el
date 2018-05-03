32770f266ec5fc78a0b1bf2a63266a34c2b3f723
Steve Purcell <steve@sanityinc.com>
2018-05-04 08:54:17 +1200

    In Haskell-related buffers, use Stack's path for executables

--- lisp/init-haskell.el
+++ lisp/init-haskell.el
@@ -47,9 +47,31 @@
 (after-load 'page-break-lines
   (push 'haskell-mode page-break-lines-modes))
 
+
+
+(define-minor-mode stack-exec-path-mode
+  "If this is a stack project, set `exec-path' to the path \"stack exec\" would use."
+  nil
+  :lighter ""
+  :global nil
+  (if stack-exec-path-mode
+      (when (and (executable-find "stack")
+                 (locate-dominating-file default-directory "stack.yaml"))
+        (setq-local
+         exec-path (parse-colon-path
+                    (replace-regexp-in-string "[\r\n]+\\'" ""
+                                              (shell-command-to-string "stack path --bin-path")))))
+    (kill-local-variable 'exec-path)))
+
+(add-hook 'haskell-mode-hook 'stack-exec-path-mode)
+
+
 
 (when (maybe-require-package 'dhall-mode)
-  (add-hook 'dhall-mode-hook 'sanityinc/no-trailing-whitespace))
+  (add-hook 'dhall-mode-hook 'sanityinc/no-trailing-whitespace)
+  (add-hook 'dhall-mode-hook 'stack-exec-path-mode))
+
+
 
 
 (provide 'init-haskell)
