ec642be4ab82c18f8f0a287a9435d01bd8d06733
Steve Purcell <steve@sanityinc.com>
2018-08-28 14:17:52 +1200

    Choose non-clashing binding for resetting frame opacity to 100

--- lisp/init-gui-frames.el
+++ lisp/init-gui-frames.el
@@ -64,7 +64,7 @@
 ;; TODO: use seethru package instead?
 (global-set-key (kbd "M-C-8") (lambda () (interactive) (sanityinc/adjust-opacity nil -2)))
 (global-set-key (kbd "M-C-9") (lambda () (interactive) (sanityinc/adjust-opacity nil 2)))
-(global-set-key (kbd "M-C-0") (lambda () (interactive) (modify-frame-parameters nil `((alpha . 100)))))
+(global-set-key (kbd "M-C-7") (lambda () (interactive) (modify-frame-parameters nil `((alpha . 100)))))
 
 
 (setq frame-title-format
