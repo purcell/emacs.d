a18ac4cd28a2f168d722d4bde1d352981ef9b744
re-mx <reimu-mx@outlook.com>
2018-08-29 15:40:43 +0800

    Set gc-cons-threshold to normal-gc-cons-threshold in emacs-startup-hook

--- init.el
+++ init.el
@@ -22,7 +22,7 @@
 (let ((normal-gc-cons-threshold (* 20 1024 1024))
       (init-gc-cons-threshold (* 128 1024 1024)))
   (setq gc-cons-threshold init-gc-cons-threshold)
-  (add-hook 'after-init-hook
+  (add-hook 'emacs-startup-hook
             (lambda () (setq gc-cons-threshold normal-gc-cons-threshold))))
 
 ;;----------------------------------------------------------------------------
