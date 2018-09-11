11b345bd4d338dc73befc3964ebb9c1a493eb7eb
re-mx <reimu-mx@outlook.com>
2018-09-12 07:03:29 +0800

    Use elm-test-runner instead of elm-test

--- lisp/init-elm.el
+++ lisp/init-elm.el
@@ -6,7 +6,7 @@
               (lambda () (sanityinc/local-push-company-backend 'company-elm)))
     (when (executable-find "elm-format")
       (setq-default elm-format-on-save t)))
-  (maybe-require-package 'elm-test)
+  (maybe-require-package 'elm-test-runner)
   (when (maybe-require-package 'flycheck-elm)
     (after-load 'elm-mode
       (flycheck-elm-setup))))
