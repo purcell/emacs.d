486dcbbe70cf5d77da40b191658d11a53ce37620
Steve Purcell <steve@sanityinc.com>
2018-09-25 09:55:23 +1200

    Add psci support for purescript

--- lisp/init-purescript.el
+++ lisp/init-purescript.el
@@ -6,8 +6,17 @@
                 (company-mode)
                 (flycheck-mode)
                 (turn-on-purescript-indentation))))
+
+  (when (maybe-require-package 'psci)
+    (add-hook 'purescript-mode-hook 'inferior-psci-mode))
+
   (when (maybe-require-package 'add-node-modules-path)
     (after-load 'purescript-mode
-      (add-hook 'purescript-mode-hook 'add-node-modules-path))))
+      (add-hook 'purescript-mode-hook 'add-node-modules-path))
+    (after-load 'psci
+      (advice-add 'psci :around (lambda (oldfun &rest args)
+                                  (let ((psci/purs-path (or (executable-find "purs")
+                                                            psci/purs-path)))
+                                    (apply oldfun args)))))))
 
 (provide 'init-purescript)
