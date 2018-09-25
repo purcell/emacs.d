49fd555ed9e25e437916166783c7d5a1778c1372
re-mx <reimu-mx@outlook.com>
2018-09-25 15:26:42 +0800

    Set shorter projectile modeline correctly

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -1,11 +1,11 @@
 (when (maybe-require-package 'projectile)
   (add-hook 'after-init-hook 'projectile-mode)
 
-  (after-load 'projectile
-    (define-key projectile-mode-map (kbd "C-c C-p") 'projectile-command-map)
+  ;; Shorter modeline
+  (setq-default projectile-mode-line-lighter " Proj")
 
-    ;; Shorter modeline
-    (setq-default projectile-mode-line-lighter " Proj"))
+  (after-load 'projectile
+    (define-key projectile-mode-map (kbd "C-c C-p") 'projectile-command-map))
 
   (maybe-require-package 'ibuffer-projectile))
 
