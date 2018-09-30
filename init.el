85b5ba63faf508ae1b7d1d2b72e8c75c9ba0811e
Steve Purcell <steve@sanityinc.com>
2018-10-01 11:28:01 +1300

    Require at least 24.4

--- init.el
+++ init.el
@@ -4,10 +4,10 @@
 ;;; This file bootstraps the configuration, which is divided into
 ;;; a number of other files.
 
-(let ((minver "24.3"))
+(let ((minver "24.4"))
   (when (version< emacs-version minver)
     (error "Your Emacs is too old -- this config requires v%s or higher" minver)))
-(when (version< emacs-version "24.5")
+(when (version< emacs-version "25.1")
   (message "Your Emacs is old, and some functionality in this config will be disabled. Please upgrade if possible."))
 
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
