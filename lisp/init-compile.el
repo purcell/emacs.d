2701170d57e7e79f61fc2ebecf4cdfef512c1fd1
re-mx <reimu-mx@outlook.com>
2018-10-21 09:36:31 +0800

    Fix wrong number of arguments for sanityinc/save-compilation-buffer

--- lisp/init-compile.el
+++ lisp/init-compile.el
@@ -24,7 +24,7 @@
   "The last buffer in which compilation took place.")
 
 (after-load 'compile
-  (defun sanityinc/save-compilation-buffer ()
+  (defun sanityinc/save-compilation-buffer (&rest _)
     "Save the compilation buffer to find it later."
     (setq sanityinc/last-compilation-buffer next-error-last-buffer))
   (advice-add 'compilation-start :after 'sanityinc/save-compilation-buffer)
