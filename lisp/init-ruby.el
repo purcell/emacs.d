c27073766c02c194cbc2a595a8579c24082edbea
re-mx <reimu-mx@outlook.com>
2018-11-02 21:53:21 +0800

    Use lambda function instead of sanityinc/ensure-mmm-erb-loaded

--- lisp/init-ruby.el
+++ lisp/init-ruby.el
@@ -82,13 +82,11 @@
 
 ;;; ERB
 (require-package 'mmm-mode)
-(defun sanityinc/ensure-mmm-erb-loaded ()
-  (require 'mmm-erb))
 
 (require 'derived)
 
 (defun sanityinc/set-up-mode-for-erb (mode)
-  (add-hook (derived-mode-hook-name mode) 'sanityinc/ensure-mmm-erb-loaded)
+  (add-hook (derived-mode-hook-name mode) (lambda () (require 'mmm-erb)))
   (mmm-add-mode-ext-class mode "\\.erb\\'" 'erb))
 
 (let ((html-erb-modes '(html-mode html-erb-mode nxml-mode)))
