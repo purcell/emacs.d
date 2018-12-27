24149c989ac3fb0696173cd70664d5f2731d8b60
josephyoung <2t@live.com>
2018-12-27 14:01:29 +0800

    Update init-locales.el

--- lisp/init-locales.el
+++ lisp/init-locales.el
@@ -10,11 +10,30 @@
       (sanityinc/utf8-locale-p (getenv "LANG"))))
 
 (when (or window-system (sanityinc/locale-is-utf8-p))
-  (set-language-environment 'gb18030)
-  (setq locale-coding-system 'gb18030)
-  (set-default-coding-systems 'gb18030)
-  (set-terminal-coding-system 'gb18030)
-  (set-selection-coding-system (if (eq system-type 'windows-nt) 'gb18030 'gb18030))
-  (prefer-coding-system 'gb18030))
+  (set-language-environment 'chinese-gb18030)
+  (setq locale-coding-system 'chinese-gb18030)
+  (set-default-coding-systems 'chinese-gb18030)
+  (set-terminal-coding-system 'chinese-gb18030)
+  (set-selection-coding-system (if (eq system-type 'windows-nt) 'chinese-gb18030 'utf-8))
+  (prefer-coding-system 'chinese-gb18030))
+
+;; Charset 设置
+;; (use-package mule
+;;              :ensure nil
+;;              :config
+
+;;              (set-language-environment "UTF-8")
+;;              (set-buffer-file-coding-system 'utf-8-unix)
+;;              (set-clipboard-coding-system 'utf-8-unix)
+;;              (set-file-name-coding-system 'utf-8-unix)
+;;              (set-keyboard-coding-system 'utf-8-unix)
+;;              (set-next-selection-coding-system 'utf-8-unix)
+;;              (set-selection-coding-system 'utf-8-unix)
+;;              (set-terminal-coding-system 'utf-8-unix)
+
+;;              (when (eq system-type 'windows-nt)
+;;                (set-selection-coding-system 'gbk-dos)
+;;                (set-next-selection-coding-system 'gbk-dos)
+;;                (set-clipboard-coding-system 'gbk-dos)))
 
 (provide 'init-locales)
