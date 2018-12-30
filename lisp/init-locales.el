0269052709cf026733fbc4a1b25e4b0513e0f5a5
josephyoung <2t@live.com>
2018-12-30 09:41:10 +0800

    Update init-locales.el
    delete all the non-unicode characters, and replace the chinese-gb18030 with utf-8. windows-nt, utf-16-le

--- lisp/init-locales.el
+++ lisp/init-locales.el
@@ -10,30 +10,12 @@
       (sanityinc/utf8-locale-p (getenv "LANG"))))
 
 (when (or window-system (sanityinc/locale-is-utf8-p))
-  (set-language-environment 'chinese-gb18030)
-  (setq locale-coding-system 'chinese-gb18030)
-  (set-default-coding-systems 'chinese-gb18030)
-  (set-terminal-coding-system 'chinese-gb18030)
-  (set-selection-coding-system (if (eq system-type 'windows-nt) 'chinese-gb18030 'utf-8))
-  (prefer-coding-system 'chinese-gb18030))
+  (set-language-environment 'utf-8)
+  (setq locale-coding-system 'utf-8)
+  (set-default-coding-systems 'utf-8)
+  (set-terminal-coding-system 'utf-8)
+  (set-selection-coding-system (if (eq system-type 'windows-nt) 'utf-16-le 'utf-8))
+  (prefer-coding-system 'utf-8))
 
-;; Charset 设置
-;; (use-package mule
-;;              :ensure nil
-;;              :config
-
-;;              (set-language-environment "UTF-8")
-;;              (set-buffer-file-coding-system 'utf-8-unix)
-;;              (set-clipboard-coding-system 'utf-8-unix)
-;;              (set-file-name-coding-system 'utf-8-unix)
-;;              (set-keyboard-coding-system 'utf-8-unix)
-;;              (set-next-selection-coding-system 'utf-8-unix)
-;;              (set-selection-coding-system 'utf-8-unix)
-;;              (set-terminal-coding-system 'utf-8-unix)
-
-;;              (when (eq system-type 'windows-nt)
-;;                (set-selection-coding-system 'gbk-dos)
-;;                (set-next-selection-coding-system 'gbk-dos)
-;;                (set-clipboard-coding-system 'gbk-dos)))
-
+;;; chinese-gb18030
 (provide 'init-locales)
