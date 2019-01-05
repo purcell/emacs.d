b0bcb82d9c51b4d2a86db61ccb46f6a12e5a410e
Steve Purcell <steve@sanityinc.com>
2019-01-05 16:49:39 +1300

    Add missing file header/footer

--- lisp/init-ibuffer.el
+++ lisp/init-ibuffer.el
@@ -1,5 +1,10 @@
+;;; init-ibuffer.el --- ibuffer settings -*- lexical-binding: t -*-
+;;; Commentary:
+
 ;; TODO: enhance ibuffer-fontification-alist
 ;;   See http://www.reddit.com/r/emacs/comments/21fjpn/fontifying_buffer_list_for_emacs_243/
+
+;;; Code:
 
 (require-package 'fullframe)
 (after-load 'ibuffer
@@ -50,3 +55,4 @@
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 
 (provide 'init-ibuffer)
+;;; init-ibuffer.el ends here
