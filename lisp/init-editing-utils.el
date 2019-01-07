418dce32d32389fba3f0c24114b1acc48aaf79b6
Steve Purcell <steve@sanityinc.com>
2019-01-08 09:33:21 +1300

    Show line numbers in prog-modes, but only if native line numbering is available

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -87,8 +87,8 @@
 
 
 
-(unless (fboundp 'display-line-numbers-mode)
-  (require-package 'nlinum))
+(when (fboundp 'display-line-numbers-mode)
+  (add-hook 'prog-mode-hook 'display-line-numbers-mode))
 
 
 (when (require-package 'rainbow-delimiters)
