5fba1e6574071877b3f57dcb58c49a2c9b374209
Steve Purcell <steve@sanityinc.com>
2019-02-06 14:04:27 +1300

    Use exec-path-from-shell when running as a daemon (except on Windows)
    Closes #693

--- lisp/init-exec-path.el
+++ lisp/init-exec-path.el
@@ -9,7 +9,9 @@
     (add-to-list 'exec-path-from-shell-variables var)))
 
 
-(when (memq window-system '(mac ns x))
+(when (or (memq window-system '(mac ns x))
+          (unless (memq system-type '(ms-dos windows-nt))
+            (daemonp)))
   (setq-default exec-path-from-shell-arguments nil)
   (exec-path-from-shell-initialize))
 
