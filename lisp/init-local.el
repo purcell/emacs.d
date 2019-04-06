83a0f4eb03923a8f95532200a3868d69751df070
Jared <yanshuai@gmail.com>
2019-04-05 20:51:22 -0400

    add env path for LaTex(org-mode)

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,3 +1,10 @@
 (add-to-list 'exec-path "/usr/local/bin")
 
+(getenv "PATH")
+(setenv "PATH"
+        (concat
+         "/Library/TeX/texbin/" ":"
+
+         (getenv "PATH")))
+
 (provide 'init-local)
