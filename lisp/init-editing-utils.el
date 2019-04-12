255aabb4e49539bda554481ce8b9f9f2b0c47de5
Steve Purcell <steve@sanityinc.com>
2019-04-12 13:54:26 +1200

    Use setq-default to set line number width, which is necessary in Emacs 27

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -88,7 +88,7 @@
 
 
 (when (fboundp 'display-line-numbers-mode)
-  (setq display-line-numbers-width 3)
+  (setq-default display-line-numbers-width 3)
   (add-hook 'prog-mode-hook 'display-line-numbers-mode))
 
 (when (maybe-require-package 'goto-line-preview)
