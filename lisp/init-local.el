8ad6c1ddb659b6f8d7731fec3e6d51b199ba0fcb
hejin <hejinpy@163.com>
2019-06-16 22:37:39 +0800

    add custom init-local.el

---
+++ lisp/init-local.el
@@ -0,0 +1,15 @@
+;;; init-local.el --- Local Settings -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+(set-default-font "monaco-16")
+
+(require-package 'dracula-theme)
+(load-theme 'dracula t)
+
+(require-package 'web-mode)
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+
+(provide 'init-local)
+;;; init-local.el ends here
