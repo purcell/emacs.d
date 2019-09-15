cdc910b5b8e1f4d11669d36715528d0df944331c
Steve Purcell <steve@sanityinc.com>
2019-09-15 13:27:10 +1200

    Log important gnutls during startup test

--- test-startup.sh
+++ test-startup.sh
@@ -8,6 +8,7 @@ echo "Attempting startup..."
 ${EMACS:=emacs} -nw --batch \
                 --eval '(let ((debug-on-error t)
                               (url-show-status nil)
+                              (gnutls-log-level 1)
                               (user-emacs-directory default-directory)
                               (user-init-file (expand-file-name "init.el"))
                               (load-path (delq default-directory load-path)))
