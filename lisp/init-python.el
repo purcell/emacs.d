09e2a66b8b59f03f0ab5fce5339ea2b0b89d60c5
Steve Purcell <steve@sanityinc.com>
2019-09-30 21:27:59 +1300

    Add note about python and virtualenvs

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -2,6 +2,12 @@
 ;;; Commentary:
 ;;; Code:
 
+
+;; See the following note about how I set up python + virtualenv to
+;; work seamlessly with Emacs:
+;; https://gist.github.com/purcell/81f76c50a42eee710dcfc9a14bfc7240
+
+
 (setq auto-mode-alist
       (append '(("SConstruct\\'" . python-mode)
                 ("SConscript\\'" . python-mode))
