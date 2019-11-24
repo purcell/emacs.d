fd97c3ee87f78a101fd20146d1a34018e5d81999
Henrik Karlsson <i97henka@gmail.com>
2019-11-24 13:24:36 +0100

    init local with windmove and copy filename.

---
+++ init-local.el
@@ -0,0 +1,25 @@
+;;; init local stuff in here - taken from hk emacs setup
+
+
+(defun hk-put-file-name-on-clipboard ()
+  "Put the current file name on the clipboard"
+  (interactive)
+  (let ((filename (if (equal major-mode 'dired-mode)
+                      default-directory
+                    (buffer-file-name))))
+    (when filename
+      (with-temp-buffer
+        (insert filename)
+        (clipboard-kill-region (point-min) (point-max)))
+      (message filename))))
+
+
+(windmove-default-keybindings)
+
+(add-hook 'org-shiftup-final-hook 'windmove-up)
+(add-hook 'org-shiftleft-final-hook 'windmove-left)
+(add-hook 'org-shiftdown-final-hook 'windmove-down)
+(add-hook 'org-shiftright-final-hook 'windmove-right)
+
+
+(provide 'init-local)
