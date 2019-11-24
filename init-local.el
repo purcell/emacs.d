a62b37f17c725dfaf01c591ee5db3f7da3b8c6da
Henrik Karlsson <i97henka@gmail.com>
2019-11-24 15:18:08 +0100

    removing copy clipboard function.

--- init-local.el
+++ init-local.el
@@ -1,20 +1,20 @@
 ;;; init local stuff in here - taken from hk emacs setup
 
 
-(defun hk-put-file-name-on-clipboard ()
-  "Put the current file name on the clipboard"
-  (interactive)
-  (let ((filename (if (equal major-mode 'dired-mode)
-                      default-directory
-                    (buffer-file-name))))
-    (when filename
-      (with-temp-buffer
-        (insert filename)
-        (clipboard-kill-region (point-min) (point-max)))
-      (message filename))))
+;; (defun hk-put-file-name-on-clipboard ()
+;;   "Put the current file name on the clipboard"
+;;   (interactive)
+;;   (let ((filename (if (equal major-mode 'dired-mode)
+;;                       default-directory
+;;                     (buffer-file-name))))
+;;     (when filename
+;;       (with-temp-buffer
+;;         (insert filename)
+;;         (clipboard-kill-region (point-min) (point-max)))
+;;       (message filename))))
 
 
-(windmove-default-keybindings)
+(windmove-default-keybindings )
 
 (add-hook 'org-shiftup-final-hook 'windmove-up)
 (add-hook 'org-shiftleft-final-hook 'windmove-left)
