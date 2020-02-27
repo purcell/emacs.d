adf083e622316570b3ad03621cdfac533a02a026
Tootooroo <aydenlin@foxmail.com>
2020-02-27 18:52:47 +0800

    set solarized-italic to nil

--- lisp/init-users.el
+++ lisp/init-users.el
@@ -121,8 +121,17 @@
           (lambda ()
             (define-key evil-normal-state-local-map (kbd "C-\\") 'elpy-goto-definition)))
 
-
+;; Solarized
 (add-to-list 'custom-theme-load-path "~/.emacs.d/emacs-color-theme-solarized")
+
+(setq solarized-italic nil)
 (load-theme 'solarized t)
+
+(add-hook 'after-make-frame-functions
+          (lambda (frame)
+            (let ((mode (if (display-graphic-p frame) 'light 'dark)))
+              (set-frame-parameter frame 'background-mode mode)
+              (set-terminal-parameter frame 'background-mode mode))
+            (enable-theme 'solarized)))
 
 (provide 'init-users)
