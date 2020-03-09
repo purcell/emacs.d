71db52615191b88b997a795119ae9a3318a4f243
Steve Purcell <steve@sanityinc.com>
2020-03-09 15:53:15 +1300

    Switch CI to Github Actions

--- test-startup.sh
+++ test-startup.sh
@@ -1,9 +1,4 @@
 #!/bin/sh -e
-if [ -n "$TRAVIS" ]; then
-    # Make it look like this is ~/.emacs.d (needed for Emacs 24.3, at least)
-    export HOME=$PWD/..
-    ln -s emacs.d ../.emacs.d
-fi
 echo "Attempting startup..."
 ${EMACS:=emacs} -nw --batch \
                 --eval '(let ((debug-on-error t)
