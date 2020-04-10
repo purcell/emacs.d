dfc41a9bca0355327d0a0063ebb44a54ec046d12
Tootooroo <aydenlin@foxmail.com>
2020-04-10 19:33:44 +0800

    Add TypeScript Tide

--- lisp/init-users.el
+++ lisp/init-users.el
@@ -168,5 +168,28 @@
 (require-package 'evil-magit)
 (require 'evil-magit)
 
+
+;; TypeScript
+(require-package 'tide)
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  ;; company is an optional dependency. You have to
+  ;; install it separately via package-install
+  ;; `M-x package-install [ret] company`
+  (company-mode +1))
+
+;; aligns annotation to the right hand side
+(setq company-tooltip-align-annotations t)
+
+;; formats the buffer before saving
+(add-hook 'before-save-hook 'tide-format-before-save)
+
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
+
 (provide 'init-users)
 ;;; init-users.el ends here
