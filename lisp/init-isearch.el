5aed7ed605ee05439f23b39453c6c3f778ce69ff
Steve Purcell <steve@sanityinc.com>
2020-04-17 14:00:49 +1200

    Fix comment location

--- lisp/init-isearch.el
+++ lisp/init-isearch.el
@@ -9,11 +9,11 @@
   (global-set-key [remap query-replace-regexp] 'anzu-query-replace-regexp)
   (global-set-key [remap query-replace] 'anzu-query-replace))
 
-;; Activate occur easily inside isearch
 (after-load 'isearch
   ;; DEL during isearch should edit the search string, not jump back to the previous result
   (define-key isearch-mode-map [remap isearch-delete-char] 'isearch-del-char)
 
+  ;; Activate occur easily inside isearch
   (when (fboundp 'isearch-occur)
     ;; to match ivy conventions
     (define-key isearch-mode-map (kbd "C-c C-o") 'isearch-occur)))
