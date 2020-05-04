3a470276abdf9d0224032d41e2ecb04a0736d3a5
Steve Purcell <steve@sanityinc.com>
2020-05-04 12:10:00 +1200

    Pick up NIX_SSL_CERT_FILE via exec-path-from-shell

--- lisp/init-exec-path.el
+++ lisp/init-exec-path.el
@@ -5,7 +5,7 @@
 (require-package 'exec-path-from-shell)
 
 (after-load 'exec-path-from-shell
-  (dolist (var '("SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO" "LANG" "LC_CTYPE"))
+  (dolist (var '("SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO" "LANG" "LC_CTYPE" "NIX_SSL_CERT_FILE"))
     (add-to-list 'exec-path-from-shell-variables var)))
 
 
