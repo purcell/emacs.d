3cdb31e612bec1c52c618d01889f9a4e44b8c30d
Steve Purcell <steve@sanityinc.com>
2020-05-06 10:47:29 +1200

    Exclude elpa package autoloads in recentf

--- lisp/init-recentf.el
+++ lisp/init-recentf.el
@@ -5,7 +5,7 @@
 (add-hook 'after-init-hook 'recentf-mode)
 (setq-default
  recentf-max-saved-items 1000
- recentf-exclude '("/tmp/" "/ssh:"))
+ recentf-exclude `("/tmp/" "/ssh:" ,(concat package-user-dir "/.*-autoloads\\.el\\'")))
 
 
 (provide 'init-recentf)
