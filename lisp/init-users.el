8f4405d40fd1a2030a9c23d723d9a50c18a402e2
Tootooroo <aydenlin@foxmail.com>
2020-06-23 17:03:16 +0800

    lsp update.

--- lisp/init-users.el
+++ lisp/init-users.el
@@ -189,8 +189,36 @@
 (require-package 'lsp-mode)
 (require-package 'lsp-ui)
 (require-package 'lsp-treemacs)
+(require-package 'dap-mode)
+(require-package 'which-key)
+
+;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+(setq lsp-keymap-prefix "s-l")
+
+(use-package lsp-mode
+  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
+         (XXX-mode . lsp)
+         ;; if you want which-key integration
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp)
+
+;; optionally
 (use-package lsp-ui :commands lsp-ui-mode)
+;; if you are helm user
+(use-package helm-lsp :commands helm-lsp-workspace-symbol)
+;; if you are ivy user
+(use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
 (use-package lsp-treemacs :commands lsp-treemacs-errors-list)
+
+;; optionally if you want to use debugger
+(use-package dap-mode)
+;; (use-package dap-LANGUAGE) to load the dap adapter for your language
+
+;; optional if you want which-key integration
+(use-package which-key
+  :config
+  (which-key-mode))
+
 
 
 ;; Python LSP
