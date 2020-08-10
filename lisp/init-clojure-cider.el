134e3c32500b77267f39689cfcede2519aeabd7b
Lucas Mafra <lucas.mafra95@gmail.com>
2020-08-10 09:52:18 -0300

    add clojure refactor

--- lisp/init-clojure-cider.el
+++ lisp/init-clojure-cider.el
@@ -3,6 +3,13 @@
 ;;; Code:
 
 (require 'init-clojure)
+(require-package 'clj-refactor)
+
+(defun clojure-refactor-setup ()
+  (clj-refactor-mode 1)
+  (yas-minor-mode 1) ;; for adding require/use/import statements
+  ;; This choice of keybinding leaves cider-macroexpand-1 unbound
+  (cljr-add-keybindings-with-prefix "C-c C-m"))
 
 (when (maybe-require-package 'cider)
   (setq nrepl-popup-stacktraces nil)
@@ -17,6 +24,8 @@
       (with-eval-after-load 'flycheck
         (flycheck-clojure-setup)))))
 
+
+(add-hook 'clojure-mode-hook #'clojure-refactor-setup)
 
 (provide 'init-clojure-cider)
 ;;; init-clojure-cider.el ends here
