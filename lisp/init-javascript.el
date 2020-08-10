4dba611e3b4a5149e989c751d82e02d2329522e0
Steve Purcell <steve@sanityinc.com>
2020-08-10 12:07:46 +1200

    Tidy up js settings

--- lisp/init-javascript.el
+++ lisp/init-javascript.el
@@ -7,11 +7,19 @@
 (maybe-require-package 'typescript-mode)
 (maybe-require-package 'prettier-js)
 
-;; Need to first remove from list if present, since elpa adds entries too, which
-;; may be in an arbitrary order
+
+;;; Basic js-mode setup
 
 (add-to-list 'auto-mode-alist '("\\.\\(js\\|es6\\)\\(\\.erb\\)?\\'" . js-mode))
 
+(with-eval-after-load 'js
+  (sanityinc/major-mode-lighter 'js-mode "JS")
+  (sanityinc/major-mode-lighter 'js-jsx-mode "JSX"))
+
+(setq-default js-indent-level 2)
+
+
+
 ;; js2-mode
 
 ;; Change some defaults: customize them to override
@@ -33,19 +41,14 @@
 
   (js2-imenu-extras-setup))
 
-(setq-default js-indent-level 2)
 ;; In Emacs >= 25, the following is an alias for js-indent-level anyway
 (setq-default js2-basic-offset 2)
-
 
 (add-to-list 'interpreter-mode-alist (cons "node" 'js2-mode))
 
 (with-eval-after-load 'js2-mode
   (sanityinc/major-mode-lighter 'js2-mode "JS2")
   (sanityinc/major-mode-lighter 'js2-jsx-mode "JSX2"))
-(with-eval-after-load 'js
-  (sanityinc/major-mode-lighter 'js-mode "JS")
-  (sanityinc/major-mode-lighter 'js-jsx-mode "JSX"))
 
 
 
