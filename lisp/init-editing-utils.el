1d211322c78b22d9a4540a5b7a0ebb97b6746cfb
Steve Purcell <steve@sanityinc.com>
2020-10-19 10:21:09 +1300

    Remove some multiple-cursors bindings I don't use

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -174,11 +174,6 @@
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-+") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
-;; From active region to multiple cursors:
-(global-set-key (kbd "C-c m r") 'set-rectangular-region-anchor)
-(global-set-key (kbd "C-c m c") 'mc/edit-lines)
-(global-set-key (kbd "C-c m e") 'mc/edit-ends-of-lines)
-(global-set-key (kbd "C-c m a") 'mc/edit-beginnings-of-lines)
 
 ;; Train myself to use M-f and M-b instead
 (global-unset-key [M-left])
