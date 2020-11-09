5922ee24e4e4129a511310d73f94f34e4f530d46
zl <chuisiniu@hotmail.com>
2020-11-09 15:01:38 +0800

    配置

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -15,7 +15,7 @@
 
 ;;; Standard package repositories
 
-(add-to-list 'package-archives '( "melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '( "melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/") t)
 ;; Official MELPA Mirror, in case necessary.
 ;;(add-to-list 'package-archives (cons "melpa-mirror" (concat proto "://www.mirrorservice.org/sites/melpa.org/packages/")) t)
 
