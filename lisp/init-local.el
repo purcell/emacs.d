c7070a6ff11418edff30b829f6d7481f09420848
chuisiniu <chuisiniu@hotmail.com>
2020-11-11 16:47:24 +0800

    导出markdown

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -10,5 +10,6 @@
 (setq org-latex-pdf-process '("xelatex -interaction nonstopmode %f"
                               "xelatex -interaction nonstopmode %f"))
 (color-theme-sanityinc-tomorrow-bright)
+(setq org-export-backends (quote (ascii html icalendar latex md)))
 (provide 'init-local)
 ;;; init-local.el ends here
