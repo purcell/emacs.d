0d7b32c9ec4be7f833ff7d75df5e1c0362f4866b
Tootooroo <aydenlin@foxmail.com>
2020-12-21 12:39:33 +0800

    pdflatex call command add.

--- lisp/my/init-users.el
+++ lisp/my/init-users.el
@@ -271,6 +271,17 @@
 ;; Org preview
 (require-package 'org-preview-html)
 
+(setq org-latex-pdf-process
+      (let
+          ((cmd (concat "pdflatex -shell-escape -interaction nonstopmode"
+                        " --synctex=1"
+                        " -output-directory %o %f")))
+        (list cmd
+              "cd %o; if test -r %b.idx; then makeindex %b.idx; fi"
+              "cd %o; bibtex %b"
+              cmd
+              cmd)))
+
 (use-package org-roam
   :ensure t
   :hook
