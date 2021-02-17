18816a9ffbc4b607d9b2409fa6775996573b3c76
Steve Purcell <steve@sanityinc.com>
2021-02-17 15:51:47 +1300

    Allow require-package to upgrade a package if min-version given
    See #771

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -34,13 +34,16 @@ If NO-REFRESH is non-nil, the available package lists will not be
 re-downloaded in order to locate PACKAGE."
   (or (package-installed-p package min-version)
       (let* ((known (cdr (assoc package package-archive-contents)))
-             (versions (mapcar #'package-desc-version known)))
-        (if (cl-some (lambda (v) (version-list-<= min-version v)) versions)
-            (package-install package)
+             (best (car (sort known (lambda (a b)
+                                      (version-list-<= (package-desc-version b)
+                                                       (package-desc-version a)))))))
+        (if (and best (version-list-<= min-version (package-desc-version best)))
+            (package-install best)
           (if no-refresh
               (error "No version of %s >= %S is available" package min-version)
             (package-refresh-contents)
-            (require-package package min-version t))))))
+            (require-package package min-version t)))
+        (package-installed-p package min-version))))
 
 (defun maybe-require-package (package &optional min-version no-refresh)
   "Try to install PACKAGE, and return non-nil if successful.
