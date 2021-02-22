e78c2b9bbea4913a8b52a4c923c0ff3419ca58c4
Steve Purcell <steve@sanityinc.com>
2021-02-22 14:20:33 +1300

    Workaround for company menu with page-break-lines is now redundant

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -28,24 +28,6 @@
   (when (maybe-require-package 'company-quickhelp)
     (add-hook 'after-init-hook 'company-quickhelp-mode)))
 
-;; Suspend page-break-lines-mode while company menu is active
-;; (see https://github.com/company-mode/company-mode/issues/416)
-(with-eval-after-load 'company
-  (with-eval-after-load 'page-break-lines
-    (defvar-local sanityinc/page-break-lines-on-p nil)
-
-    (defun sanityinc/page-break-lines-disable (&rest ignore)
-      (when (setq sanityinc/page-break-lines-on-p (bound-and-true-p page-break-lines-mode))
-        (page-break-lines-mode -1)))
-
-    (defun sanityinc/page-break-lines-maybe-reenable (&rest ignore)
-      (when sanityinc/page-break-lines-on-p
-        (page-break-lines-mode 1)))
-
-    (add-hook 'company-completion-started-hook 'sanityinc/page-break-lines-disable)
-    (add-hook 'company-after-completion-hook 'sanityinc/page-break-lines-maybe-reenable)))
-
-
 
 (provide 'init-company)
 ;;; init-company.el ends here
