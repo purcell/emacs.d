5f324a707647b98aa2cca993189b8a20a00995b9
Steve Purcell <steve@sanityinc.com>
2021-03-13 12:34:11 +1300

    Pre-declare url-show-status var in startup script
    Aiming to prevent this in Emacs 28:
    
    Debugger entered--Lisp error: (error "Defining as dynamic an already lexical var")
      internal--define-uninitialized-variable(url-show-status "Whether to show a running total of bytes transferr...")

--- test-startup.sh
+++ test-startup.sh
@@ -1,11 +1,13 @@
 #!/bin/sh -e
 echo "Attempting startup..."
 ${EMACS:=emacs} -nw --batch \
-                --eval '(let ((debug-on-error t)
+                --eval '(progn
+                        (defvar url-show-status)
+                        (let ((debug-on-error t)
                               (url-show-status nil)
                               (user-emacs-directory default-directory)
                               (user-init-file (expand-file-name "init.el"))
                               (load-path (delq default-directory load-path)))
                            (load-file user-init-file)
-                           (run-hooks (quote after-init-hook)))'
+                           (run-hooks (quote after-init-hook))))'
 echo "Startup successful"
