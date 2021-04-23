bad8ac68dd07d841eb1f570c03a5409013f4b69a
Steve Purcell <steve@sanityinc.com>
2021-04-24 10:47:54 +1200

    Don't automatically preview consult ripgrep commands

--- lisp/init-selectrum.el
+++ lisp/init-selectrum.el
@@ -31,6 +31,12 @@
     (global-set-key [remap switch-to-buffer-other-window] 'consult-buffer-other-window)
     (global-set-key [remap switch-to-buffer-other-frame] 'consult-buffer-other-frame)
     (global-set-key [remap goto-line] 'consult-goto-line)
+
+    (with-eval-after-load 'consult
+      (dolist (cmd '(consult-ripgrep sanityinc/consult-ripgrep-at-point))
+        (add-to-list 'consult-config
+                     `(,cmd :preview-key ,(kbd "M-P")))))
+
     (when (maybe-require-package 'embark-consult)
       (with-eval-after-load 'embark
         (require 'embark-consult)
