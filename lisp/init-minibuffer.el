72715575f08cbee7651b4e318321854a4385f7e9
Steve Purcell <steve@sanityinc.com>
2021-06-27 21:37:25 +1200

    Work around consult-customize insisting customized commands are loaded
    See https://github.com/minad/consult/issues/345

--- lisp/init-minibuffer.el
+++ lisp/init-minibuffer.el
@@ -20,30 +20,34 @@
       (define-key vertico-map (kbd "C-c C-c") 'embark-act)))
 
   (when (maybe-require-package 'consult)
+    (defmacro sanityinc/no-consult-preview (&rest cmds)
+      `(with-eval-after-load 'consult
+         (consult-customize ,@cmds :preview-key (kbd "M-P"))))
+
+    (sanityinc/no-consult-preview
+     consult-ripgrep
+     consult-git-grep consult-grep
+     consult-bookmark consult-recent-file consult-xref
+     consult--source-file consult--source-project-file consult--source-bookmark)
+
     (when (maybe-require-package 'projectile)
       (setq-default consult-project-root-function 'projectile-project-root))
 
-    (when (executable-find "rg")
-      (maybe-require-package 'affe)
+    (when (and (executable-find "rg") (maybe-require-package 'affe))
       (defun sanityinc/affe-grep-at-point (&optional dir initial)
         (interactive (list prefix-arg (when-let ((s (symbol-at-point)))
                                         (symbol-name s))))
         (affe-grep dir initial))
-      (global-set-key (kbd "M-?") 'sanityinc/affe-grep-at-point))
+      (global-set-key (kbd "M-?") 'sanityinc/affe-grep-at-point)
+      (sanityinc/no-consult-preview sanityinc/affe-grep-at-point)
+      (with-eval-after-load 'affe (sanityinc/no-consult-preview affe-grep)))
 
     (global-set-key [remap switch-to-buffer] 'consult-buffer)
     (global-set-key [remap switch-to-buffer-other-window] 'consult-buffer-other-window)
     (global-set-key [remap switch-to-buffer-other-frame] 'consult-buffer-other-frame)
     (global-set-key [remap goto-line] 'consult-goto-line)
 
-    (with-eval-after-load 'consult
-      (consult-customize
-       consult-ripgrep
-       affe-grep sanityinc/affe-grep-at-point
-       consult-git-grep consult-grep
-       consult-bookmark consult-recent-file consult-xref
-       consult--source-file consult--source-project-file consult--source-bookmark
-       :preview-key (kbd "M-P")))
+
 
     (when (maybe-require-package 'embark-consult)
       (with-eval-after-load 'embark
