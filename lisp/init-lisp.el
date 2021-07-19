03bcd2789976a3ea021653cdc9f58583de02b71f
Steve Purcell <steve@sanityinc.com>
2021-07-19 15:38:29 +1200

    Drop obsolete cl-lib-highlight package
    Fixes #784

--- lisp/init-lisp.el
+++ lisp/init-lisp.el
@@ -206,9 +206,6 @@ there is no current file, eval the current buffer."
 (add-to-list 'auto-mode-alist '("\\.emacs-project\\'" . emacs-lisp-mode))
 (add-to-list 'auto-mode-alist '("archive-contents\\'" . emacs-lisp-mode))
 
-(require-package 'cl-lib-highlight)
-(with-eval-after-load 'lisp-mode
-  (cl-lib-highlight-initialize))
 
 
 ;; Delete .elc files when reverting the .el from VC or magit
