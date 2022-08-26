b0cc9ea7dd7c3ab7d9a77bc89cbb0d0d106c4528
Steve Purcell <steve@sanityinc.com>
2022-08-26 17:22:50 +0200

    Disable corfu-auto in eshell-mode
    Fixes #818

--- lisp/init-corfu.el
+++ lisp/init-corfu.el
@@ -16,6 +16,8 @@
 
 (when (maybe-require-package 'corfu)
   (setq-default corfu-auto t)
+  (with-eval-after-load 'eshell
+    (add-hook 'eshell-mode-hook (lambda () (setq-local corfu-auto nil))))
   (setq-default corfu-quit-no-match 'separator)
   (add-hook 'after-init-hook 'global-corfu-mode)
 
