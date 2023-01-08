27223c90636c15ff34fb9d1eb216f5f79ceb3ee1
Vincent Boucheny <mankalas@gmail.com>
2023-01-09 11:20:52 +1300

    Set Zenburn theme by default

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -10,7 +10,7 @@
 (setq custom-safe-themes t)
 
 ;; If you don't customize it, this is the theme you get.
-(setq-default custom-enabled-themes '(sanityinc-tomorrow-bright))
+(setq-default custom-enabled-themes '(zenburn))
 
 ;; Ensure that themes will be applied even if they have not been customized
 (defun reapply-themes ()
@@ -52,7 +52,6 @@
     (defun sanityinc/display-non-graphic-p ()
       (not (display-graphic-p)))
     (add-to-list 'dimmer-exclusion-predicates 'sanityinc/display-non-graphic-p)))
-
 
 (provide 'init-themes)
 ;;; init-themes.el ends here
