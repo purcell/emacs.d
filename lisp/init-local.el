d57b8b59d189fb6d12d416953203be78b518e0ee
erimendz_ubuntu2304 <erimendz@gmail.com>
2023-07-18 16:37:28 +0300

    add first customization file

---
+++ lisp/init-local.el
@@ -0,0 +1,14 @@
+;;; init-local.el --- configure default locale -*- lexical-binding: t -*-
+;;; Commentary: users personal customization file in addition to custom.el
+;;; See this link https://github.com/eribertto/purcell.emacs.d#installation
+(setq-default truncate-lines t) ; good for tiling window managers
+(size-indication-mode 1) ; display file size
+(display-time)
+(display-battery-mode)
+
+
+
+
+
+(provide 'init-local)
+;;; init-locales.el ends here
