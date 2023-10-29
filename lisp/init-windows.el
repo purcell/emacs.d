699a47da9566a7bf145ab13a11872ea9285f3416
Steve Purcell <steve@sanityinc.com>
2023-10-29 15:33:16 +0000

    Prefer qwerty style for switch-window shortcuts

--- lisp/init-windows.el
+++ lisp/init-windows.el
@@ -14,7 +14,7 @@
 
 ;; Make "C-x o" prompt for a target window when there are more than 2
 (require-package 'switch-window)
-(setq-default switch-window-shortcut-style 'alphabet)
+(setq-default switch-window-shortcut-style 'qwerty)
 (setq-default switch-window-timeout nil)
 (global-set-key (kbd "C-x o") 'switch-window)
 
