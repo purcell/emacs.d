9cb7f5c5b8c0cc8744e00c221f6a3bc2bb15191b
Steve Purcell <steve@sanityinc.com>
2023-12-23 12:42:07 +0000

    Add config for puni

--- lisp/init-paredit.el
+++ lisp/init-paredit.el
@@ -41,5 +41,19 @@
 
 (add-hook 'sanityinc/lispy-modes-hook 'enable-paredit-mode)
 
+(when (maybe-require-package 'puni)
+  ;;(add-hook 'prog-mode-hook 'puni-mode)
+  (add-hook 'sanityinc/lispy-modes-hook (lambda () (puni-mode -1)))
+  (with-eval-after-load 'puni
+    (define-key puni-mode-map (kbd "M-(") 'puni-wrap-round)
+    (define-key puni-mode-map (kbd "C-(") 'puni-slurp-backward)
+    (define-key puni-mode-map (kbd "C-)") 'puni-slurp-forward)
+    (define-key puni-mode-map (kbd "C-}") 'puni-barf-forward)
+    (define-key puni-mode-map (kbd "C-{") 'puni-barf-backward)
+    (define-key puni-mode-map (kbd "M-<up>") 'puni-splice-killing-backward)
+    (define-key puni-mode-map (kbd "C-w") nil)))
+
+
+
 (provide 'init-paredit)
 ;;; init-paredit.el ends here
