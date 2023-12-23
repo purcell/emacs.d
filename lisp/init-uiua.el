ac39b979d42effed4ed8b11fa695ec38ccc90e87
Steve Purcell <steve@sanityinc.com>
2023-12-23 14:45:10 +0000

    Simplify uiua config

--- lisp/init-uiua.el
+++ lisp/init-uiua.el
@@ -2,10 +2,8 @@
 ;;; Commentary:
 ;;; Code:
 
-(if (maybe-require-package 'uiua-ts-mode)
-    (when (and (fboundp 'treesit-ready-p) (treesit-ready-p 'uiua t))
-      (add-to-list 'major-mode-remap-alist '(uiua-mode . uiua-ts-mode)))
-  (maybe-require-package 'uiua-mode))
+(or (maybe-require-package 'uiua-ts-mode)
+    (maybe-require-package 'uiua-mode))
 
 (with-eval-after-load 'eglot
   (add-to-list 'eglot-server-programs '((uiua-mode uiua-ts-mode) . ("uiua" "lsp"))))
