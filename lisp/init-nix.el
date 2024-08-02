6972ba91b0916569202cb1b9255702c5ac9c654d
Steve Purcell <steve@sanityinc.com>
2024-08-02 10:35:58 +0100

    Prefer nixd to nil for LSP with Nix code

--- lisp/init-nix.el
+++ lisp/init-nix.el
@@ -8,7 +8,9 @@
   (maybe-require-package 'nix-mode))
 
 (with-eval-after-load 'eglot
-  (add-to-list 'eglot-server-programs '((nix-mode nix-ts-mode) . ("nil"))))
+  ;; Prefer nixd to nil, and enable in nix-ts-mode too
+  (add-to-list 'eglot-server-programs
+               `((nix-mode nix-ts-mode) . ,(eglot-alternatives '("nixd" "nil")))))
 
 (maybe-require-package 'nixpkgs-fmt)
 (maybe-require-package 'nixfmt)
