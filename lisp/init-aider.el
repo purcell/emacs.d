40966bd6a6165b0ed248fb4b7366ea0f55b79a78
tinder-javiertrejo <javier.trejo@gotinder.com>
2025-03-29 13:29:54 -0600

    feat: Scaffold support for emacs aider

---
+++ lisp/init-aider.el
@@ -0,0 +1,17 @@
+;;; init-aider.el --- EmacsAider -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+;; (use-package aidermacs
+;;   :bind (("C-c a" . aidermacs-transient-menu))
+;;   :config
+;;                                         ;Set API_KEY in .bashrc, that will automatically picked up by aider or in elisp
+;;   (setenv "ANTHROPIC_API_KEY" "sk-...")
+;;                                         ; defun my-get-openrouter-api-key yourself elsewhere for security reasons
+;;   (setenv "OPENROUTER_API_KEY" (my-get-openrouter-api-key))
+;;   :custom
+;;                                         ; See the Configuration section below
+;;   (aidermacs-use-architect-mode t)
+;;   (aidermacs-default-model "sonnet"))
+
+;;; init-aider.el ends here
