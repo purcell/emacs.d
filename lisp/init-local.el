dcd9c376f16470136f046cdb4e1bfddc23bed9cc
marcuslannister <marcus@lannister.cc>
2025-10-29 17:24:21 +0800

    feat: add auto save

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -41,6 +41,22 @@
   :config
   (undo-fu-session-global-mode))
 
+;; auto-save
+(add-to-list 'load-path "~/github/auto-save/") ; add auto-save to your load-path
+(require 'auto-save)
+(auto-save-enable)
+
+(setq auto-save-silent t)   ; quietly save
+(setq auto-save-delete-trailing-whitespace t)  ; automatically delete spaces at the end of the line when saving
+
+;;; custom predicates if you don't want auto save.
+;;; disable auto save mode when current filetype is an gpg file.
+(setq auto-save-disable-predicates
+      '((lambda ()
+          (string-suffix-p
+           "gpg"
+           (file-name-extension (buffer-name)) t))))
+
 ;; Load OS-specific configurations
 (cond
  ((eq system-type 'windows-nt)
