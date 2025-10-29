6bd3e245fe9533524ed6e858058b0413d6a54655
marcuslannister <marcus@lannister.cc>
2025-10-29 16:54:47 +0800

    feat: add keybinding config

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -44,5 +44,8 @@
  ((eq system-type 'gnu/linux)
   (load (expand-file-name "lisp/init-local-linux.el" user-emacs-directory))))
 
+;; Load keybinding
+(load (expand-file-name "lisp/init-local-keybinding.el" user-emacs-directory))
+
 (provide 'init-local)
 
