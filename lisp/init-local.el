688ded31589208c9868fc65e68b08e6923c64f36
marcuslannister <marcus@lannister.cc>
2025-10-30 15:25:53 +0800

    feat: add yasnippet pacakge

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -110,6 +110,14 @@
 
 (require 'init-local-denote)
 
+(use-package yasnippet
+  :ensure t
+  :config
+  (yas-global-mode 1))
+
+(use-package yasnippet-snippets
+  :ensure t)
+
 ;; Load keybinding
 (require 'init-local-keybinding)
 
