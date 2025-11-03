e9e98ce2088d704ea220705965171e3d4bdb9461
marcuslannister <marcus@lannister.cc>
2025-11-03 13:10:08 +0800

    feat: set sis to other when evil insert state

--- lisp/init-local-keybinding.el
+++ lisp/init-local-keybinding.el
@@ -4,22 +4,35 @@
 (use-package sis
   :ensure t
   :config
-  (when (and (eq system-type 'darwin)
-             (display-graphic-p))
-    (let ((macism-bin (or (executable-find "macism")
-                          "/opt/homebrew/bin/macism")))
-      (when (and macism-bin (file-executable-p macism-bin))
-        (setq sis-external-ism macism-bin)))
-    ;; Configure input sources via macism
-    (sis-ism-lazyman-config
-     "com.apple.keylayout.ABC"            ; English
-     "im.rime.inputmethod.Squirrel.Hans"  ; Squirrel
-     'macism)
-    (sis-global-cursor-color-mode 1)
-    (sis-global-respect-mode 1)
-    (sis-global-context-mode 1)
-    ;; (sis-global-inline-mode 1) ; enable if inline English/Chinese is desired
-    ))
+  ;; Configure input sources based on platform
+  (if *is-a-mac*
+      (sis-ism-lazyman-config
+       "com.apple.keylayout.ABC"
+       "im.rime.inputmethod.Squirrel.Hans")
+    ;; Fallback for non-Mac systems
+    (setq sis-english-source "com.apple.keylayout.ABC"))
+
+  ;; ;; General settings (apply to all platforms)
+  ;; (setq ;; Inline mode settings
+  ;;  sis-inline-tighten-head-rule nil
+
+  ;;  ;; Cursor colors for different input sources
+  ;;  sis-default-cursor-color "#cf7fa7"   ; English input
+  ;;  sis-other-cursor-color "orange")     ; Other input (e.g., Chinese)
+
+  ;; External input source manager (commented out - using built-in method)
+  ;; (setq sis-external-ism "macism")
+
+  ;; Enable global modes
+  (sis-global-cursor-color-mode nil)
+  (sis-global-respect-mode t)
+  (sis-global-context-mode t)
+
+  ;; Inline English mode (uncomment if needed for inline English in non-English text)
+  ;; (sis-global-inline-mode t)
+
+  ;; Auto-switch to other input source when entering Evil insert mode
+  (add-hook 'evil-insert-state-entry-hook #'sis-set-other))
 
 ;; cliboard setting for mouse
 (setq select-active-regions nil)
@@ -62,119 +75,119 @@
 (evil-set-leader 'motion (kbd "SPC"))
 
 (evil-define-key nil 'global
-    ;; <leader>
-    (kbd "<leader> :")  '("M-x" . execute-extended-command)
-    (kbd "<leader> .")  '("Fine file" . find-file)
-    (kbd "<leader> ,")  '("Switch buffer" . switch-to-buffer)
-    (kbd "<leader> ;")  '("Insert timestamp" . insert-timestamp)
+  ;; <leader>
+  (kbd "<leader> :")  '("M-x" . execute-extended-command)
+  (kbd "<leader> .")  '("Fine file" . find-file)
+  (kbd "<leader> ,")  '("Switch buffer" . switch-to-buffer)
+  (kbd "<leader> ;")  '("Insert timestamp" . insert-timestamp)
 
-    ;; session
-    (kbd "<leader> qq")  '("Quit Emacs" . save-buffers-kill-terminal)
-    (kbd "<leader> qr")  '("Restart Emacs" . restart-emacs)
+  ;; session
+  (kbd "<leader> qq")  '("Quit Emacs" . save-buffers-kill-terminal)
+  (kbd "<leader> qr")  '("Restart Emacs" . restart-emacs)
 
-    ;; comment
-    (kbd "<leader> /")  '("Commentary" . my-evil-comment-or-uncomment)
+  ;; comment
+  (kbd "<leader> /")  '("Commentary" . my-evil-comment-or-uncomment)
 
-    ;; buffer
-    (kbd "<leader> bn") '("Next buffer" . evil-next-buffer)
-    (kbd "<leader> bp") '("Prev buffer" . evil-prev-buffer)
-    (kbd "<leader> bs") '("Save buffer" . basic-save-buffer)
-    (kbd "<leader> ba") '("Save all buffers" . evil-write-all)
-    (kbd "<leader> bk") '("Kill current buffer" . kill-current-buffer)
-    (kbd "<leader> bo") '("Read only mode" . read-only-mode)
-    (kbd "<leader> bm") '("View message buffer" . view-echo-area-messages)
-    (kbd "<leader> be") '("Eval buffer" . eval-buffer)
-    (kbd "<leader> br") '("Revert buffer" . revert-buffer)
+  ;; buffer
+  (kbd "<leader> bn") '("Next buffer" . evil-next-buffer)
+  (kbd "<leader> bp") '("Prev buffer" . evil-prev-buffer)
+  (kbd "<leader> bs") '("Save buffer" . basic-save-buffer)
+  (kbd "<leader> ba") '("Save all buffers" . evil-write-all)
+  (kbd "<leader> bk") '("Kill current buffer" . kill-current-buffer)
+  (kbd "<leader> bo") '("Read only mode" . read-only-mode)
+  (kbd "<leader> bm") '("View message buffer" . view-echo-area-messages)
+  (kbd "<leader> be") '("Eval buffer" . eval-buffer)
+  (kbd "<leader> br") '("Revert buffer" . revert-buffer)
 
-    ;; window
-    (kbd "<leader> w") '("Window" . evil-window-map)
+  ;; window
+  (kbd "<leader> w") '("Window" . evil-window-map)
 
-    ;; org
-    (kbd "<leader> oa") '("Agenda" . org-agenda)
-    (kbd "<leader> ol") '("Todo list" . org-todo-list)
-    (kbd "<leader> om") '("Tags search" . org-tags-view)
-    (kbd "<leader> ov") '("View search" . org-search-view)
-    (kbd "<leader> ot") '("Todo change" . org-todo)
-    (kbd "<leader> oc") '("Capture" . org-capture)
-    (kbd "<leader> od") '("Insert deadline" . org-deadline)
-    (kbd "<leader> os") '("Insert schedule" . org-schedule)
-    (kbd "<leader> or") '("Refile" . org-refile)
-    (kbd "<leader> oil") '("Inser link" . org-insert-link)
-    (kbd "<leader> op") '("Change priority" . org-priority)
-    (kbd "<leader> og") '("Lookup location" . org-goto)
-    (kbd "<leader> oih") '("Insert heading" . org-insert-heading)
-    (kbd "<leader> ois") '("Insert subheading" . org-insert-subheading)
-    (kbd "<leader> onl") '("Add link to org-now" . org-now-link)
-    (kbd "<leader> ont") '("Toggle org-now side window" . org-now)
-    (kbd "<leader> oo") '("Open at point" . org-open-at-point)
+  ;; org
+  (kbd "<leader> oa") '("Agenda" . org-agenda)
+  (kbd "<leader> ol") '("Todo list" . org-todo-list)
+  (kbd "<leader> om") '("Tags search" . org-tags-view)
+  (kbd "<leader> ov") '("View search" . org-search-view)
+  (kbd "<leader> ot") '("Todo change" . org-todo)
+  (kbd "<leader> oc") '("Capture" . org-capture)
+  (kbd "<leader> od") '("Insert deadline" . org-deadline)
+  (kbd "<leader> os") '("Insert schedule" . org-schedule)
+  (kbd "<leader> or") '("Refile" . org-refile)
+  (kbd "<leader> oil") '("Inser link" . org-insert-link)
+  (kbd "<leader> op") '("Change priority" . org-priority)
+  (kbd "<leader> og") '("Lookup location" . org-goto)
+  (kbd "<leader> oih") '("Insert heading" . org-insert-heading)
+  (kbd "<leader> ois") '("Insert subheading" . org-insert-subheading)
+  (kbd "<leader> onl") '("Add link to org-now" . org-now-link)
+  (kbd "<leader> ont") '("Toggle org-now side window" . org-now)
+  (kbd "<leader> oo") '("Open at point" . org-open-at-point)
 
-    ;; clock
-    (kbd "<leader> ct") '("Update time" . org-clock-update-time-maybe)
-    (kbd "<leader> ci") '("Start clock" . org-clock-in)
-    (kbd "<leader> co") '("Stop clock" . org-clock-out)
-    (kbd "<leader> cpi") '("Punch in clock" . bh/punch-in)
-    (kbd "<leader> cpo") '("Punch out clock" . bh/punch-out)
-    (kbd "<leader> cg") '("Go to clock" . org-clock-goto)
-    (kbd "<leader> clt") '("Clock in the interrupted task" . bh/clock-in-last-task)
-    (kbd "<leader> cs") '("Switch task" . kk/org-clock-in-switch-task)
+  ;; clock
+  (kbd "<leader> ct") '("Update time" . org-clock-update-time-maybe)
+  (kbd "<leader> ci") '("Start clock" . org-clock-in)
+  (kbd "<leader> co") '("Stop clock" . org-clock-out)
+  (kbd "<leader> cpi") '("Punch in clock" . bh/punch-in)
+  (kbd "<leader> cpo") '("Punch out clock" . bh/punch-out)
+  (kbd "<leader> cg") '("Go to clock" . org-clock-goto)
+  (kbd "<leader> clt") '("Clock in the interrupted task" . bh/clock-in-last-task)
+  (kbd "<leader> cs") '("Switch task" . kk/org-clock-in-switch-task)
 
-    ;; denote
-    (kbd "<leader> dn") '("Creat a denote" . denote)
-    (kbd "<leader> dr") '("Rename file" . denote-rename-file)
+  ;; denote
+  (kbd "<leader> dn") '("Creat a denote" . denote)
+  (kbd "<leader> dr") '("Rename file" . denote-rename-file)
 
-    ;; dired
-    (kbd "<leader> dc") '("Cd current file directory" . ai/cd-to-current-buffer)
-    (kbd "<leader> pwd") '("Show current directory" . pwd)
-    (kbd "<leader> di") '("Dired" . dired)
+  ;; dired
+  (kbd "<leader> dc") '("Cd current file directory" . ai/cd-to-current-buffer)
+  (kbd "<leader> pwd") '("Show current directory" . pwd)
+  (kbd "<leader> di") '("Dired" . dired)
 
-    ;; zoxide
-    (kbd "<leader> zf") '("Find file under a path saved in zoxide" . zoxide-find-file)
-    (kbd "<leader> zt") '("Travel to a path saved in zoxide" . zoxide-travel)
-    (kbd "<leader> zc") '("Change working directory to a path" . zoxide-cd)
+  ;; zoxide
+  (kbd "<leader> zf") '("Find file under a path saved in zoxide" . zoxide-find-file)
+  (kbd "<leader> zt") '("Travel to a path saved in zoxide" . zoxide-travel)
+  (kbd "<leader> zc") '("Change working directory to a path" . zoxide-cd)
 
-    ;; delete
-    (kbd "<leader> dd") '("Kill line" . kill-line)
+  ;; delete
+  (kbd "<leader> dd") '("Kill line" . kill-line)
 
-    ;; tab
-    (kbd "<leader> tn") '("Create new tab" . tab-new)
-    (kbd "<leader> tc") '("Close tab" . tab-close)
+  ;; tab
+  (kbd "<leader> tn") '("Create new tab" . tab-new)
+  (kbd "<leader> tc") '("Close tab" . tab-close)
 
-    ;; vterm
-    (kbd "<leader> vt") '("Create new vterm" . multi-vterm)
+  ;; vterm
+  (kbd "<leader> vt") '("Create new vterm" . multi-vterm)
 
-    ;; git
-    (kbd "<leader> gs") '("Show status" . magit-status)
-    (kbd "<leader> gb") '("Switch git status buffer" . emacs-solo/switch-git-status-buffer)
+  ;; git
+  (kbd "<leader> gs") '("Show status" . magit-status)
+  (kbd "<leader> gb") '("Switch git status buffer" . emacs-solo/switch-git-status-buffer)
 
-    ;; search
-    (kbd "<leader> sgr") '("Search with rg" . rgrep)
-    (kbd "<leader> scg") '("Search with consult rg" . consult-ripgrep)
-    (kbd "<leader> scf") '("Search with consult fd" . consult-fd)
-    (kbd "<leader> sch") '("Search with consult org heading" . consult-org-heading)
-    (kbd "<leader> sghn") '("Search with rg everything" . rg-search-everything)
+  ;; search
+  (kbd "<leader> sgr") '("Search with rg" . rgrep)
+  (kbd "<leader> scg") '("Search with consult rg" . consult-ripgrep)
+  (kbd "<leader> scf") '("Search with consult fd" . consult-fd)
+  (kbd "<leader> sch") '("Search with consult org heading" . consult-org-heading)
+  (kbd "<leader> sghn") '("Search with rg everything" . rg-search-everything)
 
-    ;; find
-    (kbd "<leader> ff") '("Find file" . find-file)
+  ;; find
+  (kbd "<leader> ff") '("Find file" . find-file)
 
-    ;; markdown
-    (kbd "<leader> mlp") '("Live Preview" . markdown-live-preview-mode)
+  ;; markdown
+  (kbd "<leader> mlp") '("Live Preview" . markdown-live-preview-mode)
 
-    ;; system
-    (kbd "<leader> ss")  '("Start server" . server-start)
+  ;; system
+  (kbd "<leader> ss")  '("Start server" . server-start)
 
-    ;; Majutsu
-    ;; (kbd "<leader> jj") '("Show status" . majutsu)
-    ;; (kbd "<leader> jl") '("Show log" . majutsu-log)
-    ;; (kbd "<leader> jd") '("Show diff" . majutsu-diff)
+  ;; Majutsu
+  ;; (kbd "<leader> jj") '("Show status" . majutsu)
+  ;; (kbd "<leader> jl") '("Show log" . majutsu-log)
+  ;; (kbd "<leader> jd") '("Show diff" . majutsu-diff)
 
-    ;; gptel
-    (kbd "<leader> gpt") '("start a chat" . gptel)
-    (kbd "<leader> gps") '("submit prompt to llm" . gptel-send)
-    (kbd "<leader> gpm") '("change prompt" . gptel-menu)
+  ;; gptel
+  (kbd "<leader> gpt") '("start a chat" . gptel)
+  (kbd "<leader> gps") '("submit prompt to llm" . gptel-send)
+  (kbd "<leader> gpm") '("change prompt" . gptel-menu)
 
-    ;; org journal
-    (kbd "<leader> jn") '("Creat a entry" . org-journal-new-entry)
-    (kbd "<leader> jo") '("Open current journal" . org-journal-open-current-journal-file))
+  ;; org journal
+  (kbd "<leader> jn") '("Creat a entry" . org-journal-new-entry)
+  (kbd "<leader> jo") '("Open current journal" . org-journal-open-current-journal-file))
 
 ;; Configure hjkl for Org Agenda
 (with-eval-after-load 'org-agenda
