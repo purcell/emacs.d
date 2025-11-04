03e94ed1f9780656aaba4d1165baefb293de141b
Steve Purcell <steve@sanityinc.com>
2025-11-04 17:56:19 +0000

    Use pyproject.toml as a project root marker

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -30,5 +30,10 @@
 (when (maybe-require-package 'reformatter)
   (reformatter-define black :program "black" :args '("-")))
 
+(with-eval-after-load 'project
+  (add-to-list 'project-vc-extra-root-markers "pyproject.toml"))
+(with-eval-after-load 'projectile
+  (add-to-list 'projectile-project-root-files "pyproject.toml"))
+
 (provide 'init-python)
 ;;; init-python.el ends here
