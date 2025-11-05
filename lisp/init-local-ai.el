7db2758afb1f13b9cdefea4d62b9211e0e5e42bc
marcuslannister <marcus@lannister.cc>
2025-11-05 17:46:18 +0800

    feat: add config for log text and change default mode to gpt-4o-mini

--- lisp/init-local-ai.el
+++ lisp/init-local-ai.el
@@ -4,43 +4,49 @@
 
 (use-package gptel
   :ensure t
-  :config (require 'gptel-org))
+  :config (require 'gptel-org)
 
-(setq
- gptel-default-mode 'org-mode
- ;; gptel-model 'openrouter:openai/gpt-4o-mini
- gptel-highlight-mode t
- gptel-backend
- (gptel-make-openai "openrouter"
-   :host "openrouter.ai"
-   :endpoint "/api/v1/chat/completions"
-   :stream t
-   :key #'gptel-api-key-from-auth-source
-   :models '("google/gemini-2.0-flash-exp:free"
-             "google/gemini-2.5-pro"
-             "openai/gpt-4o-mini"
-             "openai/gpt-4.1"
-             "anthropic/claude-sonnet-4.5"
-             "minimax/minimax-m2:free"))
+  (setq
+   gptel-default-mode 'org-mode
+   ;; gptel-model 'openrouter:openai/gpt-4o-mini
+   gptel-highlight-mode t
+   gptel-backend
+   (gptel-make-openai "openrouter"
+     :host "openrouter.ai"
+     :endpoint "/api/v1/chat/completions"
+     :stream t
+     :key #'gptel-api-key-from-auth-source
+     :models '("openai/gpt-4o-mini"
+               "openai/gpt-4.1"
+               "google/gemini-2.0-flash-exp:free"
+               "google/gemini-2.5-pro"
+               "anthropic/claude-sonnet-4.5"
+               "minimax/minimax-m2:free"))
 
- gptel-backends
- (list gptel-backend
-       (gptel-make-perplexity "perplexity"
-         :host "api.perplexity.ai"
-         :stream t
-         :key #'gptel-api-key-from-auth-source
-         :models '("sonar"))
+   gptel-backends
+   (list gptel-backend
+         (gptel-make-perplexity "perplexity"
+           :host "api.perplexity.ai"
+           :stream t
+           :key #'gptel-api-key-from-auth-source
+           :models '("sonar"))
 
-       (gptel-make-openai "sone"
-         :host "sone.19982002.xyz"
-         :stream t
-         :key #'gptel-api-key-from-auth-source
-         :models '("sone-gemini-2.5-flash-think-0"
-                   "sone-gemini-2.5-flash"
-                   "sone-gemini-2.0-flash-think-0"
-                   "sone-gemini-2.5-flash"
-                   "sone-gemini-2.5-pro-search"))))
+         (gptel-make-openai "sone"
+           :host "sone.19982002.xyz"
+           :stream t
+           :key #'gptel-api-key-from-auth-source
+           :models '("sone-gemini-2.5-flash-think-0"
+                     "sone-gemini-2.5-flash"
+                     "sone-gemini-2.0-flash-think-0"
+                     "sone-gemini-2.0-flash"
+                     "sone-gemini-2.5-pro-search")))))
 
+;; The chats can have long lines.
+(add-hook 'gptel-mode-hook 'visual-line-mode)
+
+;; And can be pages long.
+(add-hook 'gptel-post-stream-hook 'gptel-auto-scroll)
+(add-hook 'gptel-post-response-functions 'gptel-end-of-response)
 
 (require 'init-local-gptel-proof nil t)
 
