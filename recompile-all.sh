c0b146aed58b97d77c8065e230e84b64e4d0a57f
marcuslannister <marcus@lannister.cc>
2025-11-25 10:37:03 +0800

    feat: update script to Refresh package contents and recompile

--- recompile-all.sh
+++ recompile-all.sh
@@ -7,9 +7,9 @@ EMACS="/Applications/Emacs.app/Contents/MacOS/Emacs"
 echo "Recompiling config files..."
 $EMACS --batch --eval "(byte-recompile-directory \"~/.emacs.d\" 0 t)"
 
-# Recompile all packages
+# Refresh package contents and recompile all packages
 echo "Recompiling packages..."
-$EMACS --batch --eval "(progn (require 'package) (package-initialize) (package-reinitialize) (byte-recompile-directory package-user-dir 0 t))"
+$EMACS --batch --eval "(progn (require 'package) (setq package-user-dir (expand-file-name \"elpa-31.0\" user-emacs-directory)) (package-initialize) (byte-recompile-directory package-user-dir 0 t))"
 
 echo "Done! You can start Emacs normally now."
 
