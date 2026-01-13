3246f9112a1b2ad41b4b5d6e58f572e217089858
marcuslannister <marcus@lannister.cc>
2026-01-13 14:57:55 +0800

    fix: remove deprecated consult--source- symbols causing startup error on Emacs 31*

--- lisp/init-minibuffer.el
+++ lisp/init-minibuffer.el
@@ -24,8 +24,7 @@
     (sanityinc/no-consult-preview
      consult-ripgrep
      consult-git-grep consult-grep
-     consult-bookmark consult-recent-file consult-xref
-     consult--source-recent-file consult--source-project-recent-file consult--source-bookmark)
+     consult-bookmark consult-recent-file consult-xref)
 
     (defun sanityinc/consult-ripgrep-at-point (&optional dir initial)
       (interactive (list current-prefix-arg
