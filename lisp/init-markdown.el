1701ab8f9521aef959dc9ed2d8dcca5aa8846a1a
Steve Purcell <steve@sanityinc.com>
2026-03-26 14:58:42 +0000

    Revert "Use variable-pitch-mode in markdown"
    This reverts commit c78ceaba4088f25ef185abded40b973dc688e1b2.

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -5,9 +5,7 @@
 (when (maybe-require-package 'markdown-mode)
   (add-auto-mode 'markdown-mode "\\.md\\.html\\'")
   (with-eval-after-load 'whitespace-cleanup-mode
-    (add-to-list 'whitespace-cleanup-mode-ignore-modes 'markdown-mode))
-
-  (add-hook 'markdown-mode-hook 'variable-pitch-mode))
+    (add-to-list 'whitespace-cleanup-mode-ignore-modes 'markdown-mode)))
 
 
 (provide 'init-markdown)
