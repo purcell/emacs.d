3a12abc2abcae6b0da3194160840f34f3a116fbd
Steve Purcell <steve@sanityinc.com>
2026-06-18 08:48:22 +0100

    Remove user-defined org agenda sort strategy (see #889)

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -188,7 +188,7 @@ typical word processor."
         org-agenda-span 'day
         org-agenda-include-diary nil
         org-agenda-sorting-strategy
-        '((agenda habit-down time-up user-defined-up effort-up category-keep)
+        '((agenda habit-down time-up effort-up category-keep)
           (todo category-up effort-up)
           (tags category-up effort-up)
           (search category-up))
